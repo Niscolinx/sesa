5910bb247c33d4550ae1f94c04801dbe15e62e1e
Igboanugwo Collins <munisco12@gmail.com>
2023-01-15 21:48:51 +0100

    working on admin

---
+++ src/components/admins/RenderedAdmins.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RenderedAdmins = () => {
+  return (
+    <div>RenderedAdmins</div>
+  )
+}
+
+export default RenderedAdmins
--- src/pages/dashboard/Admins.tsx
+++ src/pages/dashboard/Admins.tsx
@@ -1,11 +1,47 @@
-import Header from '../../components/dashboard/Header'
+import { useState } from 'react'
+import { IoMdAdd } from 'react-icons/io'
+import AddAdmin from '../../components/admins/AddAdmin'
+import RenderedAdmins from '../../components/admins/RenderedAdmins'
 
 function Admins() {
+    const [adminsLoaded, setAdminsLoaded] = useState(false)
+    const [addAdmin, setAdmin] = useState(false)
+
+    const switchRoute = (path: 'renderedAdmins' | 'addAdmin') => {
+        switch (path) {
+            case 'renderedAdmins':
+                return <RenderedAdmins />
+
+            case 'addAdmin':
+                return <AddAdmin />
+
+            default:
+                return <RenderedAdmins />
+        }
+    }
+
     return (
         <div className='admins'>
             <h1 className='heading2'>Admins</h1>
             <div className='admins__container'>
-                <section className='overview__left'></section>
+                {adminsLoaded ? (
+                    <section>
+                        {switchRoute('renderedAdmins')}
+                    </section>
+                ) : (
+                    <section className='admins__wrapper'>
+                        <img src='/icons/admins/errorSvg.svg' alt='' />
+                        <p className='text'>
+                            Ooops you have not added any Admin yet
+                        </p>
+                        <button className='btn admins__btn'>
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add Admin
+                        </button>
+                    </section>
+                )}
             </div>
         </div>
     )
