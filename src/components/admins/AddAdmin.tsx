d8044cf1aa6c0141a61e91b22bb2d3ad6fe948d6
Igboanugwo Collins <munisco12@gmail.com>
2023-01-15 22:03:26 +0100

    working on admin

--- src/components/admins/AddAdmin.tsx
+++ src/components/admins/AddAdmin.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const AddAdmin = () => {
+  const handleSubmit = (e: React.FormEvent) =>{
+    e.preventDefault()
+  }
   return (
     <div className='addAdmin'>
-      
+      <p className='heading3'>Personal Information</p>
+      <form onSubmit={handleSubmit}>
+
+      </form>
     </div>
   )
 }
