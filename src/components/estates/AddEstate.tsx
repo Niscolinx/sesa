e43fd390a95f7e3df89a14b9ece8c0f321ea072b
Igboanugwo Collins <munisco12@gmail.com>
2023-01-16 17:44:42 +0100

    added skeleton loading

---
+++ src/components/estates/AddEstate.tsx
@@ -0,0 +1,123 @@
+import React, { useContext, useState } from 'react'
+import { GrDown } from 'react-icons/gr'
+import { IoMdAdd } from 'react-icons/io'
+import { AdminPageContext } from '../../Context/AdminPageContext'
+import { getPhotoUrl } from '../../utils/getPhotoUrl'
+
+const AddAdmin = () => {
+  const AdminContextData = useContext(AdminPageContext)
+  const { setRouteToRender } = AdminContextData
+
+
+    const [photoUrl, setPhotoUrl] = useState('')
+
+    const handlePhotoPreview = async (
+        value: React.MouseEvent<HTMLInputElement>
+    ) => {
+        const getUrl = await getPhotoUrl(`#photoUpload`)
+        setPhotoUrl(getUrl)
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        setRouteToRender("renderedAdmins")
+    }
+
+
+
+    return (
+        <div className='addAdmin'>
+            <p className='addAdmin__heading'>Personal Information</p>
+            <form onSubmit={handleSubmit} className='addAdmin__formBox'>
+                <section className='addAdmin__form'>
+                    <div className='addAdmin__form--item'>
+                        <label htmlFor='firstName'>First Name *</label>
+                        <input type='text' required id='firstName' />
+                    </div>
+                    <div className='addAdmin__form--item'>
+                        <label htmlFor='lastName'>Last Name *</label>
+                        <input type='text' required id='lastName' />
+                    </div>
+                    <div className='addAdmin__form--item'>
+                        <label htmlFor='date'>Date of Birth *</label>
+                        <input type='date' id='date' required />
+                    </div>
+                    <div className='addAdmin__form--item'>
+                        <label htmlFor='gender'>Gender *</label>
+                        <div className='item__select'>
+                            <select id='gender'>
+                                <option hidden>&nbsp;</option>
+                                <option value='male'>Male</option>
+                                <option value='female'>Female</option>
+                            </select>
+                            <GrDown />
+                        </div>
+                    </div>
+                    <div className='addAdmin__form--phoneNumber'>
+                        <label htmlFor='phoneNumber'>Phone Number *</label>
+
+                        <div className='phoneNumber__box'>
+                            <select>
+                                <option value='234'>+234</option>
+                            </select>
+                            <input
+                                required
+                                type='number'
+                                inputMode='numeric'
+                                id='phoneNumber'
+                                placeholder='Phone Number'
+                            />
+                        </div>
+                    </div>
+                    <div className='addAdmin__form--item'>
+                        <label htmlFor='email'>Email Address *</label>
+                        <input
+                            required
+                            type='email'
+                            id='email'
+                            placeholder='Your Email'
+                        />
+                    </div>
+                    <div className='addAdmin__form--file'>
+                        <label htmlFor='photoUpload'>
+                            <img src='/icons/admins/photo_library.svg' alt='' />
+                            <p>
+                                Drag estate manager picture here or{' '}
+                                <span>click</span> to upload
+                            </p>
+                        </label>
+                        <input
+                            type='file'
+                            name='photoUpload'
+                            id='photoUpload'
+                            accept='image/*'
+                            className='hidden'
+                            onClick={handlePhotoPreview}
+                        />
+
+                        {photoUrl && (
+                            <div className='file__uploadImgBox'>
+                                <img
+                                    src={photoUrl}
+                                    alt='photoPreview'
+                                    className='object-contain'
+                                />
+                            </div>
+                        )}
+                    </div>
+                    <button
+                        className='btn admins__btn'
+                        style={{ justifySelf: 'start' }}
+                    >
+                        <span>
+                            <IoMdAdd />
+                        </span>{' '}
+                        Add Admin
+                    </button>
+                </section>
+            </form>
+        </div>
+    )
+}
+
+export default AddAdmin
---
+++ src/pages/dashboard/Estates.tsx
@@ -0,0 +1,70 @@
+import { createContext, useState } from 'react'
+import { IoMdAdd } from 'react-icons/io'
+import AddEstate from '../../components/estates/AddEstate'
+import RenderedEstates from '../../components/estates/RenderedEstates'
+import {
+    EstatePageContext,
+    RenderEstatePath,
+} from '../../Context/EstatePageContext'
+
+export const estatesContext = createContext(null as any)
+
+function Estates() {
+    const [estatesLoaded, setEstatesLoaded] = useState(false)
+    const [routeToRender, setRouteToRender] =
+        useState<RenderEstatePath>('renderedEstates')
+
+    const switchRoute = (path: RenderEstatePath) => {
+        switch (path) {
+            case 'renderedEstates':
+                return <RenderedEstates />
+
+            case 'addEstate':
+                return <AddEstate />
+
+            default:
+                return <AddEstate />
+        }
+    }
+
+    console.log({ routeToRender })
+    const handleAddEstate = () => {
+        setEstatesLoaded(true)
+    }
+
+    return (
+        <EstatePageContext.Provider
+            value={{
+                routeToRender,
+                setRouteToRender,
+            }}
+        >
+            <div className='estates'>
+                <h1 className='heading2'>Estates</h1>
+                <div className='estates__container'>
+                    {estatesLoaded ? (
+                        <section>{switchRoute(routeToRender)}</section>
+                    ) : (
+                        <section className='estates__wrapper'>
+                            <img src='/icons/estates/errorSvg.svg' alt='' />
+                            <p className='text'>
+                                Ooops you have not added any Estate yet
+                            </p>
+                            <button
+                                className='btn estates__btn'
+                                onClick={handleAddEstate}
+                            >
+                                <span>
+                                    <IoMdAdd />
+                                </span>{' '}
+                                Add Estate
+                            </button>
+                        </section>
+                    )}
+                </div>
+            </div>
+        </EstatePageContext.Provider>
+    )
+}
+
+export default Estates
