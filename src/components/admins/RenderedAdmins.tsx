3cb0a731ad1d2f9c9653462831759de3d2422b22
Igboanugwo Collins <munisco12@gmail.com>
2023-01-16 11:33:55 +0100

    working on admin table

--- src/components/admins/RenderedAdmins.tsx
+++ src/components/admins/RenderedAdmins.tsx
@@ -1,118 +1,90 @@
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
+import { useEffect, useState } from 'react'
+import { CgSpinnerTwo } from 'react-icons/cg'
 
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'gender', headerName: 'Gender', width: 150 },
+type Admin = {
+    id: string
+    name: string
+    phoneNumber: string
+    status: string
+    onboardingDate: string
+    actions: string
+}
+
+const ADMINDATA: Admin[] = [
     {
-        field: 'phoneNumber',
-        headerName: 'Phone Number',
-        type: 'number',
-        width: 160,
-    },
-    {
-        field: 'onboardingDate',
-        headerName: 'Onboarding Date',
-        width: 160,
-    },
-    {
-        field: 'status',
-        headerName: 'Status',
-        width: 160,
-    },
-    {
-        field: 'actions',
-        headerName: 'Actions',
-        width: 160,
+        id: '1',
+        name: 'Jacintha Sage',
+        phoneNumber: '(+234) 814 324 6719',
+        onboardingDate: '02-May-22',
+        status: 'Active',
+        actions: 'actions'
     },
 ]
 
-const rows = [
-    {
-        id: 1,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 2,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 3,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 4,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 5,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 6,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 7,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-    {
-        id: 8,
-        name: 'Jacintha Sage',
-        gender: 'Male',
-        phoneNumber: '(+234) 814 324 6719',
-        onboardingDate: '02-May-22',
-        status: 'Active',
-        actions: '',
-    },
-]
+function RenderedAdmins() {
+    const [fetchedUsers, setFetchedUsers] = useState<Admin[] | null>([])
 
-export default function RenderedAdmins() {
+    useEffect(() => {
+        const fetchData = async () => {
+            setTimeout(() => {
+                setFetchedUsers(ADMINDATA)
+            }, 2000)
+        }
+        fetchData()
+    }, [])
+
     return (
-        <div className='renderedAdmins'>
-            <DataGrid
-                className='renderedAdmins__table'
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            />
+        <div className='renderedAdmins grid overflow-scroll h-[70vh] mt-5 md:justify-center'>
+            <table className=' w-[30rem] border border-gray-700 transaction-table md:mx-auto relative'>
+                <caption className='font-bold text-lg py-5 uppercase'>
+                    Users
+                </caption>
+
+                <thead>
+                    <tr className='mb-10'>
+                        <th>No</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Verified</th>
+                        <th>Joined</th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {fetchedUsers && fetchedUsers.length > 0 ? (
+                        fetchedUsers.map((value, i) => {
+                            return (
+                                <tr key={i} className='mx-4 py-2'>
+                                    <td>{i + 1}</td>
+                                    <td>
+                                        <span>{value.name}</span>
+                                    </td>
+                                    <td>{value.phoneNumber}</td>
+                                    <td>{value.status.toString()}</td>
+                                    <td>
+                                        <>
+                                            <div className='flex justify-center'>
+                                                {new Date(
+                                                    value.onboardingDate
+                                                ).toLocaleDateString()}
+                                            </div>
+                                        </>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    ) : (
+                        <tr>
+                            <td>
+                                <div className='absolute w-full top-[50%]'>
+                                    <CgSpinnerTwo className='animate-spin text-orange-300 text-4xl' />
+                                </div>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
+
+export default RenderedAdmins
