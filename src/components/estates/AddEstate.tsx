3392b923ee755c969df87ee3321b0b325a2d9861
Igboanugwo Collins <munisco12@gmail.com>
2023-01-18 12:20:01 +0100

    working on estates

--- src/components/estates/AddEstate.tsx
+++ src/components/estates/AddEstate.tsx
@@ -1,13 +1,12 @@
 import React, { useContext, useState } from 'react'
 import { GrDown } from 'react-icons/gr'
 import { IoMdAdd } from 'react-icons/io'
-import { EstateManagerPageContext } from '../../Context/EstateManagerPageContext'
 import { getPhotoUrl } from '../../utils/getPhotoUrl'
+import { EstatePageContext } from '../../Context/EstatePageContext'
 
-const EstateManager = () => {
-  const estateContextData = useContext(EstateManagerPageContext)
-  const { setRouteToRender } = estateContextData
-
+const AddEstate = () => {
+    const EstateContextData = useContext(EstatePageContext)
+    const { setRouteToRender } = EstateContextData
 
     const [photoUrl, setPhotoUrl] = useState('')
 
@@ -20,104 +19,304 @@ const EstateManager = () => {
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-        setRouteToRender("renderedEstateManagers")
+        setRouteToRender('renderedEstates')
     }
 
-
-
     return (
-        <div className='estateManager'>
-            <p className='estateManager__heading'>Personal Information</p>
-            <form onSubmit={handleSubmit} className='estateManager__formBox'>
-                <section className='estateManager__form'>
-                    <div className='estateManager__form--item'>
-                        <label htmlFor='firstName'>First Name *</label>
-                        <input type='text' required id='firstName' />
-                    </div>
-                    <div className='estateManager__form--item'>
-                        <label htmlFor='lastName'>Last Name *</label>
-                        <input type='text' required id='lastName' />
-                    </div>
-                    <div className='estateManager__form--item'>
-                        <label htmlFor='date'>Date of Birth *</label>
-                        <input type='date' id='date' required />
-                    </div>
-                    <div className='estateManager__form--item'>
-                        <label htmlFor='gender'>Gender *</label>
-                        <div className='item__select'>
-                            <select id='gender'>
-                                <option hidden>&nbsp;</option>
-                                <option value='male'>Male</option>
-                                <option value='female'>Female</option>
-                            </select>
-                            <GrDown />
+        <div className='addEstate'>
+            <section>
+                <p className='addEstate__heading'>Estate Details</p>
+                <form onSubmit={handleSubmit} className='addEstate__formBox'>
+                    <section className='addEstate__form'>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='firstName'>Estate Name *</label>
+                            <input type='text' required id='firstName' />
                         </div>
-                    </div>
-                    <div className='estateManager__form--phoneNumber'>
-                        <label htmlFor='phoneNumber'>Phone Number *</label>
-
-                        <div className='phoneNumber__box'>
-                            <select>
-                                <option value='234'>+234</option>
-                            </select>
-                            <input
-                                required
-                                type='number'
-                                inputMode='numeric'
-                                id='phoneNumber'
-                                placeholder='Phone Number'
-                            />
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='lastName'>State *</label>
+                            <div className='item__select'>
+                                <select id='state'>
+                                    <option disabled hidden>
+                                        Placeholder
+                                    </option>
+                                    <option value='male'>Male</option>
+                                    <option value='female'>Female</option>
+                                </select>
+                                <GrDown />
+                            </div>
                         </div>
-                    </div>
-                    <div className='estateManager__form--item'>
-                        <label htmlFor='email'>Email Address *</label>
-                        <input
-                            required
-                            type='email'
-                            id='email'
-                            placeholder='Your Email'
-                        />
-                    </div>
-                    <div className='estateManager__form--file'>
-                        <label htmlFor='photoUpload'>
-                            <img src='/icons/estates/photo_library.svg' alt='' />
-                            <p>
-                                Drag estate manager picture here or{' '}
-                                <span>click</span> to upload
-                            </p>
-                        </label>
-                        <input
-                            type='file'
-                            name='photoUpload'
-                            id='photoUpload'
-                            accept='image/*'
-                            className='hidden'
-                            onClick={handlePhotoPreview}
-                        />
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='date'>Date of Birth *</label>
+                            <input type='date' id='date' required />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='gender'>Gender *</label>
+                            <div className='item__select'>
+                                <select id='gender'>
+                                    <option hidden>&nbsp;</option>
+                                    <option value='male'>Male</option>
+                                    <option value='female'>Female</option>
+                                </select>
+                                <GrDown />
+                            </div>
+                        </div>
+                        <div className='addEstate__form--phoneNumber'>
+                            <label htmlFor='phoneNumber'>Phone Number *</label>
 
-                        {photoUrl && (
-                            <div className='file__uploadImgBox'>
-                                <img
-                                    src={photoUrl}
-                                    alt='photoPreview'
-                                    className='object-contain'
+                            <div className='phoneNumber__box'>
+                                <select>
+                                    <option value='234'>+234</option>
+                                </select>
+                                <input
+                                    required
+                                    type='number'
+                                    inputMode='numeric'
+                                    id='phoneNumber'
+                                    placeholder='Phone Number'
                                 />
                             </div>
-                        )}
-                    </div>
-                    <button
-                        className='btn estates__btn'
-                        style={{ justifySelf: 'start' }}
-                    >
-                        <span>
-                            <IoMdAdd />
-                        </span>{' '}
-                        Add Estate
-                    </button>
-                </section>
-            </form>
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='email'>Email Address *</label>
+                            <input
+                                required
+                                type='email'
+                                id='email'
+                                placeholder='Your Email'
+                            />
+                        </div>
+                        <div className='addEstate__form--file'>
+                            <label htmlFor='photoUpload'>
+                                <img
+                                    src='/icons/addEstates/photo_library.svg'
+                                    alt=''
+                                />
+                                <p>
+                                    Drag addEstate manager picture here or{' '}
+                                    <span>click</span> to upload
+                                </p>
+                            </label>
+                            <input
+                                type='file'
+                                name='photoUpload'
+                                id='photoUpload'
+                                accept='image/*'
+                                className='hidden'
+                                onClick={handlePhotoPreview}
+                            />
+
+                            {photoUrl && (
+                                <div className='file__uploadImgBox'>
+                                    <img
+                                        src={photoUrl}
+                                        alt='photoPreview'
+                                        className='object-contain'
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            className='btn addEstates__btn'
+                            style={{ justifySelf: 'start' }}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add AddEstate
+                        </button>
+                    </section>
+                </form>
+            </section>
+            <section>
+                <p className='addEstate__heading'>Estate Details</p>
+                <form onSubmit={handleSubmit} className='addEstate__formBox'>
+                    <section className='addEstate__form'>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='firstName'>Estate Name *</label>
+                            <input type='text' required id='firstName' />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='lastName'>Last Name *</label>
+                            <input type='text' required id='lastName' />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='date'>Date of Birth *</label>
+                            <input type='date' id='date' required />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='gender'>Gender *</label>
+                            <div className='item__select'>
+                                <select id='gender'>
+                                    <option hidden>&nbsp;</option>
+                                    <option value='male'>Male</option>
+                                    <option value='female'>Female</option>
+                                </select>
+                                <GrDown />
+                            </div>
+                        </div>
+                        <div className='addEstate__form--phoneNumber'>
+                            <label htmlFor='phoneNumber'>Phone Number *</label>
+
+                            <div className='phoneNumber__box'>
+                                <select>
+                                    <option value='234'>+234</option>
+                                </select>
+                                <input
+                                    required
+                                    type='number'
+                                    inputMode='numeric'
+                                    id='phoneNumber'
+                                    placeholder='Phone Number'
+                                />
+                            </div>
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='email'>Email Address *</label>
+                            <input
+                                required
+                                type='email'
+                                id='email'
+                                placeholder='Your Email'
+                            />
+                        </div>
+                        <div className='addEstate__form--file'>
+                            <label htmlFor='photoUpload'>
+                                <img
+                                    src='/icons/addEstates/photo_library.svg'
+                                    alt=''
+                                />
+                                <p>
+                                    Drag addEstate manager picture here or{' '}
+                                    <span>click</span> to upload
+                                </p>
+                            </label>
+                            <input
+                                type='file'
+                                name='photoUpload'
+                                id='photoUpload'
+                                accept='image/*'
+                                className='hidden'
+                                onClick={handlePhotoPreview}
+                            />
+
+                            {photoUrl && (
+                                <div className='file__uploadImgBox'>
+                                    <img
+                                        src={photoUrl}
+                                        alt='photoPreview'
+                                        className='object-contain'
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            className='btn addEstates__btn'
+                            style={{ justifySelf: 'start' }}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add AddEstate
+                        </button>
+                    </section>
+                </form>
+            </section>
+            <section>
+                <p className='addEstate__heading'>Estate Details</p>
+                <form onSubmit={handleSubmit} className='addEstate__formBox'>
+                    <section className='addEstate__form'>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='firstName'>Estate Name *</label>
+                            <input type='text' required id='firstName' />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='lastName'>Last Name *</label>
+                            <input type='text' required id='lastName' />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='date'>Date of Birth *</label>
+                            <input type='date' id='date' required />
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='gender'>Gender *</label>
+                            <div className='item__select'>
+                                <select id='gender'>
+                                    <option hidden>&nbsp;</option>
+                                    <option value='male'>Male</option>
+                                    <option value='female'>Female</option>
+                                </select>
+                                <GrDown />
+                            </div>
+                        </div>
+                        <div className='addEstate__form--phoneNumber'>
+                            <label htmlFor='phoneNumber'>Phone Number *</label>
+
+                            <div className='phoneNumber__box'>
+                                <select>
+                                    <option value='234'>+234</option>
+                                </select>
+                                <input
+                                    required
+                                    type='number'
+                                    inputMode='numeric'
+                                    id='phoneNumber'
+                                    placeholder='Phone Number'
+                                />
+                            </div>
+                        </div>
+                        <div className='addEstate__form--item'>
+                            <label htmlFor='email'>Email Address *</label>
+                            <input
+                                required
+                                type='email'
+                                id='email'
+                                placeholder='Your Email'
+                            />
+                        </div>
+                        <div className='addEstate__form--file'>
+                            <label htmlFor='photoUpload'>
+                                <img
+                                    src='/icons/addEstates/photo_library.svg'
+                                    alt=''
+                                />
+                                <p>
+                                    Drag addEstate manager picture here or{' '}
+                                    <span>click</span> to upload
+                                </p>
+                            </label>
+                            <input
+                                type='file'
+                                name='photoUpload'
+                                id='photoUpload'
+                                accept='image/*'
+                                className='hidden'
+                                onClick={handlePhotoPreview}
+                            />
+
+                            {photoUrl && (
+                                <div className='file__uploadImgBox'>
+                                    <img
+                                        src={photoUrl}
+                                        alt='photoPreview'
+                                        className='object-contain'
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            className='btn addEstates__btn'
+                            style={{ justifySelf: 'start' }}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add AddEstate
+                        </button>
+                    </section>
+                </form>
+            </section>
         </div>
     )
 }
 
-export default EstateManager
+export default AddEstate
