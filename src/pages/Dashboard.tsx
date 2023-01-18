8f6149c44fef24dfaabd7cde7764b4bb51983d1c
Igboanugwo Collins <munisco12@gmail.com>
2023-01-18 15:15:32 +0100

    working on estates

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,20 +7,17 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 const Modal = () => {
-
     return (
-        <div className='bg-white rounded-lg justify-center justify-items-center w-[64rem] h-[34]'>
+        <div className='bg-white rounded-lg justify-center justify-items-center w-[64rem] h-[34rem]'>
+            <img src='/icons/admins/modalSuccess.svg' alt='' />
             <p>You have successfully added an Estate</p>
 
             <div className='flex w-full'>
-                <button className='btn'>
-                    View details
-                </button>
-                <button className='btn'>
+                <button className='btn'>View details</button>
+                <button className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem]'>
                     Ok
                 </button>
             </div>
-            
         </div>
     )
 }
@@ -36,7 +33,7 @@ function Dashboard() {
                     imgUrl='/img/me.jpeg'
                 />
                 <div className='p-10 overflow-scroll max-h-[90vh]'>
-                    <Modal/>
+                    <Modal />
                     {/* <Outlet /> */}
                 </div>
             </div>
