488b389eded37d882ceba347214dfc38ef7f8ffc
Igboanugwo Collins <munisco12@gmail.com>
2023-01-19 11:21:58 +0100

    working on routeChanges for pages in redux

--- src/store/features/routeChange.ts
+++ src/store/features/routeChange.ts
@@ -1,21 +1,37 @@
+import { RenderEstateManagerPath } from './../../Context/EstateManagerPageContext';
+import { RenderAdminPath } from './../../Context/AdminPageContext';
 import { RenderEstatePath } from './../../Context/EstatePageContext';
 import { createSlice } from '@reduxjs/toolkit'
 import { AppState } from '../app/store'
 
 
 interface RouteChangeState {
+    adminPage: RenderAdminPath
     estatePage: RenderEstatePath
+    estateManagerPage: RenderEstateManagerPath
 }
 
 const initialState: RouteChangeState = {
-    estatePage: 'renderedEstates'
+    estatePage: 'renderedEstates',
+    adminPage: 'renderedAdmins',
+    estateManagerPage: 'renderedEstateManagers'
 } 
 
 const routeChange = createSlice({
     name: 'routeChange',
     initialState,
     reducers: {
+        setEstatePage: (state, action) => {
+            state.estatePage = action.payload
+        },
+        setAdminPage: (state, action) => {
+            state.adminPage = action.payload
+        },
+        setEstateManagerPage: (state, action) => {
+            state.estateManagerPage = action.payload
+        }
         
+
     },
 })
 
