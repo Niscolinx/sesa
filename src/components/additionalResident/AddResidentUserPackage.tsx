74efdaef310b63bcd3a73d46929b61271ad84f96
Igboanugwo Collins <munisco12@gmail.com>
2023-01-24 13:35:05 +0100

    working on additional residential details

--- src/components/additionalResident/AddResidentUserPackage.tsx
+++ src/components/additionalResident/AddResidentUserPackage.tsx
@@ -6,63 +6,32 @@ import { ModalContext } from '../../Context/ModalContext'
 import RolesAndPerm from '../../pages/dashboard/RolesAndPerm'
 import { getPhotoUrl } from '../../utils/getPhotoUrl'
 
-
 type Packages = 'package 1' | 'package 2' | 'package 3' | 'package 4'
 type Frequency = 'monthly' | 'weekly' | 'quarterly' | 'yearly'
 
 const AddResidentUserPackage = () => {
     const ModalContextData = useContext(ModalContext)
     const { handleOpen } = ModalContextData
-     const [packages, setPackages] = useState<Packages[]>([
+    const [packages, setPackages] = useState<Packages[]>([
         'package 1',
         'package 2',
         'package 3',
-        'package 4'
+        'package 4',
     ])
-     const [frequency, setFrequency] = useState<Frequency[]>([
+    const [frequency, setFrequency] = useState<Frequency[]>([
         'monthly',
         'weekly',
         'quarterly',
-        'yearly'
+        'yearly',
     ])
 
-
+    const [togglePackageMenu, setTogglePackageMenu] = useState(false)
+    const [selectedPackage, setSelectedPackage] = useState<Packages>('package 1')
+  
 
     
-    const [selectedPackage, setSelectedPackage] = useState<{
-        [key: string]: Packages
-    }>(null as any)
-    const [toggleDropDown, setToggleDropDown] = useState<{
-        isDropDownOpen: boolean
-        index: number | null
-    }>({
-        isDropDownOpen: false,
-        index: null,
-    })
-
-     const dropDownHandler = (
-        e: React.ChangeEvent<HTMLInputElement>,
-        index: number
-    ) => {
-        setToggleDropDown((prev) => {
-            return {
-                isDropDownOpen: e.target.checked,
-                index: index,
-            }
-        })
-    }
 
 
-
-    const selectRole = (e: React.MouseEvent, item: string, index: number) => {
-        console.log('select role')
-        setSelectedPackage((prev) => {
-            return {
-                ...prev,
-                [index]: item,
-            }
-        })
-    }
 
     return (
         <div className=' p-8 bg-white h-[70vh] rounded-lg overflow-y-scroll'>
@@ -72,54 +41,41 @@ const AddResidentUserPackage = () => {
                     gridTemplateColumns: 'repeat(auto-fit, minmax(40rem, 1fr))',
                 }}
             >
-                <td>
-                    <label
-                        className='font-semibold capitalize cursor-pointer flex items-center gap-2 relative z-10'
-                        htmlFor={i.toString()}
-                        onClick={() =>
-                            setToggleDropDown((prev) => {
-                                return {
-                                    isDropDownOpen: !prev.isDropDownOpen,
-                                    index: i,
-                                }
-                            })
-                        }
-                    >
-                        {selectedPackage && selectedPackage[i] ? (
-                            selectedPackage[i]
-                        ) : (
-                            <span className='text-color-primary'>
-                                Select Role
-                            </span>
-                        )}
-                        {isDropDownOpen && index === i ? (
-                            <GrUp className='w-[1rem] h-[1rem]' />
-                        ) : (
-                            <GrDown className='w-[1rem] h-[1rem]' />
-                        )}
-                    </label>
-                    <input
-                        type='radio'
-                        name='dropdown'
-                        className='hidden'
-                        id={i.toString()}
-                        onChange={(e) => dropDownHandler(e, i)}
-                    />
+                <div>
+                    <div className='relative'>
+                        <label
+                            className='font-semibold capitalize cursor-pointer flex items-center gap-2 relative z-10'
+                            htmlFor='package'
+                          
+                        >
+                            package
+                        </label>
+                        <input
+                            type='radio'
+                            name='dropdown'
+                            className='hidden'
+                            id='package'
+                            onChange={(e) => dropDownHandler(e, 'package')}
+                        />
 
-                    {isDropDownOpen && index === i && (
-                        <div className='absolute top-[5rem] translate-x-[6rem] border border-color-primary-light w-[24rem] bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
-                            {roles.map((item, index) => (
-                                <p
-                                    className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
-                                    key={index + i}
-                                    onClick={(e) => selectRole(e, item, i)}
-                                >
-                                    {item}
-                                </p>
-                            ))}
-                        </div>
-                    )}
+                        {isDropDownOpen && (
+                            <div className='absolute top-[5rem] translate-x-[6rem] border border-color-primary-light w-[24rem] bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
+                                {packages.map((item, index) => (
+                                    <p
+                                        className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
+                                        key={index}
+                                        onClick={() =>
+                                            setSelectedPackage(item)
+                                        }
+                                    >
+                                        {item}
+                                    </p>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
+
                 <div className='w-full grid gap-4'>
                     <label
                         htmlFor='frequency'
