938e6dc8f44d24c7e4002cc27aea3b3267fb35b8
Igboanugwo Collins <munisco12@gmail.com>
2023-01-24 10:32:39 +0100

    working on additional residential user page

--- src/components/additionalResident/AddResidentUserPackage.tsx
+++ src/components/additionalResident/AddResidentUserPackage.tsx
@@ -24,34 +24,22 @@ const AddResidentUserPackage = () => {
 
     return (
       
-        <div className='addEstateManager'>
-            <p className='addEstateManager__heading'>Personal Information</p>
-            <form onSubmit={handleSubmit} className='addEstateManager__formBox'>
-                <section className='addEstateManager__form'>
-                    <div className='addEstateManager__form--item'>
+        <div className='grid p-8 bg-white h-[80vh]'>
+                <section className=''>
+                    <div className=''>
                         <label htmlFor='firstName'>First Name *</label>
                         <input type='text' required id='firstName' />
                     </div>
-                    <div className='addEstateManager__form--item'>
+                    <div className=''>
                         <label htmlFor='lastName'>Last Name *</label>
                         <input type='text' required id='lastName' />
                     </div>
-                    <div className='addEstateManager__form--item'>
+                    <div className=''>
                         <label htmlFor='date'>Date of Birth *</label>
                         <input type='date' id='date' required />
                     </div>
-                    <div className='addEstateManager__form--item'>
-                        <label htmlFor='gender'>Gender *</label>
-                        <div className='item__select'>
-                            <select id='gender'>
-                                <option hidden>&nbsp;</option>
-                                <option value='male'>Male</option>
-                                <option value='female'>Female</option>
-                            </select>
-                            <GrDown />
-                        </div>
-                    </div>
-                    <div className='addEstateManager__form--phoneNumber'>
+                    
+                    <div className=''>
                         <label htmlFor='phoneNumber'>Phone Number *</label>
 
                         <div className='phoneNumber__box'>
@@ -67,56 +55,10 @@ const AddResidentUserPackage = () => {
                             />
                         </div>
                     </div>
-                    <div className='addEstateManager__form--item'>
-                        <label htmlFor='email'>Email Address *</label>
-                        <input
-                            required
-                            type='email'
-                            id='email'
-                            placeholder='Your Email'
-                        />
-                    </div>
-                    <div className='addEstateManager__form--file'>
-                        <label htmlFor='photoUpload'>
-                            <img
-                                src='/icons/estateManagers/photo_library.svg'
-                                alt=''
-                            />
-                            <p>
-                                Drag estate manager picture here or{' '}
-                                <span>click</span> to upload
-                            </p>
-                        </label>
-                        <input
-                            type='file'
-                            name='photoUpload'
-                            id='photoUpload'
-                            accept='image/*'
-                            className='hidden'
-                            onClick={handlePhotoPreview}
-                        />
-
-                        {photoUrl && (
-                            <div className='file__uploadImgBox'>
-                                <img
-                                    src={photoUrl}
-                                    alt='photoPreview'
-                                    className='object-contain'
-                                />
-                            </div>
-                        )}
-                    </div>
-                    <button
-                        className='btn estateManagers__btn'
-                        style={{ justifySelf: 'start' }}
-                    >
-                        <span>
-                            <IoMdAdd />
-                        </span>{' '}
-                        Add EstateManager
-                    </button>
+                    
+                    
+                    
                 </section>
-            </form>
         </div>
     )
 }
