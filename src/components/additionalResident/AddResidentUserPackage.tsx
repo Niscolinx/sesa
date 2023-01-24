8b47a3fea3a8534d9d8c2df216932f9f2750829e
Igboanugwo Collins <munisco12@gmail.com>
2023-01-24 10:44:07 +0100

    working on additional residential user page

--- src/components/additionalResident/AddResidentUserPackage.tsx
+++ src/components/additionalResident/AddResidentUserPackage.tsx
@@ -8,45 +8,47 @@ const AddResidentUserPackage = () => {
     const ModalContextData = useContext(ModalContext)
     const { handleOpen } = ModalContextData
 
-  
-
     return (
-      
-        <div className='grid p-8 bg-white h-[80vh]'>
-                <section className=''>
-                    <div className=''>
-                        <label htmlFor='firstName'>First Name *</label>
-                        <input type='text' required id='firstName' />
-                    </div>
-                    <div className=''>
-                        <label htmlFor='lastName'>Last Name *</label>
-                        <input type='text' required id='lastName' />
-                    </div>
-                    <div className=''>
-                        <label htmlFor='date'>Date of Birth *</label>
-                        <input type='date' id='date' required />
-                    </div>
-                    
-                    <div className=''>
-                        <label htmlFor='phoneNumber'>Phone Number *</label>
+        <div className=' p-8 bg-white h-[80vh]'>
+            <div className='flex justify-end'>
+                <button className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'>
+                    <img src='/icons/admins/delete.svg' alt='' />
+                    <span className='text-red-600 text-[1.4rem] font-semibold '>
+                        Deactivate
+                    </span>
+                </button>
+            </div>
+            <section className='grid'>
+                <div className=''>
+                    <label htmlFor='firstName'>First Name *</label>
+                    <input type='text' required id='firstName' />
+                </div>
+                <div className=''>
+                    <label htmlFor='lastName'>Last Name *</label>
+                    <input type='text' required id='lastName' />
+                </div>
+                <div className=''>
+                    <label htmlFor='date'>Date of Birth *</label>
+                    <input type='date' id='date' required />
+                </div>
 
-                        <div className='phoneNumber__box'>
-                            <select>
-                                <option value='234'>+234</option>
-                            </select>
-                            <input
-                                required
-                                type='number'
-                                inputMode='numeric'
-                                id='phoneNumber'
-                                placeholder='Phone Number'
-                            />
-                        </div>
+                <div className=''>
+                    <label htmlFor='phoneNumber'>Phone Number *</label>
+
+                    <div className='phoneNumber__box'>
+                        <select>
+                            <option value='234'>+234</option>
+                        </select>
+                        <input
+                            required
+                            type='number'
+                            inputMode='numeric'
+                            id='phoneNumber'
+                            placeholder='Phone Number'
+                        />
                     </div>
-                    
-                    
-                    
-                </section>
+                </div>
+            </section>
         </div>
     )
 }
