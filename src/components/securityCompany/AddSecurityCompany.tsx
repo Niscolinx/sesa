4f711f830307c152cd3cb474d63fd5e0f78e8471
Igboanugwo Collins <munisco12@gmail.com>
2023-01-31 10:49:03 +0100

    working on wallet screens

--- src/components/securityCompany/AddSecurityCompany.tsx
+++ src/components/securityCompany/AddSecurityCompany.tsx
@@ -121,7 +121,7 @@ const AddEstateManager = () => {
                         className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                     />
                 </div>
-                <div className='relative self-end grid gap-4'>
+                <div className='relative grid gap-4'>
                     <p className='text-[1.4rem] font-semibold'>State</p>
                     <p
                         className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] cursor-pointer'
@@ -129,6 +129,7 @@ const AddEstateManager = () => {
                     >
                         {selectedState}
                     </p>
+                    <GrDown className='absolute right-0'/>
 
                     {toggleStateMenu && (
                         <div className='absolute top-[8rem]  left-0 border border-color-primary-light w-[24rem] bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
