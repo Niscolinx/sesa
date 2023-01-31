dd897b5c806f7002042504b54501bcf37071f85a
Igboanugwo Collins <munisco12@gmail.com>
2023-01-31 17:10:31 +0100

    working on security detail

--- src/components/securityCompany/SecurityCompanyDetail.tsx
+++ src/components/securityCompany/SecurityCompanyDetail.tsx
@@ -149,7 +149,6 @@ const SecurityCompanyDetail = () => {
                             Security Company *
                         </label>
                         <input
-                            
                             type='text'
                             value='Orca Security'
                             required
@@ -163,7 +162,7 @@ const SecurityCompanyDetail = () => {
                             htmlFor='email'
                             className='text-[1.4rem] font-medium'
                         >
-                            Email Address *
+                            Email Address 
                         </label>
                         <input
                             type='email'
@@ -175,7 +174,22 @@ const SecurityCompanyDetail = () => {
                         />
                     </div>
 
-                  
+                    <div className='grid gap-4 relative'>
+                        <label
+                            htmlFor='address'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Address *
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            disabled
+                            value={`04, Wright Avenue Lagos, Nigeria`}
+                            id='address'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
+                        />
+                    </div>
                     <div className='grid gap-4 relative'>
                         <label
                             htmlFor='address'
@@ -191,83 +205,67 @@ const SecurityCompanyDetail = () => {
                             className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
                         />
                     </div>
-                    <div className='relative grid gap-4'>
-                        <p className='text-[1.4rem] font-semibold'>State</p>
-                        <div className='relative flex items-center'>
-                            <p
-                                className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] cursor-pointer'
-                                onClick={stateMenuToggler}
-                            >
-                                {selectedState ? (
-                                    selectedState
-                                ) : (
-                                    <span className='text-gray-500'>
-                                        Select State
-                                    </span>
-                                )}
-                            </p>
-                            {toggleStateMenu ? (
-                                <GrUp className='absolute right-4' />
-                            ) : (
-                                <GrDown className='absolute right-4' />
-                            )}
-                        </div>
-
-                        {toggleStateMenu && (
-                            <div className='absolute top-[8rem]  left-0 border border-color-primary-light w-[24rem] bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
-                                {state.map((item, index) => (
-                                    <p
-                                        className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
-                                        key={index}
-                                        onClick={() =>
-                                            handleSelectedState(item)
-                                        }
-                                    >
-                                        {item}
-                                    </p>
-                                ))}
-                            </div>
-                        )}
-                    </div>
-
-                    <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
+                    <div className='grid gap-4 relative'>
                         <label
-                            htmlFor='photoUpload'
-                            className='flex justify-center gap-4 items-center cursor-pointer'
+                            htmlFor='address'
+                            className='text-[1.4rem] font-medium'
                         >
-                            <img src='/icons/admins/photo_library.svg' alt='' />
-                            <p
-                                className='text-color-dark-1'
-                                style={{
-                                    fontFamily: 'Satoshi-Light',
-                                }}
-                            >
-                                Drag estate manager picture here or{' '}
-                                <span className='text-color-blue font-bold'>
-                                    click
-                                </span>{' '}
-                                to upload
-                            </p>
+                            Address *
                         </label>
                         <input
-                            type='file'
-                            name='photoUpload'
-                            id='photoUpload'
-                            accept='image/*'
-                            className='hidden'
-                            onClick={handlePhotoPreview}
+                            type='text'
+                            required
+                            disabled
+                            id='address'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
                         />
-
-                        {photoUrl && (
-                            <div className='flex justify-center justify-self-center'>
-                                <img
-                                    src={photoUrl}
-                                    alt='photoPreview'
-                                    className='object-cover w-[11rem] h-[11rem] rounded-full'
-                                />
-                            </div>
-                        )}
                     </div>
+                    <div className='grid gap-4 relative'>
+                        <label
+                            htmlFor='address'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Address *
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            disabled
+                            id='address'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
+                        />
+                    </div>
+                    <div className='grid gap-4 relative'>
+                        <label
+                            htmlFor='address'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Address *
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            disabled
+                            id='address'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
+                        />
+                    </div>
+                    <div className='grid gap-4 relative'>
+                        <label
+                            htmlFor='address'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Address *
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            disabled
+                            id='address'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 disabled:text-gray-600'
+                        />
+                    </div>
+
                     <button
                         className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg'
                         style={{ justifySelf: 'start' }}
