f773c215f36a8fa660aaac45e8357fda1088cbe9
Igboanugwo Collins <munisco12@gmail.com>
2023-02-01 10:55:05 +0100

    added wallet bar chart

--- src/components/charts/WalletBarChart.tsx
+++ src/components/charts/WalletBarChart.tsx
@@ -4,38 +4,32 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 const data = [
     {
         name: 'Mon',
-        pv: 30_000,
-        amt: 30_000,
+        pv: 25_000,
     },
     {
         name: 'Tue',
-        pv: 25_000,
-        amt: 25_000,
+        pv: 30_000,
     },
     {
         name: 'Wed',
         pv: 20_000,
-        amt: 20_000,
     },
     {
         name: 'Thur',
         pv: 15_000,
-        amt: 15_000,
     },
     {
         name: 'Fri',
         pv: 10_000,
-        amt: 10_000,
+        
     },
     {
         name: 'Sat',
         pv: 5_000,
-        amt: 5_000,
     },
     {
         name: 'Sun',
         pv: 0,
-        amt: 0,
     },
 ]
 
@@ -52,12 +46,12 @@ export default function WalletBarChart() {
                 bottom: 5,
             }}
         >
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='name' />
+            <CartesianGrid strokeOpacity={0} />
+            <XAxis dataKey='name'  />
             <YAxis />
             <Tooltip />
 
-            <Bar dataKey='pv' fill='#8884d8' />
+            <Bar dataKey='pv' fill='#08D231' />
         </BarChart>
     )
 }
