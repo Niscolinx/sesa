3569876b1c78cad5b971ae4bcfbb9b24a135a37f
Igboanugwo Collins <munisco12@gmail.com>
2023-02-01 10:27:55 +0100

    working on estate wallet

--- src/pages/dashboard/Wallet/EstateWallet.tsx
+++ src/pages/dashboard/Wallet/EstateWallet.tsx
@@ -6,7 +6,7 @@ const EstateWallet = () => {
     return (
         <div>
             <h1 className='heading2'>Estate Wallet</h1>
-            <div className='bg-white grid mt-12 py-10 gap-8 rounded-lg h-[80vh]'>
+            <div className='bg-white grid mt-12 py-10 gap-8 rounded-lg h-[80vh] p-8'>
                 <div className=''>
                     <OverviewWallet
                         amount={1_032_422}
