30b80319ae794329472195c1c42d21d5a8703bb3
Igboanugwo Collins <munisco12@gmail.com>
2023-02-01 05:31:10 +0100

    working on wallet

--- src/Entry/App.tsx
+++ src/Entry/App.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter(
                 path='additional-resident/:Id'
                 element={<AdditionalResidentDetails />}
             />
-            <Route path='wallet/:Id' element={<Wallet/>} />
+            <Route path='wallet' element={<Wallet/>} />
         </Route>,
         <Route path='*' element={<Navigate to='/' />} />,
     ])
