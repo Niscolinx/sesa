900cf376ba7dafbc6d2a03b45004bae6af6cbc83
Igboanugwo Collins <munisco12@gmail.com>
2023-02-02 11:00:25 +0100

    working on the commission wallet

--- src/pages/dashboard/Wallet/CommissionWallet.tsx
+++ src/pages/dashboard/Wallet/CommissionWallet.tsx
@@ -477,7 +477,7 @@ const CommissionWallet = () => {
                                     <p className='text-[1.4rem] font-semibold'>
                                         SendTo
                                     </p>
-                                    <div className='relative flex items-center w-full'>
+                                    <div className='relative flex items-center w-[20rem]'>
                                         <p
                                             className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] cursor-pointer'
                                             onClick={sendToMenuToggle}
@@ -492,7 +492,7 @@ const CommissionWallet = () => {
                                     </div>
 
                                     {sendToMenu && (
-                                        <div className='absolute top-[8rem]  left-0 border border-color-primary-light  bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
+                                        <div className='absolute top-[-2rem]  left-[22rem] border border-color-primary-light  bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
                                             {sendToArr.map((item, index) => (
                                                 <p
                                                     className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
