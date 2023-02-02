2dc3220afafc390f5895c7201bb9101d64aa1f76
Igboanugwo Collins <munisco12@gmail.com>
2023-02-02 15:41:12 +0100

    working on the commission wallet

--- src/pages/dashboard/Artisan.tsx
+++ src/pages/dashboard/Artisan.tsx
@@ -22,7 +22,7 @@ function Artisan() {
                         {/* <RenderArtisans /> */}
                     </section>
                 ) : (
-                    <section className='estates__wrapper bg-white'>
+                    <section className='grid  place-content-center w-full h-full justify-items-center gap-4 bg-white'>
                         <img src='/icons/admins/errorSvg.svg' alt='' />
                         <p className='text'>
                             Ooops you have not added any Artisan yet
