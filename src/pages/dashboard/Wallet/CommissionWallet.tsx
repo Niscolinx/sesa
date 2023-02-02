8065c041c2529b0f785e2b557d025e6b03c6e006
Igboanugwo Collins <munisco12@gmail.com>
2023-02-02 10:45:24 +0100

    working on the commission wallet

--- src/pages/dashboard/Wallet/CommissionWallet.tsx
+++ src/pages/dashboard/Wallet/CommissionWallet.tsx
@@ -432,6 +432,36 @@ const CommissionWallet = () => {
                                         className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem]'
                                     />
                                 </div>
+                                <div className='w-full grid gap-4'>
+                                    <label
+                                        htmlFor='description'
+                                        className='text-[1.4rem] font-semibold'
+                                    >
+                                        Description
+                                    </label>
+
+                                    <input
+                                        type='text'
+                                        required
+                                        id='description'
+                                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem]'
+                                    />
+                                </div>
+                                <div className='w-full grid gap-4'>
+                                    <label
+                                        htmlFor='commissionWalletAttachment'
+                                        className='text-[1.4rem] font-semibold'
+                                    >
+                                        Attach An Item
+                                    </label>
+
+                                    <input
+                                        type='file'
+                                        id='commissionWalletAttachment'
+                                        required
+                                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem]'
+                                    />
+                                </div>
 
                                 <button className='btn bg-[#0556E5] text-white rounded-lg py-4 place-self-start w-[15rem]'>
                                     Request
