756abf1fc1ed5759aba2dcd8eeb901d8d67847da
Igboanugwo Collins <munisco12@gmail.com>
2023-02-03 15:43:47 +0100

    working on add artisan

--- src/components/artisans/list/AddArtisan.tsx
+++ src/components/artisans/list/AddArtisan.tsx
@@ -171,6 +171,12 @@ const AddArtisan = () => {
                                 </div>
                             </div>
                         </div>
+                        <button
+                            className='btn bg-white text-[#0556E5] border-[#0556E5] border rounded-lg w-[15rem]'
+                            onClick={() => closeValidateDialog()}
+                        >
+                            Cancel
+                        </button>
                     </div>
                 </section>
             </dialog>
@@ -203,32 +209,29 @@ const AddArtisan = () => {
                                     setSelectedState={setValidationType}
                                 />
 
-                            
-                                    <div className='grid gap-4'>
-                                        <label
-                                            htmlFor='phoneNumber'
-                                            className='text-[1.4rem] font-medium'
-                                        >
-                                            Phone Number
-                                        </label>
+                                <div className='grid gap-4'>
+                                    <label
+                                        htmlFor='phoneNumber'
+                                        className='text-[1.4rem] font-medium'
+                                    >
+                                        Phone Number
+                                    </label>
 
-                                        <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
-                                            <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
-                                                <option value='234'>
-                                                    +234
-                                                </option>
-                                            </select>
-                                            <input
-                                                required
-                                                type='number'
-                                                inputMode='numeric'
-                                                id='phoneNumber'
-                                                placeholder='Phone Number'
-                                                className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
-                                            />
-                                        </div>
+                                    <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                                        <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                                            <option value='234'>+234</option>
+                                        </select>
+                                        <input
+                                            required
+                                            type='number'
+                                            inputMode='numeric'
+                                            id='phoneNumber'
+                                            placeholder='Phone Number'
+                                            className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                                        />
                                     </div>
-                            
+                                </div>
+
                                 <p
                                     className='text-[#043FA7] flex items-center gap-2'
                                     style={{
