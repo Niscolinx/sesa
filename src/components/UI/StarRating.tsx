821507368fbf0e859b4812ac56abdca9a79f721d
Igboanugwo Collins <munisco12@gmail.com>
2023-02-03 07:59:22 +0100

    working on artisan list

--- src/components/UI/StarRating.tsx
+++ src/components/UI/StarRating.tsx
@@ -5,6 +5,11 @@ interface IStarRating {
     starsNum: number
 }
 
+// onDoubleClick={() => {
+// setRating(0);
+// setHover(0);
+// }}
+
 const StarRating:FC<IStarRating> = ({starsNum}) => {
     const [rating, setRating] = useState(0)
     const [hover, setHover] = useState(0)
@@ -16,7 +21,9 @@ const StarRating:FC<IStarRating> = ({starsNum}) => {
                     <button
                         type='button'
                         key={index}
-                        className={index <= (hover || rating) ? 'on' : 'off'}
+                        className={
+                            index <= ((rating && hover) || hover) ? 'on' : 'off'
+                        }
                         onClick={() => setRating(index)}
                         onMouseEnter={() => setHover(index)}
                         onMouseLeave={() => setHover(rating)}
