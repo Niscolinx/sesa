3ba1b93e16077cb5e8b411ab3060c6aece82ef45
Igboanugwo Collins <munisco12@gmail.com>
2023-02-03 15:41:59 +0100

    working on add artisan

--- src/components/artisans/list/AddArtisan.tsx
+++ src/components/artisans/list/AddArtisan.tsx
@@ -20,15 +20,10 @@ const AddArtisan = () => {
     const [selectedGender, setSelectedGender] = useState<string | null>(null)
     const [isAddArtisan, setIsAddArtisan] = useState(true)
     const [validationType, setValidationType] = useState<string | null>('Phone Number')
-    const [isValidationTypeSelected, setIsValidationTypeSelected] = useState<boolean>(false)
 
     const [photoUrl, setPhotoUrl] = useState('')
 
-    useEffect(() => {
-        if (validationType) {
-            setIsValidationTypeSelected(true)
-        }
-    }, [validationType])
+    
 
     const handlePhotoPreview = async (
         _: React.MouseEvent<HTMLInputElement>
@@ -208,28 +203,32 @@ const AddArtisan = () => {
                                     setSelectedState={setValidationType}
                                 />
 
-                                <div className='grid gap-4'>
-                                    <label
-                                        htmlFor='phoneNumber'
-                                        className='text-[1.4rem] font-medium'
-                                    >
-                                        Phone Number
-                                    </label>
+                            
+                                    <div className='grid gap-4'>
+                                        <label
+                                            htmlFor='phoneNumber'
+                                            className='text-[1.4rem] font-medium'
+                                        >
+                                            Phone Number
+                                        </label>
 
-                                    <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
-                                        <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
-                                            <option value='234'>+234</option>
-                                        </select>
-                                        <input
-                                            required
-                                            type='number'
-                                            inputMode='numeric'
-                                            id='phoneNumber'
-                                            placeholder='Phone Number'
-                                            className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
-                                        />
+                                        <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                                            <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                                                <option value='234'>
+                                                    +234
+                                                </option>
+                                            </select>
+                                            <input
+                                                required
+                                                type='number'
+                                                inputMode='numeric'
+                                                id='phoneNumber'
+                                                placeholder='Phone Number'
+                                                className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                                            />
+                                        </div>
                                     </div>
-                                </div>
+                            
                                 <p
                                     className='text-[#043FA7] flex items-center gap-2'
                                     style={{
