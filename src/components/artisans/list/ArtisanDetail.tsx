79eedb5db33b04b8105b48e1657963334aa8ff3a
Igboanugwo Collins <munisco12@gmail.com>
2023-02-03 18:55:07 +0100

    working on artisan list dialog

--- src/components/artisans/list/ArtisanDetail.tsx
+++ src/components/artisans/list/ArtisanDetail.tsx
@@ -594,7 +594,7 @@ const ArtisanDetail = () => {
                         </div>
                     </div>
                 </section>
-                <section>
+                <section className='grid place-content-center w-full'>
                     <h3
                         className='text-[2rem]'
                         style={{
@@ -603,6 +603,83 @@ const ArtisanDetail = () => {
                     >
                         Know Your Artisan (KYA)
                     </h3>
+                    <div className='bg-white rounded-2xl grid items-baseline w-[90rem] min-h-[30rem] p-10 text-[1.6rem] relative gap-20'>
+                        <IoMdClose className='absolute right-4 top-4 text-[2rem] cursor-pointer' />
+
+                        <div className='relative h-[14rem] bg-blue-600 w-full mt-10 rounded-lg'>
+                            <img
+                                src='/img/me.jpeg'
+                                alt=''
+                                className='w-[10rem] h-[10rem] border rounded-full border-green-600 object-cover absolute bottom-[-6rem] left-10 object-top'
+                            />
+                        </div>
+                        <div className='mt-20'>
+                            <h2>Validation Result</h2>
+
+                            <div className='border grid mt-5'>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Validation Option
+                                    </p>
+                                    <p className='py-4'>
+                                        Phone Number | (+234) 813238432
+                                    </p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Full Name
+                                    </p>
+                                    <p className='py-4'>Michael Okonkwo</p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Date of Birth
+                                    </p>
+                                    <p className='py-4'>15 May, 1998</p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Phone Number
+                                    </p>
+                                    <p className='py-4'> (+234) 813238432</p>
+                                </div>
+                                <div className='grid grid-cols-2  gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Gender
+                                    </p>
+                                    <p className='py-4'>Male</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button className='btn text-white bg-[#0556E5] border rounded-lg w-[15rem] justify-self-center'>
+                            Ok
+                        </button>
+                    </div>
                 </section>
             </div>
         </>
