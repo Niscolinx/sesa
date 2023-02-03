293601c09be8e2ee138cd3e5d733b04eec76031f
Igboanugwo Collins <munisco12@gmail.com>
2023-02-03 13:36:46 +0100

    working on add artisan

--- src/components/artisans/list/AddArtisan.tsx
+++ src/components/artisans/list/AddArtisan.tsx
@@ -5,9 +5,11 @@ import { getPhotoUrl } from '../../../utils/getPhotoUrl'
 import {BsQuestionCircle} from 'react-icons/bs'
 
 type State = 'Lagos' | 'Imo' | 'Abia' | 'FCT'
+type Gender = 'Male' | 'Female'
 
 const AddArtisan = () => {
     const state: Array<State> = ['Lagos', 'Imo', 'Abia', 'FCT']
+    const gender: Array<Gender> = ['Male', 'Female']
 
     const [toggleStateMenu, setToggleStateMenu] = useState(false)
     const [selectedState, setSelectedState] = useState<State | null>(null)
@@ -187,7 +189,7 @@ const AddArtisan = () => {
                                 className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] cursor-pointer'
                                 onClick={stateMenuToggler}
                             >
-                                {selectedState || (
+                                {selectedGender || (
                                     <span className='text-gray-500'>
                                         Select State
                                     </span>
@@ -202,12 +204,12 @@ const AddArtisan = () => {
 
                         {toggleStateMenu && (
                             <div className='absolute top-[8rem]  left-0 border border-color-primary-light w-[24rem] bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
-                                {state.map((item, index) => (
+                                {gender.map((item, index) => (
                                     <p
                                         className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
                                         key={index}
                                         onClick={() =>
-                                            handleSelectedState(item)
+                                            handleSelectedGender(item)
                                         }
                                     >
                                         {item}
