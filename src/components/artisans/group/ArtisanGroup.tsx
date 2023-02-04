b5a3d843127d8f603277a0c6b48686b87b16ef86
Igboanugwo Collins <munisco12@gmail.com>
2023-02-04 11:32:35 +0100

    working on multiple select

--- src/components/artisans/group/ArtisanGroup.tsx
+++ src/components/artisans/group/ArtisanGroup.tsx
@@ -176,7 +176,7 @@ const ArtisanGroup: FC<{
         })
 
         if (item === 'View Details') {
-            navigate(`/dashboard/artisan/detail/:${id}`)
+            navigate(`/dashboard/artisan/group/:${id}`)
         }
 
         if (item === 'Deactivate') {
