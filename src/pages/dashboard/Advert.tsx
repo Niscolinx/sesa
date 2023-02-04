960447cf5ab6d3fee15eedccdc408b0d71f6b864
Igboanugwo Collins <munisco12@gmail.com>
2023-02-04 18:39:42 +0100

    working on view artisan group

--- src/pages/dashboard/Advert.tsx
+++ src/pages/dashboard/Advert.tsx
@@ -17,27 +17,10 @@ function Advert() {
         <div>
             <h1 className='heading2'>Artisan</h1>
             <div className='rounded-lg mt-[3rem] h-[80vh]'>
-                {artisan ? (
-                    <section>
+              
                         <RenderArtisans />
-                    </section>
-                ) : (
-                    <section className='grid  place-content-center w-full h-full justify-items-center gap-4 bg-white rounded-lg'>
-                        <img src='/icons/admins/errorSvg.svg' alt='' />
-                        <p className='text'>
-                            Ooops you have not added any Artisan yet
-                        </p>
-                        <button
-                            className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg'
-                            onClick={addArtisanHandler}
-                        >
-                            <span>
-                                <IoMdAdd />
-                            </span>{' '}
-                            Add Artisan
-                        </button>
-                    </section>
-                )}
+         
+                
             </div>
         </div>
     )
