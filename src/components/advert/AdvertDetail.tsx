966684e70c5705eec6ae6e64dc071245b8b2d6b7
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 08:31:39 +0100

    working on add advert

--- src/components/advert/AdvertDetail.tsx
+++ src/components/advert/AdvertDetail.tsx
@@ -3,7 +3,10 @@ const AdvertDetail = () => {
     return (
         <div className='grid p-8 bg-white h-[80vh] items-baseline overflow-y-scroll rounded-lg'>
 
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem laboriosam eligendi consequuntur a, totam voluptas at corrupti inventore ut quia placeat? Perferendis facere voluptatem enim sit laboriosam commodi nulla autem.
+
+            <section>
+                <img src="/img/advertHero.png" alt="" />
+            </section>
 
         </div>
     )
