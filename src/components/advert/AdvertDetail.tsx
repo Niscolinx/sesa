7685a4987374bf0c8b34ff41104e0cb12cdbf222
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 08:36:51 +0100

    working on add advert

--- src/components/advert/AdvertDetail.tsx
+++ src/components/advert/AdvertDetail.tsx
@@ -1,16 +1,31 @@
-
 const AdvertDetail = () => {
     return (
         <div className='grid p-8 bg-white h-[80vh] items-baseline overflow-y-scroll rounded-lg'>
-
-
-            <section>
-                <h2 style={{
-                    fontFamily: 'Satoshi-medium'
-                }} className='text-[2rem]'>Advert Preview</h2>
-                <img src="/img/advertHero.png" alt="" className=" h-[40rem] object-cover rounded-lg" />
+            <section className='grid gap-8'>
+                <h2
+                    style={{
+                        fontFamily: 'Satoshi-medium',
+                    }}
+                    className='text-[2rem]'
+                >
+                    Advert Preview
+                </h2>
+                <img
+                    src='/img/advertHero.png'
+                    alt=''
+                    className='h-[40rem] object-cover rounded-lg flex self-stretch'
+                />
+                <div>
+                    <h2
+                        style={{
+                            fontFamily: 'Satoshi-medium',
+                        }}
+                        className='text-[2rem]'
+                    >
+                        Advert Preview
+                    </h2>
+                </div>
             </section>
-
         </div>
     )
 }
