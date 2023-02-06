2b6d663d4302fdd63358b417af29d427d260f397
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 12:42:29 +0100

    working on line chart

--- src/components/charts/ClickRateChart.tsx
+++ src/components/charts/ClickRateChart.tsx
@@ -2,32 +2,32 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 
 const series = [
     {
-        name: 'Series 1',
+        name: 'Click Rate',
         data: [
             { category: 'Mon', value: 14 },
             { category: 'Tue', value: 17 },
             { category: 'Wed', value: 5 },
             { category: 'Thur', value: 12 },
-            { category: 'Fri', value: 11 },
-            { category: 'Sat', value: 10 },
-            { category: 'Sun', value: 8 },
+            { category: 'Fri', value: 16 },
+            { category: 'Sat', value: 13 },
+            { category: 'Sun', value: 18 },
         ],
     },
 ]
 
 export default function ClickRateChart() {
     return (
-        <LineChart width={500} height={300}>
+        <LineChart width={1000} height={300}>
             <XAxis
                 dataKey='category'
                 type='category'
                 allowDuplicatedCategory={false}
             />
-            <YAxis dataKey='value' />
+            <YAxis dataKey='value' fill='#fff' />
             <Tooltip />
-            <Legend />
             {series.map((s) => (
                 <Line
+                    fill='#08D231'
                     dataKey='value'
                     data={s.data}
                     name={s.name}
