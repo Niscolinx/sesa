8910552f52a9d20fabff1540f7da84c3c68d40e6
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 18:47:03 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -1,31 +1,6 @@
-import React, { FormEvent, useRef, useState } from 'react'
-import { IoMdClose } from 'react-icons/io'
-import { MultipleSelect } from '../UI/Select'
 
 const SOSDetails = () => {
-    const [selectedEstates, setSelectedEstates] = useState<string[]>([])
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-    }
-
-    const dialogRef = useRef<HTMLDialogElement | null>(null)
-
-    const handleClose = () => {
-        if (dialogRef.current) {
-            dialogRef.current.close()
-        }
-    }
-
-    const handleOpen = () => {
-        if (dialogRef.current) {
-            dialogRef.current.showModal()
-        }
-    }
-
-    const confirmAddedSOS = () => {
-        console.log('hellow ')
-    }
+  
 
     return (
         
