b59991a9f1069fc7b20b2492dbe4c6bb80062ffa
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 18:58:36 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -1,10 +1,17 @@
 const SOSDetails = () => {
     return (
-        <div className='grid overflow-y-scroll rounded-lg'>
+        <div className='grid overflow-y-scroll '>
             <p className='text-[2rem] font-Satoshi-Medium pb-8'>SOS Details</p>
-            <div className='bg-white h-[80vh]'>
-                <div className="p-8">
-                    <p className="text-[1.6rem]">Details</p>
+            <div className='bg-white h-[80vh] rounded-lg'>
+                <div className='p-8'>
+                    <p className='text-[1.6rem]'>Details</p>
+
+                    <div className="grid grid-cols-2">
+                    <div>
+                        <p>Name :</p>
+                        <p>Adiyan Police Station</p>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
