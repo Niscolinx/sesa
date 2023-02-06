5fa494f78db317fd3b45cc3bc88923bd2e787462
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 19:20:16 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -11,17 +11,103 @@ const SOSDetails = () => {
                     />
                     <div className='flex justify-between max-w-[70rem] '>
                         <div className='grid gap-4'>
-                            <div className='flex items-center justify-start gap-4'>
+                            <div className='flex items-center justify-start gap-4 justify-items-start'>
                                 <p className='font-Satoshi-Light'>Name :</p>
                                 <p>Adiyan Police Station</p>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Email :</p>
+                                <p>Adiyan@gmail.com</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Felix Drive, Lekki Lagos</p>
+                            </div>
+                        </div>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 1 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 2 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 3 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='grid relative border-b pb-4'>
+                    <img
+                        src='/img/closeIcon.svg'
+                        alt=''
+                        className='self-end justify-self-end'
+                    />
+                    <div className='flex justify-between max-w-[70rem] '>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4 justify-items-start'>
                                 <p className='font-Satoshi-Light'>Name :</p>
                                 <p>Adiyan Police Station</p>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Email :</p>
+                                <p>Adiyan@gmail.com</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Felix Drive, Lekki Lagos</p>
+                            </div>
+                        </div>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 1 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 2 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 3 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='grid relative border-b pb-4'>
+                    <img
+                        src='/img/closeIcon.svg'
+                        alt=''
+                        className='self-end justify-self-end'
+                    />
+                    <div className='flex justify-between max-w-[70rem] '>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4 justify-items-start'>
                                 <p className='font-Satoshi-Light'>Name :</p>
                                 <p>Adiyan Police Station</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Email :</p>
+                                <p>Adiyan@gmail.com</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Felix Drive, Lekki Lagos</p>
                             </div>
                         </div>
                         <div className='grid gap-4'>
