2be56b63a78a1c1e3c5bc1b45adfc25d52a1c871
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 19:04:54 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -3,7 +3,7 @@ const SOSDetails = () => {
         <div className='grid overflow-y-scroll '>
             <p className='text-[2rem] font-Satoshi-Medium pb-8'>SOS Details</p>
             <div className='bg-white h-[80vh] rounded-lg p-8 text-[1.6rem]'>
-                <div>
+                <div className="flex justify-between max-w-[70rem] mx-auto">
                     <div className='grid'>
                         <div className='flex items-center justify-start gap-4'>
                             <p className='font-Satoshi-Light'>Name :</p>
@@ -20,16 +20,22 @@ const SOSDetails = () => {
                     </div>
                     <div className='grid'>
                         <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Phone Number 1 :</p>
-                            <p>Adiyan Police Station</p>
+                            <p className='font-Satoshi-Light'>
+                                Phone Number 1 :
+                            </p>
+                            <p>+234 803 587 6489</p>
                         </div>
                         <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Phone Number 2</p>
-                            <p>Adiyan Police Station</p>
+                            <p className='font-Satoshi-Light'>
+                                Phone Number 2 :
+                            </p>
+                            <p>+234 803 587 6489</p>
                         </div>
                         <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Phone Number 3</p>
-                            <p>Adiyan Police Station</p>
+                            <p className='font-Satoshi-Light'>
+                                Phone Number 3 :
+                            </p>
+                            <p>+234 803 587 6489</p>
                         </div>
                     </div>
                 </div>
