6beedad2d844a17a6ee5659ecd7f34dbd65979e5
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 19:11:00 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -3,40 +3,46 @@ const SOSDetails = () => {
         <div className='grid overflow-y-scroll '>
             <p className='text-[2rem] font-Satoshi-Medium pb-8'>SOS Details</p>
             <div className='bg-white h-[80vh] rounded-lg p-8 text-[1.6rem]'>
-                <div className="flex justify-between max-w-[70rem] mx-auto relative">
-                    <img src="/img/closeIcon.svg" alt="" className="absolute left-0 right-0" />
-                    <div className='grid'>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Name :</p>
-                            <p>Adiyan Police Station</p>
+                <div className='grid relative border-b pb-4'>
+                    <img
+                        src='/img/closeIcon.svg'
+                        alt=''
+                        className='self-end justify-self-end'
+                    />
+                    <div className='flex justify-between max-w-[70rem] '>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Adiyan Police Station</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Adiyan Police Station</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>Name :</p>
+                                <p>Adiyan Police Station</p>
+                            </div>
                         </div>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Name :</p>
-                            <p>Adiyan Police Station</p>
-                        </div>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>Name :</p>
-                            <p>Adiyan Police Station</p>
-                        </div>
-                    </div>
-                    <div className='grid'>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>
-                                Phone Number 1 :
-                            </p>
-                            <p>+234 803 587 6489</p>
-                        </div>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>
-                                Phone Number 2 :
-                            </p>
-                            <p>+234 803 587 6489</p>
-                        </div>
-                        <div className='flex items-center justify-start gap-4'>
-                            <p className='font-Satoshi-Light'>
-                                Phone Number 3 :
-                            </p>
-                            <p>+234 803 587 6489</p>
+                        <div className='grid gap-4'>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 1 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 2 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
+                            <div className='flex items-center justify-start gap-4'>
+                                <p className='font-Satoshi-Light'>
+                                    Phone Number 3 :
+                                </p>
+                                <p>+234 803 587 6489</p>
+                            </div>
                         </div>
                     </div>
                 </div>
