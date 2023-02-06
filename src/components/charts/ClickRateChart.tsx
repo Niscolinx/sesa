61dfaac1b61af59a850e4b996097580cc7d0822d
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 12:32:20 +0100

    working on line chart

--- src/components/charts/ClickRateChart.tsx
+++ src/components/charts/ClickRateChart.tsx
@@ -1,12 +1,4 @@
-import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-} from 'recharts'
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 
 const series = [
     {
--- src/components/charts/WalletBarChart.tsx
+++ src/components/charts/WalletBarChart.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts'
 
 const data = [
