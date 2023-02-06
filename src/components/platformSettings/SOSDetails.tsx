2686d7dc4c279f0a7af1d0dc2f80fa5541d39e6d
Igboanugwo Collins <munisco12@gmail.com>
2023-02-06 19:03:44 +0100

    working on add sos

--- src/components/platformSettings/SOSDetails.tsx
+++ src/components/platformSettings/SOSDetails.tsx
@@ -3,18 +3,34 @@ const SOSDetails = () => {
         <div className='grid overflow-y-scroll '>
             <p className='text-[2rem] font-Satoshi-Medium pb-8'>SOS Details</p>
             <div className='bg-white h-[80vh] rounded-lg p-8 text-[1.6rem]'>
-                <div className='grid'>
-                    <div className='flex items-center justify-start gap-4'>
-                        <p className="font-Satoshi-Light">Name :</p>
-                        <p>Adiyan Police Station</p>
+                <div>
+                    <div className='grid'>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Name :</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Name :</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Name :</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
                     </div>
-                    <div className='flex items-center justify-start gap-4'>
-                        <p className="font-Satoshi-Light">Name :</p>
-                        <p>Adiyan Police Station</p>
-                    </div>
-                    <div className='flex items-center justify-start gap-4'>
-                        <p className="font-Satoshi-Light">Name :</p>
-                        <p>Adiyan Police Station</p>
+                    <div className='grid'>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Phone Number 1 :</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Phone Number 2</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
+                        <div className='flex items-center justify-start gap-4'>
+                            <p className='font-Satoshi-Light'>Phone Number 3</p>
+                            <p>Adiyan Police Station</p>
+                        </div>
                     </div>
                 </div>
             </div>
