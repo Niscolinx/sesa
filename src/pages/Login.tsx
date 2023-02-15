a54045d04538fdd39c12ef35a70d11237ebda080
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 16:45:52 +0100

    working on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,33 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useState } from 'react'
 
 const Login = () => {
+    interface Inputs {
+        email: string
+        password: string
+    }
+    const [inputs, setInputs] = useState<Inputs>({
+        email: '',
+        password: '',
+    })
+
     const handleLogin = (e: FormEvent) => {
         e.preventDefault()
     }
     return (
         <div className='flex h-screen overflow-hidden'>
-            <form onSubmit={handleLogin} className='basis-1/2 grid place-content-center gap-10'>
+            <form
+                onSubmit={handleLogin}
+                className='basis-1/2 grid place-content-center gap-10'
+            >
                 <div>
-                    <h1 className='text-[4.5rem] font-Satoshi-ExtraBold'>Welcome back!</h1>
+                    <h1 className='text-[4.5rem] font-Satoshi-ExtraBold'>
+                        Welcome back!
+                    </h1>
                     <p>Kindly enter your details</p>
                 </div>
                 <div className='grid gap-8'>
                     <div className='w-full grid gap-4'>
-                        <label
-                            htmlFor='email'
-                            className='font-semibold'
-                        >
+                        <label htmlFor='email' className='font-semibold'>
                             Email
                         </label>
                         <input
@@ -27,10 +38,7 @@ const Login = () => {
                         />
                     </div>
                     <div className='w-full grid gap-4'>
-                        <label
-                            htmlFor='password'
-                            className='font-semibold'
-                        >
+                        <label htmlFor='password' className='font-semibold'>
                             Password
                         </label>
                         <input
@@ -46,8 +54,7 @@ const Login = () => {
                 </button>
             </form>
             <figure>
-
-            <img src='/img/hero.png' alt='' />
+                <img src='/img/hero.png' alt='' />
             </figure>
         </div>
     )
