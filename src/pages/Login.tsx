0cd84689d45e8d5d3401734187ee7b0c2f798ca6
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 16:31:57 +0100

    working on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,13 +7,45 @@ const Login = () => {
     return (
         <div className='flex'>
             <form onSubmit={handleLogin}>
-            <div>
-                <h1>Welcome back!</h1>
-                <p>Kindly enter your details</p>
-            </div>
-            <div></div>
-            <div></div>
-
+                <div>
+                    <h1>Welcome back!</h1>
+                    <p>Kindly enter your details</p>
+                </div>
+                <div>
+                    <div className='w-full grid gap-4'>
+                        <label
+                            htmlFor='status'
+                            className='text-[1.4rem] font-semibold'
+                        >
+                            Transaction Type
+                        </label>
+                        <input
+                            disabled
+                            type='text'
+                            required
+                            id='status'
+                            className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-green-500 disabled:cursor-not-allowed'
+                            value={'Credit'}
+                        />
+                    </div>
+                    <div className='w-full grid gap-4'>
+                        <label
+                            htmlFor='status'
+                            className='text-[1.4rem] font-semibold'
+                        >
+                            Transaction Type
+                        </label>
+                        <input
+                            disabled
+                            type='text'
+                            required
+                            id='status'
+                            className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-green-500 disabled:cursor-not-allowed'
+                            value={'Credit'}
+                        />
+                    </div>
+                </div>
+                <div></div>
             </form>
             <img src='/img/hero.png' alt='' />
         </div>
