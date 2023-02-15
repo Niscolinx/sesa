0c5212b299d25014806b99aef03fb4a2251f2cf8
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 19:02:26 +0100

    working on security company ui

--- src/components/SuperAdmin/overview/OverviewCard.tsx
+++ src/components/SuperAdmin/overview/OverviewCard.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
 
 type OverviewCard = {
-    iconUrl: string;
-    title: string;
+    iconUrl: string
+    title: string
     number: number
     bgColor: string
     textColor: string
@@ -10,17 +10,36 @@ type OverviewCard = {
     arrow?: string
 }
 
-const OverviewCard:FC<OverviewCard> = ({iconUrl, title, number, bgColor, percent, arrow, textColor = 'black'}) => {
+const OverviewCard: FC<OverviewCard> = ({
+    iconUrl,
+    title,
+    number,
+    bgColor,
+    percent,
+    arrow,
+    textColor = 'black',
+}) => {
     return (
         <div className={`overviewCard ${bgColor}`}>
-            <img
-                src={iconUrl}
-                alt=''
-                className='overviewCard__img'
-            />
-            <div className='overviewCard__content'>
+            <img src={iconUrl} alt='' className='overviewCard__img' />
+            <div
+                className={`overviewCard__content ${percent ? 'relative' : ''}`}
+            >
                 <p className='overviewCard__title'>{title}</p>
-                <p className={`overviewCard__number ${textColor}`}>{number.toLocaleString()}</p>
+                <p className={`overviewCard__number ${textColor}`}>
+                    {number.toLocaleString()}
+                </p>
+
+                {percent && (
+                    <div className='absolute bottom-0 right-0'>
+                        <img
+                            src={arrow}
+                            alt=''
+                            className='overviewCard__arrow'
+                        />
+                        <p className='text-green-600'>{percent}%</p>
+                    </div>
+                )}
             </div>
         </div>
     )
