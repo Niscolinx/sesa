9bad5854c919357b27407a8d82154c403aab493b
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 16:59:21 +0100

    working on login

--- src/pages/SecurityCompany/Dashboard.tsx
+++ src/pages/SecurityCompany/Dashboard.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Outlet } from 'react-router'
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+import Header from '../../components/SuperAdmin/dashboard/Header'
+import Sidebar from '../../components/SuperAdmin/dashboard/Sidebar'
+import Modal from '../../components/SuperAdmin/Modal'
+
+function Dashboard() {
+    return (
+        <Modal>
+            <div className='dashboard'>
+                <Sidebar />
+                <div className='dashboard__container'>
+                    <Header
+                        fullName='Collins Igboanugwo'
+                        role='Super Admin'
+                        imgUrl='/img/me.jpeg'
+                    />
+
+                    <div className='p-10 overflow-scroll max-h-[90vh]'>
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    )
 }
 
 export default Dashboard
