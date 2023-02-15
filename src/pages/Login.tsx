9e487f0ceab9ebe33740d21ec126efe1edcd488b
Igboanugwo Collins <munisco12@gmail.com>
2023-02-15 16:29:23 +0100

    working on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 const Login = () => {
-  return (
-    <div className=''>
-
-        <img src="/img/hero.png" alt="" />
-    </div>
-  )
+    const handleLogin = (e:FormEvent) => {
+        e.preventDefault()
+    }
+    return (
+        <div className='flex'>
+            <form onSubmit={handleLogin}></form>
+            <img src='/img/hero.png' alt='' />
+        </div>
+    )
 }
 
 export default Login
