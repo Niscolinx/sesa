fb6f6587da11836763992d2bc08133f42b667aa7
Igboanugwo Collins <munisco12@gmail.com>
2023-02-20 17:23:42 +0100

    working on view security guard

--- src/pages/SecurityCompany/dashboard/Estates/EstateWorkRate.tsx
+++ src/pages/SecurityCompany/dashboard/Estates/EstateWorkRate.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import OverviewChart from '../../../../components/SuperAdmin/charts/OverviewChart'
+import WalletBarChart from '../../../../components/SuperAdmin/charts/WalletBarChart'
 
 const EstateWorkRate = () => {
     return (
         <div>
             <h1 className='heading2'>Estate Work Rate</h1>
-            <div className='mt-8 grid gap-8'>
+            <div className='mt-8 grid gap-8 bg-white p-8 rounded-lg'>
                 <WalletBarChart />
+
+                <div className='overviewChart__box'>
+                    <OverviewChart />
+
+                    <div className='overviewChart__label'>
+                        <p className='text-[3.4rem] font-bold relative'>
+                            45
+                            <span>%</span>
+                        </p>
+                        <p className='text-[1rem] max-w-[9.8rem]'>
+                            Resident Wallet
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
