d8c69e5512fb2861d332bf01c43bec82decb3860
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 09:51:31 +0100

    working on view company

--- src/pages/SecurityCompany/dashboard/company/AddSecurity/Inputs.tsx
+++ src/pages/SecurityCompany/dashboard/company/AddSecurity/Inputs.tsx
@@ -1,4 +1,3 @@
-
 export const PhoneNumber = () => {
     return (
         <div className='grid gap-4'>
@@ -72,23 +71,102 @@ export const NIN_Number = () => {
 export const DriversLicence = () => {
     return (
         <div className='grid gap-4'>
-            <label htmlFor='phoneNumber' className='text-[1.4rem] font-medium'>
-                BVN (Number)
-            </label>
+            <div>
+                <label
+                    htmlFor='phoneNumber'
+                    className='text-[1.4rem] font-medium'
+                >
+                    License (Number)
+                </label>
 
-            <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
-                <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
-                    <option value='234'>+234</option>
-                </select>
-                <input
-                    required
-                    type='number'
-                    inputMode='numeric'
-                    id='phoneNumber'
-                    placeholder='BVN (Number)'
-                    className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
-                />
+                <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                    <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                        <option value='234'>+234</option>
+                    </select>
+                    <input
+                        required
+                        type='number'
+                        inputMode='numeric'
+                        id='phoneNumber'
+                        placeholder='BVN (Number)'
+                        className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                    />
+                </div>
+            </div>
+            <div>
+                <label
+                    htmlFor='phoneNumber'
+                    className='text-[1.4rem] font-medium'
+                >
+                    Date of Birth
+                </label>
+
+                <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                    <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                        <option value='234'>+234</option>
+                    </select>
+                    <input
+                        required
+                        type='date'
+                        inputMode='numeric'
+                        id='phoneNumber'
+                        placeholder='BVN (Number)'
+                        className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                    />
+                </div>
             </div>
         </div>
     )
 }
+export const DriversLicence = () => {
+    return (
+        <div className='grid gap-4'>
+            <div>
+                <label
+                    htmlFor='phoneNumber'
+                    className='text-[1.4rem] font-medium'
+                >
+                    License (Number)
+                </label>
+
+                <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                    <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                        <option value='234'>+234</option>
+                    </select>
+                    <input
+                        required
+                        type='number'
+                        inputMode='numeric'
+                        id='phoneNumber'
+                        placeholder='BVN (Number)'
+                        className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                    />
+                </div>
+            </div>
+            <div>
+                <label
+                    htmlFor='phoneNumber'
+                    className='text-[1.4rem] font-medium'
+                >
+                    Date of Birth
+                </label>
+
+                <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                    <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                        <option value='234'>+234</option>
+                    </select>
+                    <input
+                        required
+                        type='date'
+                        inputMode='numeric'
+                        id='phoneNumber'
+                        placeholder='BVN (Number)'
+                        className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
