949c0d8eaa2a5ff9fc5b8f14e8c50b4190eeffec
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 19:14:17 +0100

    working on compose message

--- src/pages/SecurityCompany/dashboard/Messages/ViewMessage.tsx
+++ src/pages/SecurityCompany/dashboard/Messages/ViewMessage.tsx
@@ -120,45 +120,39 @@ const ViewMessage = () => {
         <>
             <ToastContainer />
 
-        
-
             <dialog className='dialog' ref={dialogRef}>
                 <section className='grid place-content-center w-full h-[100vh]'>
                     <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8'>
-                        
-                            <>
-                                <img
-                                    src='/icons/admins/modalWarning.svg'
-                                    alt=''
-                                />
-                                <p className='text-[1.6rem]'>
-                                    Are you sure you want to delete this
-                                    Security Guard
-                                </p>
+                        <>
+                            <img src='/icons/admins/modalWarning.svg' alt='' />
+                            <p className='text-[1.6rem]'>
+                                Are you sure you want to delete this Security
+                                Guard
+                            </p>
 
-                                <div className='flex w-full justify-center gap-8'>
-                                    <button
-                                        className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
-                                        onClick={() =>
-                                            handleCloseDeleteOrDeactivateDialog()
-                                        }
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
-                                        onClick={handleDeleteSecurityGuard}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            </>
+                            <div className='flex w-full justify-center gap-8'>
+                                <button
+                                    className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
+                                    onClick={() =>
+                                        handleCloseDeleteOrDeactivateDialog()
+                                    }
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
+                                    onClick={handleDeleteSecurityGuard}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </>
                     </div>
                 </section>
             </dialog>
-            <main className='bg-white grid gap-10 rounded-lg py-10'>
+            <main className='bg-white grid gap-10 rounded-lg '>
                 <h2 className='heading'>Messages</h2>
-                <section className='grid p-8 bg-white items-baseline rounded-lg'>
+                <section className='grid p-8 bg-white items-baseline rounded-lg py-10'>
                     <div className='flex justify-between items-center'>
                         <div className='flex gap-8 items-center'>
                             <label
