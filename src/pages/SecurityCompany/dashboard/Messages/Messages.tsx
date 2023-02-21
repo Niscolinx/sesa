5384561b7c61682fc2d7a7663d79024f67a2e77e
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 15:40:47 +0100

    wokring on messages

--- src/pages/SecurityCompany/dashboard/Messages/Messages.tsx
+++ src/pages/SecurityCompany/dashboard/Messages/Messages.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
 import { useNavigate } from 'react-router'
-import RenderArtisans from '../../../components/SuperAdmin/artisans/RenderArtisans'
 
 function Messages() {
     const navigate = useNavigate()
 
-    const [artisan, setArtisan] = useState(false)
+    const [isMessage, setIsMessage] = useState(false)
 
     const addArtisanHandler = () => {
-        setArtisan(true)
+        setIsMessage(false)
         //navigate('/superAdmin/artisan/add')
     }
 
@@ -17,9 +16,9 @@ function Messages() {
         <div>
             <h1 className='heading2'>Artisan</h1>
             <div className='rounded-lg mt-[3rem] h-[80vh]'>
-                {artisan ? (
+                {isMessage ? (
                     <section>
-                        <RenderArtisans />
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi maxime, ipsum exercitationem reiciendis adipisci quidem maiores est voluptas officiis soluta nam earum optio harum quo explicabo eum laboriosam nostrum quas.
                     </section>
                 ) : (
                     <section className='grid  place-content-center w-full h-full justify-items-center gap-4 bg-white rounded-lg'>
@@ -34,7 +33,7 @@ function Messages() {
                             <span>
                                 <IoMdAdd />
                             </span>{' '}
-                            Add Artisan
+                            Add Message
                         </button>
                     </section>
                 )}
