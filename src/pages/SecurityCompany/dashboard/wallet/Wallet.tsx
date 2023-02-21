94cc3673f04fed9400f47cf7e90083fcf5162704
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 21:54:43 +0100

    working on wallet

--- src/pages/SecurityCompany/dashboard/wallet/Wallet.tsx
+++ src/pages/SecurityCompany/dashboard/wallet/Wallet.tsx
@@ -295,32 +295,32 @@ function Wallet() {
                                         htmlFor='description'
                                         className='text-[1.4rem] font-semibold'
                                     >
-                                        Description
+                                        Card Number
                                     </label>
 
                                     <input
-                                        type='text'
+                                        type='number'
                                         required
                                         id='description'
                                         className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem]'
                                     />
                                 </div>
-
                                 <div className='w-full grid gap-4'>
-                                    <label
-                                        htmlFor='commissionWalletAttachment'
-                                        className='text-[1.4rem] font-semibold'
-                                    >
-                                        Attach An Item
-                                    </label>
+                                    <div>
+                                        <label
+                                            htmlFor='description'
+                                            className='text-[1.4rem] font-semibold'
+                                        >
+                                            Card Number
+                                        </label>
 
-                                    <input
-                                        type='file'
-                                        id='commissionWalletAttachment'
-                                        name='commissionWalletAttachment'
-                                        required
-                                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] appearance-none'
-                                    />
+                                        <input
+                                            type='number'
+                                            required
+                                            id='description'
+                                            className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem]'
+                                        />
+                                    </div>
                                 </div>
 
                                 <button className='btn bg-[#0556E5] text-white rounded-lg py-4 place-self-start w-[15rem]'>
