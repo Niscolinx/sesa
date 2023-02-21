5180ca5bde0162bec13ab1ed15f087c723646f7a
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 09:48:31 +0100

    working on view company

--- src/pages/SecurityCompany/dashboard/company/AddSecurity/Inputs.tsx
+++ src/pages/SecurityCompany/dashboard/company/AddSecurity/Inputs.tsx
@@ -22,6 +22,29 @@ export const PhoneNumber = () => {
         </div>
     )
 }
+export const BVN_Number = () => {
+    return (
+        <div className='grid gap-4'>
+            <label htmlFor='phoneNumber' className='text-[1.4rem] font-medium'>
+                BVN (Number)
+            </label>
+
+            <div className='flex text-[1.6rem] gap-4 h-[5rem]'>
+                <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
+                    <option value='234'>+234</option>
+                </select>
+                <input
+                    required
+                    type='number'
+                    inputMode='numeric'
+                    id='phoneNumber'
+                    placeholder='BVN (Number)'
+                    className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
+                />
+            </div>
+        </div>
+    )
+}
 export const NIN_Number = () => {
     return (
         <div className='grid gap-4'>
@@ -45,7 +68,8 @@ export const NIN_Number = () => {
         </div>
     )
 }
-export const BVN_Number = () => {
+
+export const DriversLicence = () => {
     return (
         <div className='grid gap-4'>
             <label htmlFor='phoneNumber' className='text-[1.4rem] font-medium'>
