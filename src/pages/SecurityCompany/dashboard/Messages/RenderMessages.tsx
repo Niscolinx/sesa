c5ab59e0e1791eee1a574d02f89bf12b8d92951c
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 16:33:07 +0100

    wokring on messages

--- src/pages/SecurityCompany/dashboard/Messages/RenderMessages.tsx
+++ src/pages/SecurityCompany/dashboard/Messages/RenderMessages.tsx
@@ -14,11 +14,10 @@ export interface Message {
     transmissionChannel: string
     subject: string
     description: string
-    status: 'Sent' | 'Pending'  
+    status: 'Sent' | 'Pending'
 }
 
 const recipients = ['Thomas Nwaje', 'Solomon Nwaje']
-
 
 export const MESSAGE_LIST: Message[] = Array.from({ length: 10 }).map(
     (_, i) => ({
@@ -155,157 +154,151 @@ function RenderedMessages() {
     }
 
     return (
-            <div className='bg-color-white rounded-lg border min-w-[112rem]'>
-                <div className='grid text-[1.6rem] border rounded-lg'>
-                    <div className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
-                        <p className=' font-bold'>
-                            Messages <span>(200)</span>
-                        </p>
-                        <div className='relative flex items-center'>
-                            <img
-                                src='/icons/admins/search.svg'
-                                alt=''
-                                className='absolute left-4 text-[4rem]'
-                            />
-                            <input
-                                type='text'
-                                value={search}
-                                onChange={handleSearch}
-                                placeholder='Search Parameters'
-                                className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
-                            />
-                        </div>
-                        <div className='relative flex items-center'>
-                            <select className=' cursor-pointer w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'>
-                                <option hidden value=''>
-                                    Sort By
-                                </option>
-                                <option value='date'>date</option>
-                                <option value='alpha'>Alpha</option>
-                            </select>
-                            <GrDown className='absolute right-4 text-[1.3rem]' />
-                        </div>
-
-                        <button
-                            className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'
-                            onClick={composeMessageHandler}
-                        >
-                            <span>
-                                <IoMdAdd />
-                            </span>{' '}
-                            <p>compose Message</p>
-                        </button>
+        <div className='bg-color-white rounded-lg border min-w-[112rem]'>
+            <div className='grid text-[1.6rem] border rounded-lg'>
+                <div className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
+                    <p className=' font-bold'>
+                        Messages <span>(200)</span>
+                    </p>
+                    <div className='relative flex items-center'>
+                        <img
+                            src='/icons/admins/search.svg'
+                            alt=''
+                            className='absolute left-4 text-[4rem]'
+                        />
+                        <input
+                            type='text'
+                            value={search}
+                            onChange={handleSearch}
+                            placeholder='Search Parameters'
+                            className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
+                        />
+                    </div>
+                    <div className='relative flex items-center'>
+                        <select className=' cursor-pointer w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'>
+                            <option hidden value=''>
+                                Sort By
+                            </option>
+                            <option value='date'>date</option>
+                            <option value='alpha'>Alpha</option>
+                        </select>
+                        <GrDown className='absolute right-4 text-[1.3rem]' />
                     </div>
 
-                    
+                    <button
+                        className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'
+                        onClick={composeMessageHandler}
+                    >
+                        <span>
+                            <IoMdAdd />
+                        </span>{' '}
+                        <p>compose Message</p>
+                    </button>
+                </div>
 
-                        <div className='grid gap-8 mt-8 p-8'>
-                            {slicedPages && slicedPages.length > 0 ? (
-                                React.Children.toArray(
-                                    slicedPages[paginate.index].map(
-                                        ({
-                                            id,
-                                            estateName,
-                                            noOfSecurityGuards,
-                                            address,
-                                        }) => {
-                                            return (
-                                                <div className='grid justify-between border-b grid-cols-4 items-center gap-8 '>
-                                                    <p className='flex items-center gap-4'>
-                                                        <input
-                                                            type='checkbox'
-                                                            className='cursor-pointer'
-                                                        />
-                                                        <label htmlFor='file'>
-                                                            {estateName}
-                                                        </label>
-                                                    </p>
-                                                    <p>{address}</p>
-                                                    <p>{noOfSecurityGuards}</p>
-                                                    <button
-                                                        className='text-color-primary text-left'
-                                                        onClick={() =>
-                                                            detailsHandler(id)
-                                                        }
-                                                    >
-                                                        View Details
-                                                    </button>
-                                                </div>
-                                            )
-                                        }
-                                    )
-                                )
-                            ) : (
-                                <div>
-                                    <div className='relative'>
-                                        <div className='absolute w-full grid place-content-center'>
-                                            <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
-                                        </div>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                    <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
-                        <div className='flex gap-8 items-center'>
-                            <p>View</p>
-                            <select
-                                name=''
-                                id=''
-                                className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
-                                onChange={handleItemsPerPage}
-                            >
-                                {itemsPerPageArr.map((item, index) => (
-                                    <option
-                                        value={item}
-                                        key={index}
-                                        selected={item === itemsPerPage}
-                                        className='capitalize cursor-pointer bg-white'
-                                    >
-                                        {item}
-                                    </option>
-                                ))}
-                            </select>
-                            <p className='text'>List per page</p>
-                        </div>
-                        <ul className='flex items-center gap-5 ml-10'>
-                            <HiOutlineChevronLeft
-                                onClick={handlePrev}
-                                className='cursor-pointer'
-                            />
-
-                            {slicedPages?.map((item, index) => {
-                                return (
-                                    <li key={index}>
-                                        {index + 1 === currentPage ? (
-                                            <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
-                                                {index + 1}
-                                            </span>
-                                        ) : (
-                                            <span
-                                                className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
-                                                onClick={(e) =>
-                                                    jumpToPage(e, index)
+                <div className='grid gap-8 mt-8 p-8'>
+                    {slicedPages && slicedPages.length > 0 ? (
+                        React.Children.toArray(
+                            slicedPages[paginate.index].map(
+                                ({
+                                    id,
+                                    estateName,
+                                    noOfSecurityGuards,
+                                    address,
+                                }) => {
+                                    return (
+                                        <div className='grid justify-between border-b grid-cols-4 items-center gap-8 '>
+                                            <p className='flex items-center gap-4'>
+                                                <input
+                                                    type='checkbox'
+                                                    className='cursor-pointer'
+                                                />
+                                                <label htmlFor='file'>
+                                                    {estateName}
+                                                </label>
+                                            </p>
+                                            <p>{address}</p>
+                                            <p>{noOfSecurityGuards}</p>
+                                            <button
+                                                className='text-color-primary text-left'
+                                                onClick={() =>
+                                                    detailsHandler(id)
                                                 }
                                             >
-                                                {index + 1}
-                                            </span>
-                                        )}
-                                    </li>
-                                )
-                            })}
+                                                View Details
+                                            </button>
+                                        </div>
+                                    )
+                                }
+                            )
+                        )
+                    ) : (
+                        <div>
+                            <div className='relative'>
+                                <div className='absolute w-full grid place-content-center'>
+                                    <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
+                <div className='flex gap-8 items-center'>
+                    <p>View</p>
+                    <select
+                        name=''
+                        id=''
+                        className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
+                        onChange={handleItemsPerPage}
+                    >
+                        {itemsPerPageArr.map((item, index) => (
+                            <option
+                                value={item}
+                                key={index}
+                                selected={item === itemsPerPage}
+                                className='capitalize cursor-pointer bg-white'
+                            >
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                    <p className='text'>List per page</p>
+                </div>
+                <ul className='flex items-center gap-5 ml-10'>
+                    <HiOutlineChevronLeft
+                        onClick={handlePrev}
+                        className='cursor-pointer'
+                    />
 
-                            {/* <li className='grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'>
+                    {slicedPages?.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                {index + 1 === currentPage ? (
+                                    <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
+                                        {index + 1}
+                                    </span>
+                                ) : (
+                                    <span
+                                        className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
+                                        onClick={(e) => jumpToPage(e, index)}
+                                    >
+                                        {index + 1}
+                                    </span>
+                                )}
+                            </li>
+                        )
+                    })}
+
+                    {/* <li className='grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'>
                         {totalPage}
                     </li> */}
-                            <HiOutlineChevronRight
-                                onClick={handleNext}
-                                className='cursor-pointer'
-                            />
-                        </ul>
-                    </footer>
-                </div>
-        
+                    <HiOutlineChevronRight
+                        onClick={handleNext}
+                        className='cursor-pointer'
+                    />
+                </ul>
+            </footer>
         </div>
     )
 }
