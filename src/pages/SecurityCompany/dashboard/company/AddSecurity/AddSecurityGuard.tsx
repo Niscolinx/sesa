e667019f6caf5e42a2ccc2acd3ccdd6f117811fb
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 11:38:59 +0100

    working on view company

--- src/pages/SecurityCompany/dashboard/company/AddSecurity/AddSecurityGuard.tsx
+++ src/pages/SecurityCompany/dashboard/company/AddSecurity/AddSecurityGuard.tsx
@@ -284,18 +284,25 @@ const AddSecurityGuard = () => {
                                     Guard
                                 </p>
 
+                                <p className='font-bold' style={{
+                                    fontFamily: 'Satoshi-Medium',
+                                }}>
+                                    Do you want to open a bank account for this
+                                    Security Guard?
+                                </p>
+
                                 <div className='flex w-full justify-center gap-8'>
                                     <button
                                         className='btn bg-white text-[#0556E5] border-[#0556E5] border rounded-lg w-[15rem]'
                                         onClick={() => handleClose()}
                                     >
-                                        Cancel
+                                        Skip, Later
                                     </button>
                                     <button
                                         className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
                                         onClick={() => handleClose()}
                                     >
-                                        Ok
+                                        Open an Account
                                     </button>
                                 </div>
                             </div>
