0fe0c89f1a24e162c0e0442bf3ef6afdb1f9d751
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 05:22:27 +0100

    working on view company

--- src/pages/SecurityCompany/dashboard/company/Overview.tsx
+++ src/pages/SecurityCompany/dashboard/company/Overview.tsx
@@ -1,9 +1,308 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { CgSpinnerTwo } from 'react-icons/cg'
+import { GrDown } from 'react-icons/gr'
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi'
+import { Overview } from '../SecurityCompanyOverview'
+
+interface AttendanceReport {
+    id: number
+    guardName: string
+    phoneNumber: string
+    date: string
+    guardCode: number
+    clockInCount: number
+    clockOutCount: number
+}
+
+const ATTENDANCE_REPORT_DATA: AttendanceReport[] = Array.from({
+    length: 20,
+}).map((_, i) => ({
+    id: i,
+    guardName: 'John Doe',
+    date: '12-May-2023',
+    phoneNumber: '+2347024954270',
+    guardCode: Math.floor(Math.random() * 3000 + 1000),
+    clockInCount: Math.floor(Math.random() * 10 + 1),
+    clockOutCount: Math.floor(Math.random() * 10 + 1),
+}))
+
 
 const Overview = () => {
-  return (
-    <div>Overview</div>
-  )
+    const [fetchedAttendanceReportData, setFetchedAttendanceReportData] =
+        useState<AttendanceReport[]>([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setFetchedAttendanceReportData(ATTENDANCE_REPORT_DATA)
+        }, 1000)
+    }, [])
+
+    interface Paginate {
+        index: number
+        currentPage: number
+        itemsPerPage: number
+        totalPage: number
+        slicedPages: AttendanceReport[][] | null
+    }
+
+    const itemsPerPageArr = [2, 4, 6, 8]
+    const perPage = 4
+
+    const [paginate, setPaginate] = useState<Paginate>({
+        index: 0,
+        currentPage: 1,
+        itemsPerPage: perPage,
+        totalPage: Math.ceil(fetchedAttendanceReportData.length / perPage),
+        slicedPages: null,
+    })
+
+    const handleItemsPerPage = (e: ChangeEvent<HTMLSelectElement>) => {
+        const item = parseInt(e.target.value)
+
+        const slicedPages: AttendanceReport[][] = []
+        for (let i = 0; i < fetchedAttendanceReportData.length; i += item) {
+            slicedPages.push(fetchedAttendanceReportData.slice(i, i + item))
+        }
+
+        setPaginate((prev) => {
+            return {
+                ...prev,
+                itemsPerPage: item,
+                index: 0,
+                currentPage: 1,
+                slicedPages,
+                totalPage: Math.ceil(fetchedAttendanceReportData.length / item),
+            }
+        })
+    }
+
+    useEffect(() => {
+        const slicedPages: AttendanceReport[][] = []
+        for (
+            let i = 0;
+            i < fetchedAttendanceReportData.length;
+            i += paginate.itemsPerPage
+        ) {
+            slicedPages.push(
+                fetchedAttendanceReportData.slice(i, i + paginate.itemsPerPage)
+            )
+        }
+
+        setPaginate((prev) => {
+            return {
+                ...prev,
+                slicedPages,
+                totalPage: Math.ceil(
+                    fetchedAttendanceReportData.length / paginate.itemsPerPage
+                ),
+            }
+        })
+    }, [fetchedAttendanceReportData])
+
+    const handleNext = () => {
+        console.log(paginate.currentPage, paginate.totalPage)
+        if (paginate.currentPage === paginate.totalPage) return
+        setPaginate((prev) => {
+            return {
+                ...prev,
+                index: prev.index + 1,
+                currentPage: prev.currentPage + 1,
+            }
+        })
+    }
+
+    const handlePrev = () => {
+        if (paginate.currentPage === 1) return
+        setPaginate((prev) => {
+            return {
+                ...prev,
+                index: prev.index - 1,
+                currentPage: prev.currentPage - 1,
+            }
+        })
+    }
+
+    const { currentPage, slicedPages, itemsPerPage } = paginate
+
+    const jumpToPage = (e: React.MouseEvent, index: number) => {
+        setPaginate((prev) => {
+            return {
+                ...prev,
+                index,
+                currentPage: index + 1,
+            }
+        })
+    }
+
+    return (
+        <div className='grid text-[1.6rem]'>
+            <caption className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
+                <p className=' font-bold'>
+                    Attendance Report List <span>(4)</span>
+                </p>
+                <div className='relative flex items-center'>
+                    <img
+                        src='/icons/admins/search.svg'
+                        alt=''
+                        className='absolute left-4 text-[4rem]'
+                    />
+                    <input
+                        type='text'
+                        placeholder='Search Parameters'
+                        className='pl-16 w-[18rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
+                    />
+                </div>
+                <div className='relative flex items-center'>
+                    <select className=' cursor-pointer w-[18rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4 bg-white'>
+                        <option hidden value=''>
+                            Sort By
+                        </option>
+                        <option value='date'>date</option>
+                        <option value='alpha'>Alpha</option>
+                    </select>
+                    <GrDown className='absolute right-4 text-[1.3rem]' />
+                </div>
+                <div className='relative flex items-center'>
+                    <select className=' cursor-pointer w-[18rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4 bg-white'>
+                        <option hidden value=''>
+                            Start Date
+                        </option>
+                        <option value='date'>date</option>
+                        <option value='alpha'>Alpha</option>
+                    </select>
+                    <GrDown className='absolute right-4 text-[1.3rem]' />
+                </div>
+                <div className='relative flex items-center'>
+                    <select className=' cursor-pointer w-[18rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4 bg-white'>
+                        <option hidden value=''>
+                            End Date
+                        </option>
+                        <option value='date'>date</option>
+                        <option value='alpha'>Alpha</option>
+                    </select>
+                    <GrDown className='absolute right-4 text-[1.3rem]' />
+                </div>
+            </caption>
+
+            <div className='grid'>
+                <div
+                    className='grid justify-between text-color-dark-1 bg-color-grey p-8 grid-cols-6 gap-8'
+                    style={{
+                        fontSize: '1.6rem',
+                    }}
+                >
+                    <p className='flex items-center gap-2'>
+                        <input type='checkbox' className='cursor-pointer' />
+                        <p>Guard Name</p>
+                    </p>
+                    <p>Date</p>
+                    <p>Guard Code</p>
+                    <p>Phone Number</p>
+                    <p>Clock In</p>
+                    <p>Clock Out </p>
+                </div>
+
+                <div className='grid gap-8 mt-8 p-8'>
+                    {slicedPages && slicedPages.length > 0 ? (
+                        React.Children.toArray(
+                            slicedPages[paginate.index].map(
+                                ({
+                                    guardCode,
+                                    guardName,
+                                    date,
+                                    clockInCount,
+                                    clockOutCount,
+                                    phoneNumber,
+                                }) => {
+                                    return (
+                                        <div className='grid justify-between border-b grid-cols-6 gap-8 '>
+                                            <p className='flex items-center gap-4'>
+                                                <input
+                                                    type='checkbox'
+                                                    className='cursor-pointer'
+                                                />
+
+                                                <span>{guardName}</span>
+                                            </p>
+                                            <p>{date}</p>
+                                            <p>{guardCode}</p>
+                                            <p>{phoneNumber}</p>
+                                            <p>{clockInCount}</p>
+                                            <p>{clockOutCount}</p>
+                                        </div>
+                                    )
+                                }
+                            )
+                        )
+                    ) : (
+                        <div>
+                            <div className='relative'>
+                                <div className='absolute w-full grid place-content-center'>
+                                    <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
+                <div className='flex gap-8 items-center'>
+                    <p>View</p>
+                    <select
+                        name=''
+                        id=''
+                        className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
+                        onChange={handleItemsPerPage}
+                    >
+                        {itemsPerPageArr.map((item, index) => (
+                            <option
+                                value={item}
+                                key={index}
+                                selected={item === itemsPerPage}
+                                className='capitalize cursor-pointer bg-white'
+                            >
+                                {item}
+                            </option>
+                        ))}
+                    </select>
+                    <p className='text'>List per page</p>
+                </div>
+                <ul className='flex items-center gap-5 ml-10'>
+                    <HiOutlineChevronLeft
+                        onClick={handlePrev}
+                        className='cursor-pointer'
+                    />
+
+                    {slicedPages?.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                {index + 1 === currentPage ? (
+                                    <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
+                                        {index + 1}
+                                    </span>
+                                ) : (
+                                    <span
+                                        className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
+                                        onClick={(e) => jumpToPage(e, index)}
+                                    >
+                                        {index + 1}
+                                    </span>
+                                )}
+                            </li>
+                        )
+                    })}
+
+                    {/* <li className='grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'>
+                        {totalPage}
+                    </li> */}
+                    <HiOutlineChevronRight
+                        onClick={handleNext}
+                        className='cursor-pointer'
+                    />
+                </ul>
+            </footer>
+        </div>
+    )
 }
 
 export default Overview
