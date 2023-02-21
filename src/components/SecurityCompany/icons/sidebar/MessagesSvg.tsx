f62f0d4eac39d8ddf0ba65c43d5cc3ed16e66d30
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 09:40:14 +0100

    working on view company

--- src/components/SecurityCompany/icons/sidebar/MessagesSvg.tsx
+++ src/components/SecurityCompany/icons/sidebar/MessagesSvg.tsx
@@ -11,18 +11,18 @@ const MessagesSvg = () => {
         >
             <path
                 fillRule='evenodd'
-                clip-rule='evenodd'
+                clipRule='evenodd'
                 d='M23 7L16 12L23 17V7V7Z'
-                stroke-width='2'
-                stroke-linecap='round'
+                strokeWidth='2'
+                strokeLinecap='round'
                 stroke-linejoin='round'
             />
             <path
                 fillRule='evenodd'
-                clip-rule='evenodd'
+                clipRule='evenodd'
                 d='M1 7C1 5.89543 1.89543 5 3 5H14C15.1046 5 16 5.89543 16 7V17C16 18.1046 15.1046 19 14 19H3C1.89543 19 1 18.1046 1 17V7Z'
-                stroke-width='2'
-                stroke-linecap='round'
+                strokeWidth='2'
+                strokeLinecap='round'
                 stroke-linejoin='round'
             />
         </svg>
