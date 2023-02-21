d5f44f93bf779438c5d37a3ff1d77ab84db168c0
Igboanugwo Collins <munisco12@gmail.com>
2023-02-21 11:47:44 +0100

    working on view company

--- src/pages/SecurityCompany/dashboard/company/AddSecurity/AddSecurityGuard.tsx
+++ src/pages/SecurityCompany/dashboard/company/AddSecurity/AddSecurityGuard.tsx
@@ -274,37 +274,7 @@ const AddSecurityGuard = () => {
                             </form>
                         ) : dialogState === 'add-security-guard' ? (
                             <div className='bg-white rounded-2xl grid place-content-center justify-items-center h-[30rem] gap-8 text-[1.6rem]'>
-                                <img
-                                    src='/icons/admins/modalSuccess.svg'
-                                    alt=''
-                                />
-
-                                <p>
-                                    You have successfully added an Security
-                                    Guard
-                                </p>
-
-                                <p className='font-bold text-[1.8rem] max-w-[40rem] text-center' style={{
-                                    fontFamily: 'Satoshi-Medium',
-                                }}>
-                                    Do you want to open a bank account for this
-                                    Security Guard?
-                                </p>
-
-                                <div className='flex w-full justify-center gap-8'>
-                                    <button
-                                        className='btn bg-white text-[#0556E5] border-[#0556E5] border rounded-lg w-[20rem] font-bold'
-                                        onClick={() => handleClose()}
-                                    >
-                                        Skip, Later
-                                    </button>
-                                    <button
-                                        className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[20rem]'
-                                        onClick={() => handleClose()}
-                                    >
-                                        Open an Account
-                                    </button>
-                                </div>
+                                //TODO
                             </div>
                         ) : (
                             <div className='bg-white rounded-2xl grid place-content-center justify-items-center h-[30rem] gap-8 text-[1.6rem]'>
