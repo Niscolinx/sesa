fbd62c729e3507b1acd7011c2f7b6890e694e537
Igboanugwo Collins <munisco12@gmail.com>
2023-02-23 17:45:21 +0100

    removed all headings in order to put the breadcrumbs

--- src/components/UI/BreadCrumb.tsx
+++ src/components/UI/BreadCrumb.tsx
@@ -8,14 +8,19 @@ function BreadCrumb() {
     return (
         <div className='flex gap-4 mb-[2rem]'>
             {breadcrumbs.map(({ match, breadcrumb }, i) => {
-                if (i > 1 && Object.keys(match.params).length < 1)
+                console.log({i},  breadcrumbs.length - 2)
+
+                const showBreadCrumb = i > 1 && Object.keys(match.params).length < 1
+                const showArrow = i < breadcrumbs.length - 2
+
+                if (showBreadCrumb)
                     return (
                         <p
                             className='flex items-center gap-2'
                             key={match.pathname}
                         >
                             <NavLink to={match.pathname}>{breadcrumb}</NavLink>
-                            {i < breadcrumbs.length - 1 && (
+                            {showArrow && (
                                 <span>
                                     <BiChevronRight />
                                 </span>
