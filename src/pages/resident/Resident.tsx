64132456268af7c69092a0dec2ab438b1ccaf9cb
Igboanugwo Collins <munisco12@gmail.com>
2023-02-28 15:29:50 +0100

    working on wallet

---
+++ src/pages/resident/Resident.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { IoMdAdd } from 'react-icons/io'
+
+
+function Residents() {
+  
+
+   
+    return (
+        <div>
+            <div className='rounded-lg mt-[3rem] h-[80vh]'>
+                {additionalResident ? (
+                    <section>{switchRoute(additionalResidentPath)}</section>
+                ) : (
+                    <section className='grid place-content-center w-full h-full justify-items-center gap-4 bg-white'>
+                        <img src='/icons/admins/errorSvg.svg' alt='' />
+                        <p className='text'>
+                            Ooops you have not added any Package yet
+                        </p>
+                        <button
+                            className='bg-color-blue-1 text-white flex gap-2 items-center rounded-lg justify-self-center py-4 px-16 text-[1.6rem]'
+                            onClick={handleAddEstate}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add Package
+                        </button>
+                    </section>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Residents
