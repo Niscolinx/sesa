130cc233c652a88d883cef70557f233328f4fd25
Igboanugwo Collins <munisco12@gmail.com>
2023-02-28 09:06:18 +0100

    working on estate manager

---
+++ src/components/EstateManager/icons/IconsBox.tsx
@@ -0,0 +1,93 @@
+import { Link } from 'react-router-dom';
+
+import AdditionalResidentSvg from './sidebar/AdditionalResidentSvg';
+import AdminsSvg from './sidebar/AdminsSvg';
+import AdvertSvg from './sidebar/AdvertSvg';
+import ArtisanSvg from './sidebar/ArtisanSvg';
+import EstateManagerSvg from './sidebar/EstateManagerSvg';
+import EstatesSvg from './sidebar/EstatesSvg';
+import OverviewSvg from './sidebar/overviewSvg';
+import RolesAndPermSvg from './sidebar/RolesAndPermSvg';
+import SecurityCompSvg from './sidebar/SecurityCompSvg';
+import SettingsSvg from './sidebar/SettingsSvg';
+import WalletSvg from './sidebar/WalletSvg';
+
+function IconsBox() {
+  return (
+      <ul className='iconsBox'>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin'>
+                  <OverviewSvg />
+                  <span>Overview</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/admins'>
+                  {' '}
+                  <AdminsSvg />
+                  <span>Admins</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/estates' className='fillIcon'>
+                  <EstatesSvg />
+                  <span>Estates</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/estateManagers' className='fillIcon'>
+                  {' '}
+                  <EstateManagerSvg />
+                  <span>Estate Manager</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/rolesAndPerm'>
+                  <RolesAndPermSvg />
+                  <span>Roles & Permissions</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/additional-resident'>
+                  {' '}
+                  <AdditionalResidentSvg />
+                  <span>Additional Resident</span>{' '}
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/security-company' className='fillIcon'>
+                  {' '}
+                  <SecurityCompSvg />
+                  <span>Security Company</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/wallet' className='fillIcon'>
+                  <WalletSvg />
+                  <span>Wallet</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/artisan' className='fillIcon'>
+                  {' '}
+                  <ArtisanSvg />
+                  <span>Artisan</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/advert'>
+                  <AdvertSvg />
+                  <span>Advert</span>
+              </Link>
+          </li>
+          <li className='iconsBox__list'>
+              <Link to='/superAdmin/platformSettings' className='fillIcon'>
+                  <SettingsSvg />
+                  <span>Settings</span>
+              </Link>
+          </li>
+      </ul>
+  )
+}
+
+export default IconsBox;
---
+++ src/components/EstateManager/icons/sidebar/overviewSvg.tsx
@@ -0,0 +1,47 @@
+const OverviewSvg = () => {
+    return (
+        <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='none'
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M3 3H10V10H3V3Z'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+            />
+            <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M14 3H21V10H14V3Z'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+            />
+            <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M14 14H21V21H14V14Z'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+            />
+            <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M3 14H10V21H3V14Z'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+            />
+        </svg>
+    )
+}
+
+export default OverviewSvg
