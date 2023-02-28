985499acdfc8cf10b94eb997e2e004c2c592fa7c
Igboanugwo Collins <munisco12@gmail.com>
2023-02-28 15:32:44 +0100

    working on wallet

--- src/Entry/routes/estateManagerRoutes.tsx
+++ src/Entry/routes/estateManagerRoutes.tsx
@@ -3,12 +3,14 @@ import Dashboard from '../../pages/EstateManager/Dashboard'
 import Overview from '../../pages/EstateManager/Overview'
 import Wallet from '../../pages/EstateManager/wallet/Wallet'
 import WalletDetails from '../../pages/EstateManager/wallet/WalletDetails'
+import Residents from '../../pages/EstateManager/residents/Residents'
 
 const estateManagerRoutes = (
     <Route path='/estateManager' element={<Dashboard />}>
         <Route index element={<Overview />} />
         <Route path='wallet' element={<Wallet />} />
         <Route path='wallet/transaction-details/:id' element={<WalletDetails />} />
+        <Route path='resident' element={<Residents />} />
     </Route>
 )
 
