b61667c699011b3cefda58cac591bdb7c2a1cfad
Igboanugwo Collins <munisco12@gmail.com>
2023-02-28 13:56:50 +0100

    working on wallet

--- src/Entry/routes/estateManagerRoutes.tsx
+++ src/Entry/routes/estateManagerRoutes.tsx
@@ -3,11 +3,13 @@ import  {
 } from 'use-react-router-breadcrumbs'
 import Dashboard from '../../pages/EstateManager/Dashboard'
 import Overview from '../../pages/EstateManager/Overview'
+import Wallet from '../../pages/EstateManager/Wallet'
 
 
 const estateManagerRoutes = (
     <Route path='/estateManager' element={<Dashboard />}>
         <Route index element={<Overview />} />
+        <Route path='wallet' element={<Wallet />} />
     </Route>
 )
 
