b8adb7a0a99eaa237f960bd0cad7b9c0cf319d8e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-01 20:38:44 +0100

    working on property

--- src/pages/EstateManager/household/CreateHousehold.tsx
+++ src/pages/EstateManager/household/CreateHousehold.tsx
@@ -18,46 +18,53 @@ function CreateHousehold() {
     return (
         <div>
             <div className='estateDetail__radioBox'>
-               
-                <label htmlFor='add-property' className='capitalize'>
-                    Add Property
-                </label>
-                <input
-                    type='radio'
-                    name='household'
-                    id='add-property'
-                    className='hidden'
-                    defaultChecked
-                    onChange={() => setPathToSwitch('add-property')}
-                />
+                <>
+                    <input
+                        type='radio'
+                        name='household'
+                        id='add-property'
+                        className='hidden'
+                        onChange={() => setPathToSwitch('add-property')}
+                    />
+                    <label htmlFor='add-property' className='capitalize'>
+                        Add Property
+                    </label>
+                </>
 
-                <label htmlFor='add-property'>Add Resident</label>
-                <input
-                    type='radio'
-                    name='household'
-                    id='add-resident'
-                    className='hidden'
-                    onChange={() => setPathToSwitch('add-resident')}
-                />
-                <label htmlFor='add-resident'>Add RFID (Car Sticker)</label>
-                <input
-                    type='radio'
-                    name='household'
-                    id='add-RFID'
-                    className='hidden'
-                    onChange={() => setPathToSwitch('add-RFID')}
-                />
-                <label htmlFor='add-resident'>Add Access Card</label>
-                <input
-                    type='radio'
-                    name='household'
-                    id='add-accessCard'
-                    className='hidden'
-                    onChange={() => setPathToSwitch('add-accessCard')}
-                />
+                <>
+                    <input
+                        type='radio'
+                        name='household'
+                        id='add-resident'
+                        className='hidden'
+                        onChange={() => setPathToSwitch('add-resident')}
+                    />
+                    <label htmlFor='add-resident'>Add Resident</label>
+                </>
+
+                <>
+                    <input
+                        type='radio'
+                        name='household'
+                        id='add-RFID'
+                        className='hidden'
+                        onChange={() => setPathToSwitch('add-RFID')}
+                    />
+                    <label htmlFor='add-RFID'>Add RFID (Car Sticker)</label>
+                </>
+                <>
+                    <input
+                        type='radio'
+                        name='household'
+                        id='add-accessCard'
+                        className='hidden'
+                        onChange={() => setPathToSwitch('add-accessCard')}
+                    />
+                    <label htmlFor='add-accessCard'>Add Access Card</label>
+                </>
             </div>
             <div className='mt-8 grid gap-8'>
-                <section className='bg-color-white rounded-lg border min-w-[112rem] overflow-scroll'>
+                <section className='bg-color-white rounded-lg min-w-[112rem] overflow-scroll'>
                     {handlePathSwitch.get(pathToSwitch)}
                 </section>
             </div>
