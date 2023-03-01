bdbf670aa075972e416552bec9169bc7a34b4fb4
Igboanugwo Collins <munisco12@gmail.com>
2023-03-01 11:37:42 +0100

    working on add resident

--- src/pages/EstateManager/residents/viewResident.tsx
+++ src/pages/EstateManager/residents/viewResident.tsx
@@ -117,8 +117,6 @@ const ViewResident = () => {
         <>
             <ToastContainer />
 
-          
-
             <dialog className='dialog' ref={validatedDialogRef}>
                 <section className='grid place-content-center w-full h-[100vh]'>
                     <div className='bg-white rounded-2xl grid items-baseline w-[90rem] min-h-[30rem] p-10 text-[1.6rem] relative gap-20'>
@@ -210,36 +208,33 @@ const ViewResident = () => {
             <dialog className='dialog' ref={dialogRef}>
                 <section className='grid place-content-center w-full h-[100vh]'>
                     <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8'>
-                       
-                            <>
-                                <img
-                                    src='/icons/admins/modalDeactivate.svg'
-                                    alt=''
-                                />
-                                <p className='text-[1.6rem]'>
-                                    Are you sure you want to deactivate this
-                                    Security Guard
-                                </p>
+                        <>
+                            <img
+                                src='/icons/admins/modalDeactivate.svg'
+                                alt=''
+                            />
+                            <p className='text-[1.6rem]'>
+                                Are you sure you want to deactivate this
+                                Security Guard
+                            </p>
 
-                                <div className='flex w-full justify-center gap-8'>
-                                    <button
-                                        className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
-                                        onClick={() =>
-                                            handleCloseDeleteOrDeactivateDialog()
-                                        }
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
-                                        onClick={handleDeactivateSecurityGuard}
-                                    >
-                                        Deactivate
-                                    </button>
-                                </div>
-                            </>
-                     
-                            
+                            <div className='flex w-full justify-center gap-8'>
+                                <button
+                                    className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
+                                    onClick={() =>
+                                        handleCloseDeleteOrDeactivateDialog()
+                                    }
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
+                                    onClick={handleDeactivateSecurityGuard}
+                                >
+                                    Deactivate
+                                </button>
+                            </div>
+                        </>
                     </div>
                 </section>
             </dialog>
@@ -320,25 +315,16 @@ const ViewResident = () => {
                         </div>
 
                         <div className='flex gap-8'>
+                            
                             <button
-                                className='border border-color-blue-1 text-color-blue-1 px-16 py-4 flex items-center  rounded-lg gap-4'
+                                className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
                                 onClick={() =>
                                     openDeleteOrDeactivateDialog('Deactivate')
                                 }
                             >
-                                <span className=' text-[1.4rem] font-semibold'>
-                                    Deactivate
-                                </span>
-                            </button>
-                            <button
-                                className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
-                                onClick={() =>
-                                    openDeleteOrDeactivateDialog('Delete')
-                                }
-                            >
                                 <img src='/icons/admins/delete.svg' alt='' />
                                 <span className='text-red-600 text-[1.4rem] font-semibold'>
-                                    Delete
+                                    Deactivate
                                 </span>
                             </button>
                         </div>
