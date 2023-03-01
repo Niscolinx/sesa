5c1dd4488e1ff9eb1eac6c5ace4f4d409780517e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-01 11:31:10 +0100

    working on add resident

--- src/pages/EstateManager/residents/AddResident.tsx
+++ src/pages/EstateManager/residents/AddResident.tsx
@@ -109,6 +109,93 @@ const AddResident = () => {
         <>
             <ToastContainer />
 
+            <dialog className='dialog' ref={validateDialogRef}>
+                <section className='grid place-content-center w-full h-[100vh]'>
+                    <div className='bg-white rounded-2xl grid items-baseline w-[90rem] min-h-[30rem] p-10 text-[1.6rem] relative gap-20'>
+                        <IoMdClose
+                            className='absolute right-4 top-4 text-[2rem] cursor-pointer'
+                            onClick={() => closeValidateDialog()}
+                        />
+
+                        <div className='relative h-[14rem] bg-blue-600 w-full mt-10 rounded-lg'>
+                            <img
+                                src='/img/me.jpeg'
+                                alt=''
+                                className='w-[10rem] h-[10rem] border rounded-full border-green-600 object-cover absolute bottom-[-6rem] left-10 object-top'
+                            />
+                        </div>
+                        <div className='mt-20'>
+                            <h2>Validation Result</h2>
+
+                            <div className='border grid mt-5'>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Validation Option
+                                    </p>
+                                    <p className='py-4'>
+                                        Phone Number | (+234) 813238432
+                                    </p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Full Name
+                                    </p>
+                                    <p className='py-4'>Michael Okonkwo</p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Date of Birth
+                                    </p>
+                                    <p className='py-4'>15 May, 1998</p>
+                                </div>
+                                <div className='grid grid-cols-2 border-b gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Phone Number
+                                    </p>
+                                    <p className='py-4'> (+234) 813238432</p>
+                                </div>
+                                <div className='grid grid-cols-2  gap-4'>
+                                    <p
+                                        className='border-r py-4 pl-4 text-gray-700'
+                                        style={{
+                                            fontFamily: 'Satoshi-Light',
+                                        }}
+                                    >
+                                        Gender
+                                    </p>
+                                    <p className='py-4'>Male</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            className='btn text-white bg-[#0556E5] border rounded-lg w-[15rem] justify-self-center'
+                            onClick={() => closeValidateDialog()}
+                        >
+                            Ok
+                        </button>
+                    </div>
+                </section>
+            </dialog>
             <dialog className='dialog' ref={dialogRef}>
                 <section className='grid place-content-center w-full h-[100vh]'>
                     <div className='bg-white rounded-2xl grid items-baseline w-[64rem] min-h-[30rem] p-10 gap-8 text-[1.6rem] relative'>
@@ -185,7 +272,7 @@ const AddResident = () => {
                                     </button>
                                     <button
                                         className=' bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
-                                        //onClick={handleReAssign}
+                                        onClick={() => handleClose()}
                                     >
                                         Ok
                                     </button>
