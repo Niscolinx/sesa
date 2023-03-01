60c12a4448ecf5914e9093bd9efe6f99e246e754
Igboanugwo Collins <munisco12@gmail.com>
2023-03-01 12:58:23 +0100

    working on add resident

--- src/pages/EstateManager/properties/Property.tsx
+++ src/pages/EstateManager/properties/Property.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
 import OverviewCard from '../../../components/EstateManager/OverviewCard'
+import { EstateChart } from '../../../components/SuperAdmin/charts/OverviewChart'
 import UniqueResident from '../residents/UniqueResident'
 
 function Property() {
-    type Path = 'unique' | 'profile'
-
     const [isProperty, setIsProperty] = useState(false)
 
     const addproperty = () => {
@@ -18,48 +17,28 @@ function Property() {
         <>
             {isProperty ? (
                 <div>
-                    <section className='bg-white rounded-lg p-8 grid text-[1.4rem] '>
-                        <div
-                            className=' grid gap-8'
-                            style={{
-                                gridTemplateColumns:
-                                    'repeat(auto-fit, minmax(30rem, 1fr))',
-                            }}
-                        >
-                            <OverviewCard
-                                title='Unique propertys'
-                                number={1532}
-                                iconUrl='/icons/estateManager/people.svg'
-                                bgColor='bg-[#F5F9FA]'
-                                textColor='text-[#00C2FF]'
-                                bottomLeft='Alpha 56%'
-                                bottomRight='Res. User 44%'
-                            />
-                            <OverviewCard
-                                title='KYR Validation'
-                                number='45%'
-                                iconUrl='/icons/estateManager/validation.svg'
-                                bgColor='bg-[#EDFDEC]'
-                                textColor='text-[#1A8F56]'
-                                bottomLeft='% of propertys whose identity has been validated'
-                            />
-                            <OverviewCard
-                                title='property Profiles'
-                                number={18100}
-                                iconUrl='/icons/estateManager/profiles.svg'
-                                bgColor='bg-[#FCF3FA]'
-                                textColor='text-[#B6008E]'
-                                bottomLeft='LR 39% - TR 29%'
-                                bottomRight='LD 10% - LNR 2%'
-                            />
+                    <section className='bg-white rounded-lg p-8 grid text-[1.4rem] grid-cols-2 '>
+                        
+                        <div className='overviewChart__box'>
+                            <EstateChart color1='#3d08d2' color2='#F74183' />
+
+                            <div className='overviewChart__label'>
+                                <p className='text-[3rem] font-bold relative'>
+                                    50,000
+                                </p>
+                                <p className='text-[1.2rem] max-w-[9.8rem]'>
+                                    Total Check-Out
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            
                         </div>
                     </section>
 
                     <div className='rounded-lg mt-[3rem] h-[80vh]'>
                         <section>
-                            
-
-                          <UniqueResident/>
+                            <UniqueResident />
                         </section>
                     </div>
                 </div>
