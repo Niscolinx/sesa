15bfc9e28d1ed980af434cfba80986531f6339d0
Igboanugwo Collins <munisco12@gmail.com>
2023-03-01 11:42:21 +0100

    working on add resident

--- src/Entry/routes/estateManagerRoutes.tsx
+++ src/Entry/routes/estateManagerRoutes.tsx
@@ -5,6 +5,7 @@ import Wallet from '../../pages/EstateManager/wallet/Wallet'
 import WalletDetails from '../../pages/EstateManager/wallet/WalletDetails'
 import Residents from '../../pages/EstateManager/residents/Residents'
 import AddResident from '../../pages/EstateManager/residents/AddResident'
+import ViewResident from '../../pages/EstateManager/residents/viewResident'
 
 const estateManagerRoutes = (
     <Route path='/estateManager' element={<Dashboard />}>
@@ -13,6 +14,7 @@ const estateManagerRoutes = (
         <Route path='wallet/transaction-details/:id' element={<WalletDetails />} />
         <Route path='residents' element={<Residents />} />
         <Route path='residents/add' element={<AddResident />} />
+        <Route path='residents/view/:id' element={<ViewResident />} />
     </Route>
 )
 
