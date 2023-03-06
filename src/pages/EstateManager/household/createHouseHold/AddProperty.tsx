d501067aa517ae214b1ce3acc39392a8f183460e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-06 10:36:44 +0100

    working on household

--- src/pages/EstateManager/household/createHouseHold/AddProperty.tsx
+++ src/pages/EstateManager/household/createHouseHold/AddProperty.tsx
@@ -23,54 +23,57 @@ function AddProperty() {
                     setSelectedState={setSelectedPropertyCode}
                 />
             </div>
-            <section className='w-full flex gap-16 relative'>
-                <div>
-                    <img
-                        src={'/img/img3.png'}
-                        alt=''
-                        className='w-[20rem] h-[18rem] object-cover rounded-lg'
-                    />
-                </div>
+            {selectedPropertyCode && (
+                <section className='w-full flex gap-16 relative'>
+                    <div>
+                        <img
+                            src={'/img/img3.png'}
+                            alt=''
+                            className=' object-cover rounded-lg'
+                        />
+                    </div>
 
-                <div className='grid'>
-                    <div>
-                        <p className='text-[1.4rem] text-[#043FA7]'>
-                            Property Code
-                        </p>
-                        <p className='font-[1.6rem] whitespace-nowrap'>
-                            {selectedPropertyCode}
-                        </p>
+                    <div className='flex '>
+                        <div className='grid gap-8'>
+                            <div>
+                                <p className='text-[1.4rem] text-[#043FA7]'>
+                                    Property Code
+                                </p>
+                                <p className='font-[1.6rem] whitespace-nowrap'>
+                                    {selectedPropertyCode}
+                                </p>
+                            </div>
+                            <div>
+                                <p className='text-[#043FA7]'>Property Type</p>
+                                <p>Duplex</p>
+                            </div>
+                            <div>
+                                <p className='text-[#043FA7]'>
+                                    Property Address
+                                </p>
+                                <p className='max-w-[30rem]'>
+                                    10, Address Street, Address Avenue, Lagos,
+                                    Nigeria.
+                                </p>{' '}
+                            </div>
+                        </div>
+                        <div className='grid gap-8 auto-rows-max'>
+                            <div>
+                                <p className='text-[1.4rem] text-[#043FA7]'>
+                                    Property Category
+                                </p>
+                                <p className='font-[1.6rem] whitespace-nowrap'>
+                                    Business
+                                </p>
+                            </div>
+                            <div>
+                                <p className='text-[#043FA7]'>Property Name</p>
+                                <p>Wale House</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <p className='text-[#043FA7]'>Property Type</p>
-                        <p>Duplex</p>
-                    </div>
-                    <div>
-                        <p className='text-[#043FA7]'>Property Address</p>
-                        <p>
-                            10, Address Street, Address Avenue, Lagos, Nigeria.
-                        </p>{' '}
-                    </div>
-                </div>
-                <div className='grid'>
-                    <div>
-                        <p className='text-[1.4rem] text-[#043FA7]'>
-                            Property Category
-                        </p>
-                        <p className='font-[1.6rem] whitespace-nowrap'>
-                            Business
-                        </p>
-                    </div>
-                    <div>
-                        <p className='text-[#043FA7]'>Property Name</p>
-                        <p>Wale House</p>
-                    </div>
-                    <div>
-                        <p className='text-[#043FA7]'>Status</p>
-                        <p className='text-[#1D9F5F]'>Active</p>
-                    </div>
-                </div>
-            </section>
+                </section>
+            )}
         </div>
     )
 }
