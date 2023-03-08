2ccfe6f60a20131d39c85ee7bf7d7bb25df36f22
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 19:11:46 +0100

    working on site worker

--- src/pages/EstateManager/SiteWorker/AddSiteWorker.tsx
+++ src/pages/EstateManager/SiteWorker/AddSiteWorker.tsx
@@ -348,6 +348,62 @@ const [propertyCode, setPropertyCode] = useState<string | null>(null)
                             selectedState={propertyCode}
                             setSelectedState={setPropertyCode}
                         />
+
+                        {propertyCode && (
+                            <section className='w-full flex gap-16 relative'>
+                                <div>
+                                    <img
+                                        src={'/img/img3.png'}
+                                        alt=''
+                                        className=' object-cover rounded-lg'
+                                    />
+                                </div>
+
+                                <div className='flex '>
+                                    <div className='grid gap-8'>
+                                        <div>
+                                            <p className='text-[1.4rem] text-[#043FA7]'>
+                                                Property Code
+                                            </p>
+                                            <p className='font-[1.6rem] whitespace-nowrap'>
+                                                {propertyCode}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className='text-[#043FA7]'>
+                                                Property Type
+                                            </p>
+                                            <p>Duplex</p>
+                                        </div>
+                                        <div>
+                                            <p className='text-[#043FA7]'>
+                                                Property Address
+                                            </p>
+                                            <p className='max-w-[30rem]'>
+                                                10, Address Street, Address
+                                                Avenue, Lagos, Nigeria.
+                                            </p>{' '}
+                                        </div>
+                                    </div>
+                                    <div className='grid gap-8 auto-rows-max'>
+                                        <div>
+                                            <p className='text-[1.4rem] text-[#043FA7]'>
+                                                Property Category
+                                            </p>
+                                            <p className='font-[1.6rem] whitespace-nowrap'>
+                                                Business
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <p className='text-[#043FA7]'>
+                                                Property Name
+                                            </p>
+                                            <p>Wale House</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        )}
                     </div>
                     <form
                         onSubmit={handleSubmit}
