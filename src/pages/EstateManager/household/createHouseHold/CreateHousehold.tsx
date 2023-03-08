35ec3556aa64f0a654cdb25c64c12bf6ccb02f4e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 09:23:19 +0100

    working on household

--- src/pages/EstateManager/household/createHouseHold/CreateHousehold.tsx
+++ src/pages/EstateManager/household/createHouseHold/CreateHousehold.tsx
@@ -4,6 +4,7 @@ import {
     createContext,
     Dispatch,
     SetStateAction,
+    useRef,
 } from 'react'
 import { TfiArrowCircleLeft, TfiArrowCircleRight } from 'react-icons/tfi'
 
@@ -97,8 +98,50 @@ function CreateHousehold() {
     const handleSubmit = () => {
         console.log('submitted')
     }
+
+    const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+    const handleClose = () => {
+        dialogRef.current?.close()
+    }
+
+    const handleOpen = () => {
+        dialogRef.current?.showModal()
+    }
+
+    const addPropertyHandler = () => {
+        handleOpen()
+    }
+
     return (
         <>
+            <dialog className='dialog' ref={dialogRef}>
+                <section className='grid place-content-center w-full h-[100vh]'>
+                    <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8'>
+                        <img
+                            src='/icons/admins/modalSuccess.svg'
+                            alt=''
+                            className='animate__animated animate__pulse'
+                            style={{
+                                animationIterationCount: 'infinite',
+                            }}
+                        />
+                        <p>You have successfully added an Property</p>
+
+                        <div className='flex w-full justify-center gap-8'>
+                            <button className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'>
+                                View details
+                            </button>
+                            <button
+                                className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
+                                onClick={() => handleClose()}
+                            >
+                                Ok
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </dialog>
             <HouseholdContext.Provider
                 value={{
                     selectedPropertyCode,
