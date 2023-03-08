19c5ad507c6f89c8871ab7d18b538e9d1213fb3e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 10:40:12 +0100

    working on household

--- src/pages/EstateManager/household/ViewHouseHold.tsx
+++ src/pages/EstateManager/household/ViewHouseHold.tsx
@@ -33,6 +33,95 @@ function ViewHouseHold() {
                         </p>
                         <p className='py-4'>Sandfill</p>
                     </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Property Code
+                        </p>
+                        <p className='py-4'>P09897</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Property Type
+                        </p>
+                        <p className='py-4'>3 Bedroom Self Con</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Property Class
+                        </p>
+                        <p className='py-4'>Residential</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Property Address
+                        </p>
+                        <p className='py-4'>Block 21 Flat 19 , Zone A</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Number of Occupants
+                        </p>
+                        <p className='py-4'>5</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Number of Access Card
+                        </p>
+                        <p className='py-4'>3</p>
+                    </div>
+                    <div className='grid grid-cols-2 border-b border-b-black gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Number of RFID
+                        </p>
+                        <p className='py-4'>5</p>
+                    </div>
+                    <div className='grid grid-cols-2  gap-4'>
+                        <p
+                            className='border-r py-4 pl-4 text-gray-700 border-r-black'
+                            style={{
+                                fontFamily: 'Satoshi-Light',
+                            }}
+                        >
+                            Number of Assigned Products
+                        </p>
+                        <p className='py-4'>5</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
