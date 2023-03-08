8d82dd347d41da63e53efb2b9b4881e697e28b32
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 14:15:34 +0100

    working on household

---
+++ src/pages/EstateManager/household/viewHouseHold/Payments.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Payments() {
+  return (
+      <div className='mt-20'>
+          <div className=' grid mt-5 '>
+              <div className='grid grid-cols-2 gap-4'>
+                  <p
+                      className=' py-4  text-gray-700 '
+                      style={{
+                          fontFamily: 'Satoshi-Light',
+                      }}
+                  >
+                      Estate
+                  </p>
+                  <p className='py-4'>Sandfill</p>
+              </div>
+           
+          </div>
+      </div>
+  )
+}
+
+export default Payments
