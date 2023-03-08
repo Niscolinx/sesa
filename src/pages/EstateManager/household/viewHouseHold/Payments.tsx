2c34de07c83938f52b3052912200feb221bc203c
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 14:32:28 +0100

    working on household

--- src/pages/EstateManager/household/viewHouseHold/Payments.tsx
+++ src/pages/EstateManager/household/viewHouseHold/Payments.tsx
@@ -7,19 +7,19 @@ function Payments() {
                 <p className='font-Satoshi-Medium text-[2rem] mb-4'>
                     Product Information
                 </p>
-                <section>
-                    <div className='grid grid-cols-2 gap-4 w-[60rem]'>
+                <section className='w-[60rem]'>
+                    <div className='grid grid-cols-2 gap-4 '>
                         <div className='grid grid-cols-2 items-center gap-4 justify-start w-[20rem] whitespace-nowrap'>
-                            <p className=' py-4  text-gray-700 font-Satoshi-Light  '>
+                            <p className='text-gray-700 font-Satoshi-Light  '>
                                 Product Code:
                             </p>
-                            <p className='py-4'>R87231</p>
+                            <p>R87231</p>
                         </div>
                         <div className='grid grid-cols-2 items-center gap-4 whitespace-nowrap w-[20rem]'>
-                            <p className=' py-4  text-gray-700 font-Satoshi-Light '>
+                            <p className='text-gray-700 font-Satoshi-Light '>
                                 Start Date:
                             </p>
-                            <p className='py-4'>22 Feb 2023</p>
+                            <p>22 Feb 2023</p>
                         </div>
                     </div>
                 </section>
