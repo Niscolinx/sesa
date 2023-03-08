abdd88da83120bb76bcce2e05697bc0636e949ce
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 14:16:55 +0100

    working on household

--- src/pages/EstateManager/household/viewHouseHold/Payments.tsx
+++ src/pages/EstateManager/household/viewHouseHold/Payments.tsx
@@ -4,12 +4,11 @@ function Payments() {
   return (
       <div className='mt-20'>
           <div className=' grid mt-5 '>
+            <p className='font-Satoshi-Medium text-[2rem]'>Product Information</p>
               <div className='grid grid-cols-2 gap-4'>
                   <p
-                      className=' py-4  text-gray-700 '
-                      style={{
-                          fontFamily: 'Satoshi-Light',
-                      }}
+                      className=' py-4  text-gray-700 font-Satoshi-Light '
+                     
                   >
                       Estate
                   </p>
