c8b43c147a21712f7c49a1bd9ded07ae202ec51f
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 18:01:22 +0100

    working on household

--- src/pages/EstateManager/EstateStaff/ViewEstateStaff.tsx
+++ src/pages/EstateManager/EstateStaff/ViewEstateStaff.tsx
@@ -564,14 +564,8 @@ const ViewEstateStaff = () => {
                         className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg col-span-full mt-[5rem]'
                         style={{ justifySelf: 'start' }}
                     >
-                        <span>
-                            <img
-                                src='/icons/admins/saveDisk.svg'
-                                alt=''
-                                className='w-[1.7rem] h-[1.7rem]'
-                            />
-                        </span>{' '}
-                        Save Changes
+                       
+                        Generate ID Card
                     </button>
                 </section>
             </main>
