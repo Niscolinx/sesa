02d8852e5f8a83c0e5e56dce2b7b1f8538593d82
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 20:19:32 +0100

    working on household

--- src/pages/EstateManager/SecurityGuard/ViewSecurityGuard.tsx
+++ src/pages/EstateManager/SecurityGuard/ViewSecurityGuard.tsx
@@ -438,64 +438,7 @@ const ViewSecurityGuard = () => {
                             </button>
                         </div>
                     </div>
-                    <section className=' relative mb-[5rem]'>
-                        <p className='text-[2rem] font-Satoshi-Medium mb-10'>
-                            Work Location Details
-                        </p>
-                        <div className='w-full flex gap-16'>
-                            <div>
-                                <img
-                                    src={'/img/img3.png'}
-                                    alt=''
-                                    className=' object-cover rounded-lg'
-                                />
-                            </div>
-
-                            <div className='flex '>
-                                <div className='grid gap-8'>
-                                    <div>
-                                        <p className='text-[1.4rem] text-[#043FA7]'>
-                                            Property Code
-                                        </p>
-                                        <p className='font-[1.6rem] whitespace-nowrap'>
-                                            ThomasEstate/SO-2345CDGK1
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className='text-[#043FA7]'>
-                                            Property Type
-                                        </p>
-                                        <p>Duplex</p>
-                                    </div>
-                                    <div>
-                                        <p className='text-[#043FA7]'>
-                                            Property Address
-                                        </p>
-                                        <p className='max-w-[30rem]'>
-                                            10, Address Street, Address Avenue,
-                                            Lagos, Nigeria.
-                                        </p>{' '}
-                                    </div>
-                                </div>
-                                <div className='grid gap-8 auto-rows-max'>
-                                    <div>
-                                        <p className='text-[1.4rem] text-[#043FA7]'>
-                                            Property Category
-                                        </p>
-                                        <p className='font-[1.6rem] whitespace-nowrap'>
-                                            Business
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className='text-[#043FA7]'>
-                                            Property Name
-                                        </p>
-                                        <p>Wale House</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    
 
                     <section>
                         <p className='text-[2rem] font-Satoshi-Medium'>
