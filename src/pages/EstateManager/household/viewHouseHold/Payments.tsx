ddf970e21eb4b17ccf0914c14bd6cf90b772dc71
Igboanugwo Collins <munisco12@gmail.com>
2023-03-08 14:18:53 +0100

    working on household

--- src/pages/EstateManager/household/viewHouseHold/Payments.tsx
+++ src/pages/EstateManager/household/viewHouseHold/Payments.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 
 function Payments() {
-  return (
-      <div className='mt-20'>
-          <div className=' grid mt-5 '>
-            <p className='font-Satoshi-Medium text-[2rem]'>Product Information</p>
-              <div className='grid grid-cols-2 gap-4'>
-                  <p
-                      className=' py-4  text-gray-700 font-Satoshi-Light '
-                     
-                  >
-                      Estate
-                  </p>
-                  <p className='py-4'>Sandfill</p>
-              </div>
-           
-          </div>
-      </div>
-  )
+    return (
+        <div className='mt-20'>
+            <div className=' grid mt-5 '>
+                <p className='font-Satoshi-Medium text-[2rem]'>
+                    Product Information
+                </p>
+                <div className='grid grid-cols-2 gap-4'>
+                    <div className='flex items-center gap-4'>
+                        <p className=' py-4  text-gray-700 font-Satoshi-Light '>
+                            Product Code
+                        </p>
+                        <p className='py-4'>R87231</p>
+                    </div>
+                    <div className='flex items-center gap-4'>
+                        <p className=' py-4  text-gray-700 font-Satoshi-Light '>
+                            Start Date
+                        </p>
+                        <p className='py-4'>22 Feb 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Payments
