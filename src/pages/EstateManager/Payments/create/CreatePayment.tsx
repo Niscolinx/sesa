cfaad1986190b65e86a1f1c9528efb99a567d1ee
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 09:52:03 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/CreatePayment.tsx
+++ src/pages/EstateManager/Payments/create/CreatePayment.tsx
@@ -5,26 +5,18 @@ import HouseholdFilter from './filter/Household'
 
 export type PaymentPlan = 'fixed' | 'flexible'
 export type PaymentType = 'full' | 'installment'
-type Steps = 'initial' | ''
+type Step = 'initial' | 'list'
 
 const CreatePayment = () => {
-    const [paymentPlan, setPaymentPlan] = useState<string | null | PaymentPlan>(
-        null
-    )
-    const [step, setStep] = useState('initial')
-    const [trackPayment, setTrackPayment] = useState<string | null>(null)
-    const [amount, setAmount] = useState(0)
-
-    const [paymentType, setPaymentType] = useState<PaymentType | string | null>(
-        null
-    )
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-    }
+   
 
     const renderPaymentType = new Map([['full', <HouseholdFilter/>]]) satisfies Map<
         PaymentType,
+        JSX.Element
+    >
+
+    const displayStep = new Map([['initial', <HouseholdFilter/>]]) satisfies Map<
+        Step,
         JSX.Element
     >
 
@@ -33,71 +25,7 @@ const CreatePayment = () => {
             <ToastContainer />
 
             <div className='grid p-8 bg-white h-[80vh] items-baseline overflow-y-scroll rounded-lg'>
-                <form
-                    onSubmit={handleSubmit}
-                    className='grid max-w-[84rem] gap-16 mt-12'
-                    style={{
-                        gridTemplateColumns:
-                            ' repeat(auto-fit, minmax(35rem, 1fr))',
-                    }}
-                >
-                    <div className='grid gap-4 relative '>
-                        <label
-                            htmlFor='firstName'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Dues Name
-                        </label>
-                        <input
-                            type='text'
-                            required
-                            id='firstName'
-                            placeholder='placeholder'
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                        />
-                    </div>
-
-                    <Select
-                        label='Track Payment'
-                        state={['Yes', 'No']}
-                        selectedState={trackPayment}
-                        setSelectedState={setTrackPayment}
-                    />
-
-                    <div>
-                        <label
-                            htmlFor='amount'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Amount
-                        </label>
-                        <input
-                            type='number'
-                            required
-                            id='amount'
-                            value={amount <= 0 ? '' : amount}
-                            onChange={(e) => setAmount(Number(e.target.value))}
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                        />
-                    </div>
-
-                    <Select
-                        label='Payment Type'
-                        state={['full', 'installment'] satisfies PaymentType[]}
-                        selectedState={paymentType}
-                        setSelectedState={setPaymentType}
-                    />
-                    <Select
-                        label='Payment Plan'
-                        state={['fixed', 'flexible'] satisfies PaymentPlan[]}
-                        selectedState={paymentPlan}
-                        setSelectedState={setPaymentPlan}
-                    />
-
-                    <button className='btn text-white bg-color-blue-1 py-4 px-16 rounded-lg w-[15rem] col-span-full mt-10'>
-                        Next
-                    </button>
-                </form>
+                
             </div>
         </>
     )
---
+++ src/pages/EstateManager/Payments/create/paymentType/flexible/FlexiblePaymentType.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Select } from '../../../../../../components/SuperAdmin/UI/Select'
+import { PaymentPlan, PaymentType } from '../../CreatePayment'
+
+function FlexiblePaymentType() {
+     const [paymentPlan, setPaymentPlan] = useState<
+         string | null | PaymentPlan
+     >(null)
+     const [step, setStep] = useState('initial')
+     const [trackPayment, setTrackPayment] = useState<string | null>(null)
+     const [amount, setAmount] = useState(0)
+
+     const [paymentType, setPaymentType] = useState<
+         PaymentType | string | null
+     >(null)
+
+     const handleSubmit = (e: React.FormEvent) => {
+         e.preventDefault()
+     }
+  return (
+      <form
+          onSubmit={handleSubmit}
+          className='grid max-w-[84rem] gap-16 mt-12'
+          style={{
+              gridTemplateColumns: ' repeat(auto-fit, minmax(35rem, 1fr))',
+          }}
+      >
+          <div className='grid gap-4 relative '>
+              <label htmlFor='firstName' className='text-[1.4rem] font-medium'>
+                  Dues Name
+              </label>
+              <input
+                  type='text'
+                  required
+                  id='firstName'
+                  placeholder='placeholder'
+                  className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+              />
+          </div>
+
+          <Select
+              label='Track Payment'
+              state={['Yes', 'No']}
+              selectedState={trackPayment}
+              setSelectedState={setTrackPayment}
+          />
+
+          <div>
+              <label htmlFor='amount' className='text-[1.4rem] font-medium'>
+                  Amount
+              </label>
+              <input
+                  type='number'
+                  required
+                  id='amount'
+                  value={amount <= 0 ? '' : amount}
+                  onChange={(e) => setAmount(Number(e.target.value))}
+                  className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+              />
+          </div>
+
+          <Select
+              label='Payment Type'
+              state={['full', 'installment'] satisfies PaymentType[]}
+              selectedState={paymentType}
+              setSelectedState={setPaymentType}
+          />
+          <Select
+              label='Payment Plan'
+              state={['fixed', 'flexible'] satisfies PaymentPlan[]}
+              selectedState={paymentPlan}
+              setSelectedState={setPaymentPlan}
+          />
+
+          <button className='btn text-white bg-color-blue-1 py-4 px-16 rounded-lg w-[15rem] col-span-full mt-10'>
+              Next
+          </button>
+      </form>
+  )
+}
+
+export default FlexiblePaymentType
