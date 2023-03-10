ce5e152dfe9b599db702ee6dec93dd4f74dfdd75
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 13:44:40 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
+++ src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
@@ -112,13 +112,13 @@ function PaymentForm({ props }: PaymentForm) {
             />
 
             <div className='grid gap-4'>
-                <label htmlFor='amount' className='text-[1.4rem] font-medium'>
+                <label htmlFor='deadline' className='text-[1.4rem] font-medium'>
                     Deadline
                 </label>
                 <input
                     type='date'
                     required
-                
+                    min=''
                     className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                 />
             </div>
