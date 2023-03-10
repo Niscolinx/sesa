144eec2c9e013ef4e2bf0df6eb0e429a7ab56b11
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 18:06:30 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
+++ src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
@@ -312,19 +312,24 @@ function PaymentForm({ props }: PaymentForm) {
 
                         {installmentField.map((item, idx) => (
                             <div className='flex items-center gap-4'>
-                                <span className='text-color-blue-1 font-medium'>
-                                    Installment {idx + 1}
-                                </span>
-                                <p className='flex items-center font-semibold'>
-                                    <span>
-                                        <img src='/icons/Naira.svg' alt='' />
-                                    </span>{' '}
-                                    {item.amount > 0 && (
-                                        <span>
-                                            {item.amount.toLocaleString()}
+                                {item.amount > 0 && (
+                                    <>
+                                        <span className='text-color-blue-1 font-medium'>
+                                            Installment {idx + 1}
                                         </span>
-                                    )}
-                                </p>
+                                        <p className='flex items-center font-semibold'>
+                                            <span>
+                                                <img
+                                                    src='/icons/Naira.svg'
+                                                    alt=''
+                                                />
+                                            </span>{' '}
+                                            <span>
+                                                {item.amount.toLocaleString()}
+                                            </span>
+                                        </p>
+                                    </>
+                                )}
                             </div>
                         ))}
                     </div>
