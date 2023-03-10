115fd78e7c0931ae82f0cf8caf18e817de752af5
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 08:23:11 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/CreatePayment.tsx
+++ src/pages/EstateManager/Payments/create/CreatePayment.tsx
@@ -55,140 +55,45 @@ const CreatePayment = () => {
                             htmlFor='firstName'
                             className='text-[1.4rem] font-medium'
                         >
-                            First Name *
+                            Dues Name
                         </label>
                         <input
                             type='text'
                             required
                             id='firstName'
+                            placeholder='placeholder'
                             className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                         />
                     </div>
-                    <div className='grid gap-4 relative '>
+                    
+                     <Select
+                        label='Gender'
+                        state={['Male', 'Female']}
+                        selectedState={selectedGender}
+                        setSelectedState={setSelectedGender}
+                    />
                         <label
-                            htmlFor='lastName'
+                            htmlFor='firstName'
                             className='text-[1.4rem] font-medium'
                         >
-                            Middle Name *
+                            Dues Name
                         </label>
                         <input
                             type='text'
                             required
-                            id='lastName'
+                            id='firstName'
+                            placeholder='placeholder'
                             className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                         />
                     </div>
-                    <div className='grid gap-4 relative '>
-                        <label
-                            htmlFor='lastName'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Last Name *
-                        </label>
-                        <input
-                            type='text'
-                            required
-                            id='lastName'
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                        />
-                    </div>
-
-                    <div className='grid gap-4 relative '>
-                        <label
-                            htmlFor='lastName'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Date of Birth
-                        </label>
-                        <input
-                            type='text'
-                            required
-                            id='lastName'
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                        />
-                    </div>
-                    <div className='grid gap-4 relative'>
-                        <label
-                            htmlFor='email'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Email Address *
-                        </label>
-                        <input
-                            type='email'
-                            required
-                            id='email'
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                        />
-                    </div>
-
-                    <div className='grid gap-4'>
-                        <label
-                            htmlFor='phoneNumber'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Phone Number *
-                        </label>
-
-                        <div className='flex text-[1.6rem] gap-4   h-[5rem]'>
-                            <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
-                                <option value='234'>+234</option>
-                            </select>
-                            <input
-                                required
-                                type='number'
-                                inputMode='numeric'
-                                id='phoneNumber'
-                                placeholder='Phone Number'
-                                className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
-                            />
-                        </div>
-                    </div>
+                    
+                   
                     <Select
                         label='Gender'
                         state={['Male', 'Female']}
                         selectedState={selectedGender}
                         setSelectedState={setSelectedGender}
                     />
-
-                    <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
-                        <label
-                            htmlFor='photoUpload'
-                            className='flex justify-center gap-4 items-center cursor-pointer'
-                        >
-                            <img src='/icons/admins/photo_library.svg' alt='' />
-                            <p
-                                className='text-color-dark-1'
-                                style={{
-                                    fontFamily: 'Satoshi-Light',
-                                }}
-                            >
-                                Drag estate manager picture here or{' '}
-                                <span className='text-color-blue font-bold'>
-                                    click
-                                </span>{' '}
-                                to upload
-                            </p>
-                        </label>
-                        <input
-                            type='file'
-                            name='photoUpload'
-                            id='photoUpload'
-                            accept='image/*'
-                            className='hidden'
-                            onClick={handlePhotoPreview}
-                        />
-
-                        {photoUrl && (
-                            <div className='flex justify-center justify-self-center'>
-                                <img
-                                    src={photoUrl}
-                                    alt='photoPreview'
-                                    className='object-cover w-[11rem] h-[11rem] rounded-full'
-                                />
-                            </div>
-                        )}
-                    </div>
 
                     <button className='btn text-white bg-color-blue-1 py-4 px-16 rounded-lg w-[15rem]'>
                         Next
