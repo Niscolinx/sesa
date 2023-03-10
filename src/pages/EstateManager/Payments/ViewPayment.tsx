0f8341760a9a5f3f3de4f08b08eaa856d7e7a36c
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 04:45:58 +0100

    working on payments

--- src/pages/EstateManager/Payments/ViewPayment.tsx
+++ src/pages/EstateManager/Payments/ViewPayment.tsx
@@ -301,7 +301,7 @@ const handleSelectedTrend = (item: Trend) => {
                             )}
                         </div>
                     </div>
-                    <div>
+                    <div className='w-full justify-self-center'>
 
                     <WalletBarChart width={800}/>
                     </div>
