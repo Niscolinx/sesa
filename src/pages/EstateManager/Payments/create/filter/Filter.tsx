ddb8e78c77168ad62866e5c883b63147dae67222
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 14:31:10 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/filter/Filter.tsx
+++ src/pages/EstateManager/Payments/create/filter/Filter.tsx
@@ -50,10 +50,9 @@ const Filter: FC = () => {
                                 }}
                             />
                             <p className='text-[1.6rem]'>
-                                {
-                                    console.log({filter}, typeof filter)
-                                }
-                                You have successfully added a { filter && filter?.substring(0)}
+                                You have successfully added a{' '}
+                                {filter &&
+                                    filter.substring(0, filter.length - 1)}
                             </p>
                             <div className='flex w-full justify -center gap-8'>
                                 <button
