084a953c48bb6b4f4a4bbb143a95096a450ff71c
Igboanugwo Collins <munisco12@gmail.com>
2023-03-10 10:08:28 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/CreatePayment.tsx
+++ src/pages/EstateManager/Payments/create/CreatePayment.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import { Select } from '../../../../components/SuperAdmin/UI/Select'
 import HouseholdFilter from './filter/Household'
+import FlexiblePaymentType from './paymentType/flexible/FlexiblePaymentType'
 
 export type PaymentType = 'fixed' | 'flexible'
 export type PaymentPlan = 'full' | 'installment'
@@ -23,28 +24,22 @@ const CreatePayment = () => {
         e.preventDefault()
     }
 
-    const renderPaymentType = new Map([['fixed', <HouseholdFilter/>]]) satisfies Map<
-        PaymentType,
-        JSX.Element
-    >
+    const renderPaymentType = new Map([
+        ['fixed', <HouseholdFilter />],
+    ]) satisfies Map<PaymentType, JSX.Element>
 
-    const displayStep = new Map([['initial', <HouseholdFilter/>]]) satisfies Map<
-        Step,
-        JSX.Element
-    >
+    const displayStep = new Map([
+        ['initial', <HouseholdFilter />],
+    ]) satisfies Map<Step, JSX.Element>
 
-    const handleNext = () => {
-
-    }
-
-    
+    const handleNext = () => {}
 
     return (
         <>
             <ToastContainer />
 
             <div className='grid p-8 bg-white h-[80vh] items-baseline overflow-y-scroll rounded-lg'>
-               
+                <FlexiblePaymentType handleNext={handleNext} />
             </div>
         </>
     )
