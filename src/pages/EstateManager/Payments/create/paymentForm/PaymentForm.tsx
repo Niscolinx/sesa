86d7517c9d5aabfc4c6c22797e79be5eeac7ad79
Igboanugwo Collins <munisco12@gmail.com>
2023-03-11 16:48:37 +0100

    working on create payment

--- src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
+++ src/pages/EstateManager/Payments/create/paymentForm/PaymentForm.tsx
@@ -394,7 +394,7 @@ function PaymentForm({ props }: PaymentForm) {
                         ))}
 
                         <div className='flex items-center gap-4 my-5 font-Satoshi-Medium '>
-                            {balanceAmount === -1 ? (
+                            {balanceAmount < 0 ? (
                                 <span className='border border-red-600 py-6 px-6'>
                                     Installment is greater than total Amount to
                                     pay
