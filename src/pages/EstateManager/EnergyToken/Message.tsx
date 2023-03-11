28a1617d04d48af2e77d83f71ccd7041cacb3d6d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-11 16:58:42 +0100

    working on energy token

---
+++ src/pages/EstateManager/EnergyToken/Message.tsx
@@ -0,0 +1,47 @@
+import { useAtom } from 'jotai'
+import { useState } from 'react'
+import { IoMdAdd } from 'react-icons/io'
+import { useNavigate } from 'react-router'
+import RenderMessages from './RenderMessages'
+
+function Message() {
+    const navigate = useNavigate()
+
+    const [isMessage, setIsMessage] = useState(false)
+
+    const addMessageHandler = () => {
+        setIsMessage(true)
+        //navigate('/superAdmin/artisan/add')
+    }
+
+    return (
+        <div>
+           
+            <div className='rounded-lg mt-[3rem] h-[80vh]'>
+                {isMessage ? (
+                    <section>
+                       <RenderMessages/>
+                    </section>
+                ) : (
+                    <section className='grid  place-content-center w-full h-full justify-items-center gap-4 bg-white rounded-lg'>
+                        <img src='/icons/admins/errorSvg.svg' alt='' />
+                        <p className='text'>
+                            Ooops you have not added any Message yet
+                        </p>
+                        <button
+                            className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg'
+                            onClick={addMessageHandler}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add Message
+                        </button>
+                    </section>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Message
