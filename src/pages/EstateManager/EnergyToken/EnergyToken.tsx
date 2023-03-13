98c8bc4d185f8f4cc8db05c1f247c0849526125b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 17:38:17 +0100

    working on bulk upload

--- src/pages/EstateManager/EnergyToken/EnergyToken.tsx
+++ src/pages/EstateManager/EnergyToken/EnergyToken.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
-
+import TokenList from './TokenList'
 
 function EnergyToken() {
-
     type Path = 'tokenList' | 'tokenPurchase'
     const [isEnergyToken, setIsEnergyToken] = useState(false)
     const [currentPath, setCurrentPath] = useState<Path>('tokenList')
@@ -12,69 +11,61 @@ function EnergyToken() {
         setIsEnergyToken(true)
     }
 
-   
-
-
-
-    
-
-     const paths = new Map<Path, JSX.Element>([
-         ['tokenList', <></> ],
-         ['tokenPurchase', <></> ],
-     ])
+    const paths = new Map<Path, JSX.Element>([
+        ['tokenList', <TokenList/>],
+        ['tokenPurchase', <></>],
+    ])
 
     return (
         <div>
             <div className='rounded-lg min-h-[80vh] bg-white p-8'>
                 {isEnergyToken ? (
                     <section>
-                        <section>
-                            <div className='estateDetail__radioBox' style={{
+                        <div
+                            className='estateDetail__radioBox'
+                            style={{
                                 marginTop: 0,
-                                marginBottom: '2rem'
-                            }}>
-                                <input
-                                    type='radio'
-                                    name='token'
-                                    id='tokenList'
-                                    className='hidden'
-                                    onChange={() => setCurrentPath('tokenList')}
-                                    defaultChecked
-                                />
-                                <label
-                                    htmlFor='tokenList'
-                                    className={` ${
-                                        currentPath === 'tokenList'
-                                            ? 'font-Satoshi-Medium'
-                                            : 'capitalize'
-                                    }`}
-                                >
-                                    Token List
-                                </label>
+                                marginBottom: '2rem',
+                            }}
+                        >
+                            <input
+                                type='radio'
+                                name='token'
+                                id='tokenList'
+                                className='hidden'
+                                onChange={() => setCurrentPath('tokenList')}
+                                defaultChecked
+                            />
+                            <label
+                                htmlFor='tokenList'
+                                className={` ${
+                                    currentPath === 'tokenList'
+                                        ? 'font-Satoshi-Medium'
+                                        : 'capitalize'
+                                }`}
+                            >
+                                Token List
+                            </label>
 
-                                <input
-                                    type='radio'
-                                    name='token'
-                                    id='tokenPurchase'
-                                    className='hidden'
-                                    onChange={() =>
-                                        setCurrentPath('tokenPurchase')
-                                    }
-                                />
-                                <label
-                                    htmlFor='tokenPurchase'
-                                    className={` ${
-                                        currentPath === 'tokenPurchase'
-                                            ? 'font-Satoshi-Medium'
-                                            : 'capitalize'
-                                    }`}
-                                >
-                                    Token Purchase
-                                </label>
-                            </div>
-                            <div>{paths.get(currentPath)}</div>
-                        </section>
-                        
+                            <input
+                                type='radio'
+                                name='token'
+                                id='tokenPurchase'
+                                className='hidden'
+                                onChange={() => setCurrentPath('tokenPurchase')}
+                            />
+                            <label
+                                htmlFor='tokenPurchase'
+                                className={` ${
+                                    currentPath === 'tokenPurchase'
+                                        ? 'font-Satoshi-Medium'
+                                        : 'capitalize'
+                                }`}
+                            >
+                                Token Purchase
+                            </label>
+                        </div>
+                        <div>{paths.get(currentPath)}</div>
                     </section>
                 ) : (
                     <section className='grid place-content-center w-full h-[80vh] justify-items-center gap-4 bg-white rounded-lg'>
