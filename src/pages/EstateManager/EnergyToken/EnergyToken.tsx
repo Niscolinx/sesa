0f8531db85062df652bc2d9b54898470313d4745
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 05:14:36 +0100

    working on energy token

--- src/pages/EstateManager/EnergyToken/EnergyToken.tsx
+++ src/pages/EstateManager/EnergyToken/EnergyToken.tsx
@@ -65,7 +65,7 @@ function EnergyToken() {
                         </div>
                     </section>
                 ) : (
-                    <section className='grid place-content-center w-full h-full justify-items-center gap-4 bg-white rounded-lg'>
+                    <section className='grid place-content-center w-full h-[80vh] justify-items-center gap-4 bg-white rounded-lg'>
                         <img src='/icons/admins/errorSvg.svg' alt='' />
                         <p className='text'>
                             Ooops you have not added any Energy Token yet
