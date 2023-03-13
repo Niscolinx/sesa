69c87b9bf4fbcbf4fed3cd9ebe973be0e38681cf
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 11:53:26 +0100

    working on bulk upload

--- src/pages/EstateManager/EnergyToken/bulkUpload.tsx
+++ src/pages/EstateManager/EnergyToken/bulkUpload.tsx
@@ -112,6 +112,47 @@ const BulkUpload = () => {
                                     </div>
                                 </div>
                             </section>
+                            <section>
+                                <p className='text-[2rem] border-b pb-2 mb-10'>
+                                    Denomination (N15,000)
+                                </p>
+                                <div
+                                    className='grid gap-16 items-baseline'
+                                    style={{
+                                        gridTemplateColumns:
+                                            ' repeat(auto-fit, minmax(35rem, 1fr))',
+                                    }}
+                                >
+                                    <div className='grid gap-4 relative '>
+                                        <label
+                                            htmlFor='firstName'
+                                            className='text-[1.4rem] font-medium'
+                                        >
+                                            Token Seriel Number
+                                        </label>
+                                        <input
+                                            type='text'
+                                            required
+                                            id='firstName'
+                                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                        />
+                                    </div>
+                                    <div className='grid gap-4 relative '>
+                                        <label
+                                            htmlFor='lastName'
+                                            className='text-[1.4rem] font-medium'
+                                        >
+                                            Token Code
+                                        </label>
+                                        <input
+                                            type='text'
+                                            required
+                                            id='lastName'
+                                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                        />
+                                    </div>
+                                </div>
+                            </section>
                             <div
                                 className='grid gap-16 items-baseline'
                                 style={{
