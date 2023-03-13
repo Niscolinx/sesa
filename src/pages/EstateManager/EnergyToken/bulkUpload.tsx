c248cc61d4ed4ae437e6918a6bcfbf745a2e75b4
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 11:51:31 +0100

    working on bulk upload

--- src/pages/EstateManager/EnergyToken/bulkUpload.tsx
+++ src/pages/EstateManager/EnergyToken/bulkUpload.tsx
@@ -71,36 +71,47 @@ const BulkUpload = () => {
                             onSubmit={handleSubmit}
                             className='grid max-w-[84rem] gap-16 mt-12 '
                         >
-                            <div className='grid gap-4 relative '>
-                                <label
-                                    htmlFor='firstName'
-                                    className='text-[1.4rem] font-medium'
+                            <section>
+
+                            <div
+                                className='grid gap-16 items-baseline'
+                                style={{
+                                    gridTemplateColumns:
+                                    ' repeat(auto-fit, minmax(35rem, 1fr))',
+                                }}
                                 >
-                                    Token Seriel Number
-                                </label>
-                                <input
-                                    type='text'
-                                    required
-                                    id='firstName'
-                                    className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                                />
+                                <div className='grid gap-4 relative '>
+                                    <label
+                                        htmlFor='firstName'
+                                        className='text-[1.4rem] font-medium'
+                                        >
+                                        Token Seriel Number
+                                    </label>
+                                    <input
+                                        type='text'
+                                        required
+                                        id='firstName'
+                                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                        />
+                                </div>
+                                <div className='grid gap-4 relative '>
+                                    <label
+                                        htmlFor='lastName'
+                                        className='text-[1.4rem] font-medium'
+                                        >
+                                        Token Code
+                                    </label>
+                                    <input
+                                        type='text'
+                                        required
+                                        id='lastName'
+                                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                        />
+                                </div>
                             </div>
-                            <div className='grid gap-4 relative '>
-                                <label
-                                    htmlFor='lastName'
-                                    className='text-[1.4rem] font-medium'
-                                >
-                                    Token Code
-                                </label>
-                                <input
-                                    type='text'
-                                    required
-                                    id='lastName'
-                                    className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                                />
-                            </div>
-                           
-                            <div className='grid gap-16 items-baseline'
+                            </section>
+                            <div
+                                className='grid gap-16 items-baseline'
                                 style={{
                                     gridTemplateColumns:
                                         ' repeat(auto-fit, minmax(35rem, 1fr))',
