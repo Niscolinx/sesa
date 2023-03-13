fe20a0bdea5bd90c47ca609ca10b40c1c676c168
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 05:22:55 +0100

    working on energy token

--- src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
+++ src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
@@ -110,43 +110,14 @@ const AddEnergyToken = () => {
                                 />
                             </div>
                         </div>
-                        <div className='grid gap-4 relative '>
-                            <label
-                                htmlFor='lastName'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Date of Birth
-                            </label>
-                            <input
-                                type='text'
-                                required
-                                id='lastName'
-                                className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                            />
-                        </div>
+                        <Select
+                            label='Gender'
+                            state={['Male', 'Female']}
+                            selectedState={selectedGender}
+                            setSelectedState={setSelectedGender}
+                        />
 
-                        <div className='grid gap-4'>
-                            <label
-                                htmlFor='phoneNumber'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Phone Number *
-                            </label>
-
-                            <div className='flex text-[1.6rem] gap-4   h-[5rem]'>
-                                <select className='w-[30%] rounded-lg border border-color-grey py-4.8 px-4 outline-none cursor-pointer text-color-dark relative h-full'>
-                                    <option value='234'>+234</option>
-                                </select>
-                                <input
-                                    required
-                                    type='number'
-                                    inputMode='numeric'
-                                    id='phoneNumber'
-                                    placeholder='Phone Number'
-                                    className='w-full rounded-lg border border-color-grey py-4.8 px-8 outline-none text-color-dark'
-                                />
-                            </div>
-                        </div>
+                       
                         <Select
                             label='Gender'
                             state={['Male', 'Female']}
