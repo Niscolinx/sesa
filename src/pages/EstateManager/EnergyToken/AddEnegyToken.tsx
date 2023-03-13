b50555d0f22d108e238185aed87bfa2c5863bf2a
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 05:21:40 +0100

    working on energy token

--- src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
+++ src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
@@ -44,7 +44,7 @@ const AddEnergyToken = () => {
         <>
             <main>
                 <section className='grid p-8 bg-white items-baseline rounded-lg'>
-                    <button className=' mb-5 ml-auto border border-color-blue text-color-blue-1 font-Satoshi-Medium'>
+                    <button className=' mb-5 ml-auto border border-color-blue text-color-blue-1 font-Satoshi-Medium py-4 px-6 rounded-2xl w-[20rem]'>
                         Bulk Upload
                     </button>
                     <p className='text-[2rem] font-Satoshi-Medium'>
