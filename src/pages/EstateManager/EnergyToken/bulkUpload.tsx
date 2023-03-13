c6d5dfe5af42b0a53d645a51f7063125d253ec5e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 11:50:00 +0100

    working on bulk upload

--- src/pages/EstateManager/EnergyToken/bulkUpload.tsx
+++ src/pages/EstateManager/EnergyToken/bulkUpload.tsx
@@ -99,31 +99,8 @@ const BulkUpload = () => {
                                     className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                                 />
                             </div>
-                            <div className='grid gap-4'>
-                                <label
-                                    htmlFor='amount'
-                                    className='text-[1.4rem] font-medium'
-                                >
-                                    Denomination
-                                </label>
-                                <div className='relative rounded-lg border border-color-grey outline-none flex items-center pl-4'>
-                                    <input
-                                        type='text'
-                                        required
-                                        id='denomination'
-                                        value={denomination}
-                                        onChange={denominationHandler}
-                                        name='amount'
-                                        className='w-full border-none outline-none py-4 px-4 pl-5'
-                                    />
-                                    <img
-                                        src='/icons/Naira.svg'
-                                        alt=''
-                                        className='absolute'
-                                    />
-                                </div>
-                            </div>
-                            <div
+                           
+                            <div className='grid gap-16 items-baseline'
                                 style={{
                                     gridTemplateColumns:
                                         ' repeat(auto-fit, minmax(35rem, 1fr))',
