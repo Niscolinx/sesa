bcf576a441e8f63e5248916781f31df781f872f5
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 05:48:41 +0100

    working on energy token

--- src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
+++ src/pages/EstateManager/EnergyToken/AddEnegyToken.tsx
@@ -183,7 +183,7 @@ const AddEnergyToken = () => {
                             </p>
                         </div>
 
-                        <button className='btn  bg-color-blue-1 text-white rounded-lg py-4 px-8 capitalize col-span-full mt-[5rem]'>
+                        <button className='btn  bg-color-blue-1 text-white flex gap-2 items-center  rounded-lg py-4 px-8 capitalize col-span-full mt-[5rem] justify-start'>
                             <span>
                                 <IoMdAdd />
                             </span>{' '}
