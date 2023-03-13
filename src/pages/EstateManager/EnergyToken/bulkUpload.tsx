eb3c2cbdc90b7578d7041058b413920c0e9e21fd
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 05:42:01 +0100

    working on energy token

--- src/pages/EstateManager/EnergyToken/bulkUpload.tsx
+++ src/pages/EstateManager/EnergyToken/bulkUpload.tsx
@@ -63,143 +63,183 @@ const BulkUpload = () => {
     return (
         <>
             <main>
-                <section className='grid p-8 bg-white items-baseline rounded-lg'>
-                    <Link to='/estateManager/energy-token/bulk-upload'>
-                        <button className=' mb-5 ml-auto border border-color-blue text-color-blue-1 font-Satoshi-Medium py-4 px-6 rounded-2xl w-[20rem]'>
-                            Bulk Upload
-                        </button>
-                    </Link>
-                    <p className='text-[2rem] font-Satoshi-Medium'>
-                        Token Details
-                    </p>
-                    <form
-                        onSubmit={handleSubmit}
-                        className='grid max-w-[84rem] gap-16 mt-12 '
-                        style={{
-                            gridTemplateColumns:
-                                ' repeat(auto-fit, minmax(35rem, 1fr))',
-                        }}
-                    >
-                        <div className='grid gap-4 relative '>
-                            <label
-                                htmlFor='firstName'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Token Seriel Number
-                            </label>
-                            <input
-                                type='text'
-                                required
-                                id='firstName'
-                                className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                            />
-                        </div>
-                        <div className='grid gap-4 relative '>
-                            <label
-                                htmlFor='lastName'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Token Code
-                            </label>
-                            <input
-                                type='text'
-                                required
-                                id='lastName'
-                                className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                            />
-                        </div>
-                        <div className='grid gap-4'>
-                            <label
-                                htmlFor='amount'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Denomination
-                            </label>
-                            <div className='relative rounded-lg border border-color-grey outline-none flex items-center pl-4'>
+                {isUploaded ? (
+                    <section className='grid p-8 bg-white items-baseline rounded-lg'>
+                        <Link to='/estateManager/energy-token/bulk-upload'>
+                            <button className=' mb-5 ml-auto border border-color-blue text-color-blue-1 font-Satoshi-Medium py-4 px-6 rounded-2xl w-[20rem]'>
+                                Bulk Upload
+                            </button>
+                        </Link>
+                        <p className='text-[2rem] font-Satoshi-Medium'>
+                            Token Details
+                        </p>
+                        <form
+                            onSubmit={handleSubmit}
+                            className='grid max-w-[84rem] gap-16 mt-12 '
+                            style={{
+                                gridTemplateColumns:
+                                    ' repeat(auto-fit, minmax(35rem, 1fr))',
+                            }}
+                        >
+                            <div className='grid gap-4 relative '>
+                                <label
+                                    htmlFor='firstName'
+                                    className='text-[1.4rem] font-medium'
+                                >
+                                    Token Seriel Number
+                                </label>
                                 <input
                                     type='text'
                                     required
-                                    id='denomination'
-                                    value={denomination}
-                                    onChange={denominationHandler}
-                                    name='amount'
-                                    className='w-full border-none outline-none py-4 px-4 pl-5'
-                                />
-                                <img
-                                    src='/icons/Naira.svg'
-                                    alt=''
-                                    className='absolute'
+                                    id='firstName'
+                                    className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                                 />
                             </div>
-                        </div>
-                        <Select
-                            label='Customer Notification'
-                            state={['SMS', 'In-App']}
-                            selectedState={customerNofication}
-                            setSelectedState={setCustomerNotification}
+                            <div className='grid gap-4 relative '>
+                                <label
+                                    htmlFor='lastName'
+                                    className='text-[1.4rem] font-medium'
+                                >
+                                    Token Code
+                                </label>
+                                <input
+                                    type='text'
+                                    required
+                                    id='lastName'
+                                    className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                />
+                            </div>
+                            <div className='grid gap-4'>
+                                <label
+                                    htmlFor='amount'
+                                    className='text-[1.4rem] font-medium'
+                                >
+                                    Denomination
+                                </label>
+                                <div className='relative rounded-lg border border-color-grey outline-none flex items-center pl-4'>
+                                    <input
+                                        type='text'
+                                        required
+                                        id='denomination'
+                                        value={denomination}
+                                        onChange={denominationHandler}
+                                        name='amount'
+                                        className='w-full border-none outline-none py-4 px-4 pl-5'
+                                    />
+                                    <img
+                                        src='/icons/Naira.svg'
+                                        alt=''
+                                        className='absolute'
+                                    />
+                                </div>
+                            </div>
+                            <Select
+                                label='Customer Notification'
+                                state={['SMS', 'In-App']}
+                                selectedState={customerNofication}
+                                setSelectedState={setCustomerNotification}
+                            />
+
+                            <div className='grid gap-4'>
+                                <label
+                                    htmlFor='convenienceFee'
+                                    className='text-[1.4rem] font-medium'
+                                >
+                                    Convenience Fee
+                                </label>
+                                <div className='relative rounded-lg border border-color-grey outline-none flex items-center pl-4'>
+                                    <input
+                                        type='text'
+                                        required
+                                        id='convenienceFee'
+                                        value={convenienceFee}
+                                        onChange={convenienceFeeHandler}
+                                        name='convenienceFee'
+                                        className='w-full border-none outline-none py-4 px-4 pl-5'
+                                    />
+                                    <img
+                                        src='/icons/Naira.svg'
+                                        alt=''
+                                        className='absolute'
+                                    />
+                                </div>
+                            </div>
+                            <div className='grid gap-4 relative'>
+                                <label
+                                    htmlFor='instruction'
+                                    className='text-[1.4rem] font-medium'
+                                >
+                                    Instuction/Message *
+                                </label>
+                                <input
+                                    type='instruction'
+                                    required
+                                    id='instruction'
+                                    className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                                />
+                            </div>
+
+                            <div>
+                                <Select
+                                    label='Notification Threshold'
+                                    state={['10%', '20%', '30%', '40%', '50%']}
+                                    selectedState={notificationThreshold}
+                                    setSelectedState={setNotificationThreshold}
+                                />
+                                <p className='text-color-blue-1 font-Satoshi-Light text-[1.2rem]'>
+                                    This is the threshold the system
+                                    automatically sends a notification
+                                </p>
+                            </div>
+
+                            <button className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'>
+                                <span>
+                                    <IoMdAdd />
+                                </span>{' '}
+                                <p>Create Token</p>
+                            </button>
+                        </form>
+                    </section>
+                ) : (
+                    <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
+                        <label
+                            htmlFor='photoUpload'
+                            className='flex justify-center gap-4 items-center cursor-pointer'
+                        >
+                            <img src='/icons/admins/photo_library.svg' alt='' />
+                            <p
+                                className='text-color-dark-1'
+                                style={{
+                                    fontFamily: 'Satoshi-Light',
+                                }}
+                            >
+                                Drag picture here{' '}
+                                <span className='text-color-blue font-bold'>
+                                    click
+                                </span>{' '}
+                                to upload
+                            </p>
+                        </label>
+                        <input
+                            type='file'
+                            name='photoUpload'
+                            id='photoUpload'
+                            accept='image/*'
+                            className='hidden'
+                            onClick={handlePhotoPreview}
                         />
 
-                        <div className='grid gap-4'>
-                            <label
-                                htmlFor='convenienceFee'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Convenience Fee
-                            </label>
-                            <div className='relative rounded-lg border border-color-grey outline-none flex items-center pl-4'>
-                                <input
-                                    type='text'
-                                    required
-                                    id='convenienceFee'
-                                    value={convenienceFee}
-                                    onChange={convenienceFeeHandler}
-                                    name='convenienceFee'
-                                    className='w-full border-none outline-none py-4 px-4 pl-5'
-                                />
+                        {photoUrl && (
+                            <div className='flex justify-center justify-self-center'>
                                 <img
-                                    src='/icons/Naira.svg'
-                                    alt=''
-                                    className='absolute'
+                                    src={photoUrl}
+                                    alt='photoPreview'
+                                    className='object-cover w-[11rem] h-[11rem] rounded-full'
                                 />
                             </div>
-                        </div>
-                        <div className='grid gap-4 relative'>
-                            <label
-                                htmlFor='instruction'
-                                className='text-[1.4rem] font-medium'
-                            >
-                                Instuction/Message *
-                            </label>
-                            <input
-                                type='instruction'
-                                required
-                                id='instruction'
-                                className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                            />
-                        </div>
-
-                        <div>
-                            <Select
-                                label='Notification Threshold'
-                                state={['10%', '20%', '30%', '40%', '50%']}
-                                selectedState={notificationThreshold}
-                                setSelectedState={setNotificationThreshold}
-                            />
-                            <p className='text-color-blue-1 font-Satoshi-Light text-[1.2rem]'>
-                                This is the threshold the system automatically
-                                sends a notification
-                            </p>
-                        </div>
-
-
-                        <button className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'>
-                            <span>
-                                <IoMdAdd />
-                            </span>{' '}
-                            <p>Create Token</p>
-                        </button>
-                    </form>
-                </section>
+                        )}
+                    </div>
+                )}
             </main>
         </>
     )
