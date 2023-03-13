06c5c5084b4b10fcc4570ae777db00792726dcd0
Igboanugwo Collins <munisco12@gmail.com>
2023-03-13 17:36:36 +0100

    working on bulk upload

--- src/pages/EstateManager/EnergyToken/EnergyToken.tsx
+++ src/pages/EstateManager/EnergyToken/EnergyToken.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
-import { useNavigate } from 'react-router'
-import { Link } from 'react-router-dom'
-import { EstateChart as Chart } from '../../../components/SuperAdmin/charts/OverviewChart'
+
 
 function EnergyToken() {
 
@@ -79,53 +77,7 @@ function EnergyToken() {
                             </div>
                             <div>{paths.get(currentPath)}</div>
                         </section>
-                        <Link to='/estateManager/energy-token/add'>
-                            <button className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'>
-                                <span>
-                                    <IoMdAdd />
-                                </span>{' '}
-                                <p>Add Token</p>
-                            </button>
-                        </Link>
-                        <div
-                            className='grid p-8 justify-items-center'
-                            style={{
-                                gridTemplateColumns:
-                                    'repeat(auto-fit, minmax(25rem, 1fr))',
-                            }}
-                        >
-                            {Array.from({ length: 7 }, (_, i) => (
-                                <div key={i}>
-                                    <div className='overviewChart__box'>
-                                        <Chart
-                                            color1='#098DFF'
-                                            color2='#C5C1C3'
-                                            outerRadius={90}
-                                            data={property_data}
-                                        />
-
-                                        <div className='overviewChart__label'>
-                                            <p className='text-[3rem] font-Satoshi-Medium relative text-[#FF0000]'>
-                                                85%
-                                            </p>
-                                            <p className='text-[1.2rem] max-w-[9.8rem]'>
-                                                count: XXXXX
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className=' grid justify-items-center'>
-                                        <p className='flex items-center'>
-                                            <img
-                                                src='/icons/Naira.svg'
-                                                alt=''
-                                            />
-                                            <span>5,000</span>
-                                        </p>
-                                        <p>Denomination</p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+                        
                     </section>
                 ) : (
                     <section className='grid place-content-center w-full h-[80vh] justify-items-center gap-4 bg-white rounded-lg'>
---
+++ src/pages/EstateManager/EnergyToken/TokenList.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
+import { EstateChart as Chart } from '../../../components/SuperAdmin/charts/OverviewChart'
+
+function TokenList() {
+  return (
+      <div>
+          <Link to='/estateManager/energy-token/add'>
+              <button className='btn ml-auto bg-color-blue-1 text-white flex gap-2 items-center self-center rounded-lg py-4 px-8 capitalize'>
+                  <span>
+                      <IoMdAdd />
+                  </span>{' '}
+                  <p>Add Token</p>
+              </button>
+          </Link>
+          <div
+              className='grid p-8 justify-items-center'
+              style={{
+                  gridTemplateColumns: 'repeat(auto-fit, minmax(25rem, 1fr))',
+              }}
+          >
+              {Array.from({ length: 7 }, (_, i) => (
+                  <div key={i}>
+                      <div className='overviewChart__box'>
+                          <Chart
+                              color1='#098DFF'
+                              color2='#C5C1C3'
+                              outerRadius={90}
+                              data={property_data}
+                          />
+
+                          <div className='overviewChart__label'>
+                              <p className='text-[3rem] font-Satoshi-Medium relative text-[#FF0000]'>
+                                  85%
+                              </p>
+                              <p className='text-[1.2rem] max-w-[9.8rem]'>
+                                  count: XXXXX
+                              </p>
+                          </div>
+                      </div>
+                      <div className=' grid justify-items-center'>
+                          <p className='flex items-center'>
+                              <img src='/icons/Naira.svg' alt='' />
+                              <span>5,000</span>
+                          </p>
+                          <p>Denomination</p>
+                      </div>
+                  </div>
+              ))}
+          </div>
+      </div>
+  )
+}
+
+export default TokenList
