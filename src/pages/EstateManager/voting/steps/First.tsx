0884ff79cf4c01cf9374be76aecf377a23e0c093
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 05:38:14 +0100

    working voting and election

--- src/pages/EstateManager/voting/steps/First.tsx
+++ src/pages/EstateManager/voting/steps/First.tsx
@@ -31,25 +31,25 @@ function First() {
                         className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                     />
                 </div>
-                <div className='flex items-center gap-8 w-full'>
-                    <div className='grid gap-4 relative w-[40rem]'>
-                        <label
-                            htmlFor='firstName'
-                            className='text-[1.4rem] font-medium'
-                        >
-                            Election Category
-                        </label>
+                <div className='grid gap-4 relative'>
+                    <label
+                        htmlFor='firstName'
+                        className='text-[1.4rem] font-medium'
+                    >
+                        Election Category
+                    </label>
+                    <div className='flex items-center gap-8 w-full'>
                         <input
                             type='text'
                             required
                             id='firstName'
                             placeholder='placeholder'
-                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                            className=' rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 w-[40rem]'
                         />
+                        <button>
+                            <img src='/icons/add_Icon.svg' alt='' />
+                        </button>
                     </div>
-                    <button>
-                        <img src='/icons/add_Icon.svg' alt='' />
-                    </button>
                 </div>
             </form>
         </div>
