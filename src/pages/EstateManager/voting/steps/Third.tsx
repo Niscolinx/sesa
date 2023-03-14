1e6712a8bf3aaed9bba58b5ab99061bf29455a0d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 12:41:49 +0100

    working on election

---
+++ src/pages/EstateManager/voting/steps/Third.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent } from 'react'
+import { useCreateElectionContext } from '../createElection'
+
+function Third() {
+    const { electionCategory, setElectionCategory } = useCreateElectionContext()
+
+    const add_another_category = () => {
+        setElectionCategory((prev) => [...prev, ''])
+    }
+
+    const handle_category_change = (
+        e: ChangeEvent<HTMLInputElement>,
+        idx: number
+    ) => {
+        setElectionCategory((prev) => {
+            const updatedArr = [...prev]
+            updatedArr[idx] = e.target.value
+
+            return updatedArr
+        })
+    }
+
+    return (
+        <div>
+            <form className='grid max-w-[50rem] gap-16 items-start content-start capitalize'>
+                <div className='grid gap-4 relative w-[40rem]'>
+                    <label
+                        htmlFor='thirdName'
+                        className='text-[1.4rem] font-medium'
+                    >
+                        Election Title
+                    </label>
+                    <input
+                        type='text'
+                        required
+                        id='thirdName'
+                        placeholder='placeholder'
+                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                    />
+                </div>
+
+               
+            </form>
+        </div>
+    )
+}
+
+export default Third
