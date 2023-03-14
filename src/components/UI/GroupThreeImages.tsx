9ef63d6798fa620263b09fded84d2269607502a0
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:32:55 +0100

    working on election

--- src/components/UI/GroupThreeImages.tsx
+++ src/components/UI/GroupThreeImages.tsx
@@ -6,19 +6,20 @@ interface GroupThreeImages {
 
 const imgArr = ['/img/avatar1.png', '/img/avatar2.png', '/img/avatar3.png']
 
-
-function GroupThreeImages({ images = imgArr}: GroupThreeImages) {
+function GroupThreeImages({ images = imgArr }: GroupThreeImages) {
     return (
-        <div className='flex gap-16 '>
-            <div className='relative w-[10rem] flex items-center justify-between'>
-                {images.map((item, i) => (
-                    <img
-                        src='/img/avatar1.png'
-                        alt=''
-                        className={`w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-${i * 20} z-${(i + 1) + 10}`}
-                    />
-                ))}
+        <div className='relative w-[10rem] flex items-center justify-between'>
+            {images.map((item, i) => (
                 <img
+                    key={i}
+                    src={item}
+                    alt=''
+                    className={`w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-${
+                        i * 20
+                    } z-${i + 1 + 10}`}
+                />
+            ))}
+            {/* <img
                     src='/img/avatar1.png'
                     alt=''
                     className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-0 z-10'
@@ -33,8 +34,7 @@ function GroupThreeImages({ images = imgArr}: GroupThreeImages) {
                     alt=''
                     className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-[40%] z-30'
                 />
-            </div>
-            <button className='text-color-blue'>View Candidates</button>
+            </div> */}
         </div>
     )
 }
