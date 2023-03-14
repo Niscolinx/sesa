9cdc8ef53d08a6e022c8c428e0dd4f490161e8c2
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 15:16:53 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -3,20 +3,16 @@ import { GroupThreeImages } from '../../../../components/UI/GroupThreeImages'
 import { CandidateField, useCreateElectionContext } from '../createElection'
 
 const Last = () => {
-    const {electionCategory, candidate_details} = useCreateElectionContext()
+    const { electionCategory, candidate_details } = useCreateElectionContext()
     const [candidateImgs, setCandidateImgs] = useState<string[]>([])
 
-    const imgArr = [
-        '/img/avatar1.png',
-        '/img/avatar2.png',
-        '/img/avatar3.png',
-    ]
+    const imgArr = ['/img/avatar1.png', '/img/avatar2.png', '/img/avatar3.png']
 
     useEffect(() => {
-
-        const extractedImgs = candidate_details.reduce((acc: string[], cur ) => (
-            acc.concat(cur.photoUrl)
-        ), [])
+        const extractedImgs = candidate_details.reduce(
+            (acc: string[], cur) => acc.concat(cur.photoUrl),
+            []
+        )
 
         setCandidateImgs(extractedImgs)
     }, [candidate_details])
@@ -34,29 +30,25 @@ const Last = () => {
                 <p className='text-[2rem] font-Satoshi-Medium'>
                     Election Categories
                 </p>
-        {
-            electionCategory && electionCategory.map((election, i) => {
-                return (
-                    <div className='grid gap-8 w-[60rem]'>
-                        <div className='grid grid-cols-2 mt-[5rem] pb-5 border-b w-full'>
-                            <p>{election}</p>
-                            <div className='flex items-center gap-16'>
-                                {
-                                    candidate_details.map((detail, i) => (
-                                        <GroupThreeImages images={detail.photoUrl} />
+                {electionCategory &&
+                    electionCategory.map((election, i) => {
+                        return (
+                            <div className='grid gap-8 w-[60rem]'>
+                                <div className='grid grid-cols-2 mt-[5rem] pb-5 border-b w-full'>
+                                    <p>{election}</p>
+                                    <div className='flex items-center gap-16'>
+                                        <GroupThreeImages
+                                            images={candidateImgs}
+                                        />
 
-                                    ))
-                                }
-                                <button className='text-color-blue'>
-                                    View Candidates
-                                </button>
+                                        <button className='text-color-blue'>
+                                            View Candidates
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                )
-            })
-        }
-                
+                        )
+                    })}
             </section>
         </main>
     )
