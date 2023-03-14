1f59ba649cee62b8b28cdf6729a12a3822e36d93
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 08:16:12 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Second.tsx
+++ src/pages/EstateManager/voting/steps/Second.tsx
@@ -27,7 +27,7 @@ function Second() {
     return (
         <div>
             <form className='grid  gap-16 items-start content-start capitalize'>
-                <div className='grid gap-4 relative w-[40rem]'>
+                <div className='grid gap-4 relative w-[35rem]'>
                     <Select
                         state={[
                             'ALIBA DESMOND (RES CODE: 2345CDGK1)',
@@ -43,32 +43,44 @@ function Second() {
                     />
                 </div>
 
-                {electionCategory.map((item, idx) => (
-                    <div className='grid gap-4 relative' key={idx}>
+                <div
+                    className='grid gap-4'
+                    style={{
+                        gridTemplateColumns:
+                            'repeat(auto-fit, minmax(35rem, 1fr))',
+                    }}
+                >
+                    <div className='grid gap-4 select-none pointer-events-none'>
                         <label
-                            htmlFor='category'
+                            htmlFor='firstName'
                             className='text-[1.4rem] font-medium'
                         >
-                            Election Category
+                            Name
                         </label>
-                        <div className='flex items-center gap-8 w-full'>
-                            <input
-                                type='text'
-                                required
-                                id='category'
-                                placeholder='placeholder'
-                                value={item.value}
-                                onChange={(e) => handle_category_change(e, idx)}
-                                className=' rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4 w-[40rem]'
-                            />
-                            {idx + 1 === electionCategory.length && (
-                                <button onClick={() => add_another_category()}>
-                                    <img src='/icons/add_Icon.svg' alt='' />
-                                </button>
-                            )}
-                        </div>
+                        <input
+                            type='text'
+                            required
+                            id='firstName'
+                            placeholder='placeholder'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                        />
                     </div>
-                ))}
+                    <div className='grid gap-4 select-none pointer-events-none'>
+                        <label
+                            htmlFor='firstName'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Name
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            id='firstName'
+                            placeholder='placeholder'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                        />
+                    </div>
+                </div>
             </form>
         </div>
     )
