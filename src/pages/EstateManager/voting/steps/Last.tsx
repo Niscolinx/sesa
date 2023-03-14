f560877fc71df937d07a064b19131aaf0faf1d4b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:15:03 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -1,32 +1,36 @@
-
-
-
 const Last = () => {
-   
-
     return (
-        <main className='bg-color-white rounded-lg'>
-           
-          <section className='capitalize'>
-            <p className='text-[2rem] font-Satoshi-Medium'>Election Title</p>
-            <p>Peace Estate 2023 General Election</p>
-          </section>
+        <main className='bg-color-white rounded-lg grid gap-8'>
+            <section className='capitalize'>
+                <p className='text-[2rem] font-Satoshi-Medium'>
+                    Election Title
+                </p>
+                <p>Peace Estate 2023 General Election</p>
+            </section>
 
-          <section>
-            <p className='text-[2rem] font-Satoshi-Medium'>Election Categories</p>
+            <section>
+                <p className='text-[2rem] font-Satoshi-Medium'>
+                    Election Categories
+                </p>
 
-            <div className='grid gap-8 max-w-[60rem]'>
-                <div className="grid grid-cols-2">
-                    <p>President</p>
-                    <div className="flex gap-16">
-                    <div className="relative">
-                        <img src="/img/avatar11.png" alt="" className="w-[3rem] h-[3rem] object-cover rounded-full" />
-                    </div>
-                    <button className='text-color-blue'>View Candidates</button>
+                <div className='grid gap-8 max-w-[60rem]'>
+                    <div className='grid grid-cols-2'>
+                        <p>President</p>
+                        <div className='flex gap-16'>
+                            <div className='relative'>
+                                <img
+                                    src='/img/avatar11.png'
+                                    alt=''
+                                    className='w-[3rem] h-[3rem] object-cover rounded-full'
+                                />
+                            </div>
+                            <button className='text-color-blue'>
+                                View Candidates
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-          </section>
+            </section>
         </main>
     )
 }
