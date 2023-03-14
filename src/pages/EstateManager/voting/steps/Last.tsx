7bd33c1f026140df9fbfc1107c1e0189b46aac8e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:22:53 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -17,21 +17,21 @@ const Last = () => {
                     <div className='grid grid-cols-2'>
                         <p>President</p>
                         <div className='flex gap-16'>
-                            <div className='relative bg-red-600 w-[20rem] flex items-center justify-between'>
+                            <div className='relative w-[10rem] flex items-center justify-between'>
                                 <img
                                     src='/img/avatar1.png'
                                     alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full absolute left-0 z-10'
+                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-0 z-10'
                                 />
                                 <img
                                     src='/img/avatar2.png'
                                     alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full absolute left-[10%] z-20'
+                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-[20%] z-20'
                                 />
                                 <img
                                     src='/img/avatar3.png'
                                     alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full absolute left-[20%] z-30'
+                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-[40%] z-30'
                                 />
                             </div>
                             <button className='text-color-blue'>
