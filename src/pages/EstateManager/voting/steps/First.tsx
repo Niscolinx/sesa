7608593db3051c6129b54ce4ac92957a60004b0d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 05:33:36 +0100

    working voting and election

--- src/pages/EstateManager/voting/steps/First.tsx
+++ src/pages/EstateManager/voting/steps/First.tsx
@@ -15,10 +15,7 @@ function First() {
     // }
     return (
         <div>
-            <form
-                className='grid max-w-[40rem] gap-16 items-start content-start capitalize'
-               
-            >
+            <form className='grid max-w-[40rem] gap-16 items-start content-start capitalize'>
                 <div className='grid gap-4 relative '>
                     <label
                         htmlFor='firstName'
@@ -34,21 +31,25 @@ function First() {
                         className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                     />
                 </div>
-
-                <div className='grid gap-4 relative '>
-                    <label
-                        htmlFor='firstName'
-                        className='text-[1.4rem] font-medium'
-                    >
-                        Election Category
-                    </label>
-                    <input
-                        type='text'
-                        required
-                        id='firstName'
-                        placeholder='placeholder'
-                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                    />
+                <div className='flex items-center gap-8'>
+                    <div className='grid gap-4 relative '>
+                        <label
+                            htmlFor='firstName'
+                            className='text-[1.4rem] font-medium'
+                        >
+                            Election Category
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            id='firstName'
+                            placeholder='placeholder'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                        />
+                    </div>
+                    <button>
+                        <img src='/icons/add_Icon.svg' alt='' />
+                    </button>
                 </div>
             </form>
         </div>
