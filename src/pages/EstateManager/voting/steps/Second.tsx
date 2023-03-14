6500aa20cdf59b5fceccb34cc6aa72e1ef1825c1
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 08:17:05 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Second.tsx
+++ src/pages/EstateManager/voting/steps/Second.tsx
@@ -26,7 +26,7 @@ function Second() {
 
     return (
         <div>
-            <form className='grid  gap-16 items-start content-start capitalize'>
+            <div className='grid  gap-16 items-start content-start capitalize'>
                 <div className='grid gap-4 relative w-[35rem]'>
                     <Select
                         state={[
@@ -44,7 +44,7 @@ function Second() {
                 </div>
 
                 <div
-                    className='grid gap-4'
+                    className='grid gap-4 opacity-50'
                     style={{
                         gridTemplateColumns:
                             'repeat(auto-fit, minmax(35rem, 1fr))',
@@ -70,7 +70,7 @@ function Second() {
                             htmlFor='firstName'
                             className='text-[1.4rem] font-medium'
                         >
-                            Name
+                            Gender
                         </label>
                         <input
                             type='text'
@@ -80,8 +80,21 @@ function Second() {
                             className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
                         />
                     </div>
+                    <Select
+                        state={[
+                            'ALIBA DESMOND (RES CODE: 2345CDGK1)',
+                            'ALIBA DESMOND (RES CODE: 2345CDGK2)',
+                            'ALIBA DESMOND (RES CODE: 2345CDGK3)',
+                            'ALIBA DESMOND (RES CODE: 2345CDGK4)',
+                            'ALIBA DESMOND (RES CODE: 2345CDGK5)',
+                        ]}
+                        label='Election Candidates (Select Resident Code)'
+                        isSearchable
+                        selectedState={candidate}
+                        setSelectedState={setCandidate}
+                    />
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
