b5edc95ffb75325ea2979bce2e120c90f1efcbec
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:35:01 +0100

    working on election

--- src/components/UI/GroupThreeImages.tsx
+++ src/components/UI/GroupThreeImages.tsx
@@ -4,20 +4,22 @@ interface GroupThreeImages {
     images: string[]
 }
 
-
 export function GroupThreeImages({ images }: GroupThreeImages) {
     return (
         <div className='relative w-[10rem] flex items-center justify-between'>
-            {images.map((item, i) => (
-                <img
-                    key={i+item}
-                    src={item}
-                    alt=''
-                    className={`w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-${
-                        i * 20
-                    } z-${i + 1 + 10}`}
-                />
-            ))}
+            {images.map((item, i) => {
+                console.log({item})
+                return (
+                    <img
+                        key={i + item}
+                        src={item}
+                        alt=''
+                        className={`w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-${
+                            i * 20
+                        } z-${i + 1 + 10}`}
+                    />
+                )
+            })}
             {/* <img
                     src='/img/avatar1.png'
                     alt=''
