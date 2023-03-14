95422d3f34f66cdbcf92cc13682de89ffc80f4e8
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:26:01 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -16,28 +16,7 @@ const Last = () => {
                 <div className='grid gap-8 max-w-[60rem]'>
                     <div className='grid grid-cols-2 mt-[5rem] pb-5 border-b'>
                         <p>President</p>
-                        <div className='flex gap-16 '>
-                            <div className='relative w-[10rem] flex items-center justify-between'>
-                                <img
-                                    src='/img/avatar1.png'
-                                    alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-0 z-10'
-                                />
-                                <img
-                                    src='/img/avatar2.png'
-                                    alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-[20%] z-20'
-                                />
-                                <img
-                                    src='/img/avatar3.png'
-                                    alt=''
-                                    className='w-[3rem] h-[3rem] object-cover rounded-full border border-color-white absolute left-[40%] z-30'
-                                />
-                            </div>
-                            <button className='text-color-blue'>
-                                View Candidates
-                            </button>
-                        </div>
+                       
                     </div>
                 </div>
             </section>
