a60b4e9bfd826ee128f7c92b96190f2942c3c69c
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:00:21 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -10,37 +10,7 @@ const Last: FC = () => {
 
     return (
         <main className='bg-color-white rounded-lg'>
-            <p className='flex items-center gap-2'>
-                <span>NB: All alphas are denoted with the icon </span>
-                <img src='/img/alpha.svg' alt='' />
-            </p>
-            <div className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
-                <p className=' font-bold'>
-                    Voters List <span>(50)</span>
-                </p>
-                <div className='relative flex items-center'>
-                    <img
-                        src='/icons/admins/search.svg'
-                        alt=''
-                        className='absolute left-4 text-[4rem]'
-                    />
-                    <input
-                        type='text'
-                        placeholder='Search Parameters'
-                        className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
-                    />
-                </div>
-                <div className='relative flex items-center'>
-                    <select className=' cursor-pointer w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4 bg-white'>
-                        <option hidden value=''>
-                            Sort By
-                        </option>
-                        <option value='date'>property</option>
-                        <option value='alpha'>Name</option>
-                    </select>
-                    <GrDown className='absolute right-4 text-[1.3rem]' />
-                </div>
-            </div>
+           
           
         </main>
     )
