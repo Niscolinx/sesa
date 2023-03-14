f33b6dc792d548472c2da58d4f7f5c8bbdad065b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 09:00:45 +0100

    working on election

--- src/pages/EstateManager/voting/createElection.tsx
+++ src/pages/EstateManager/voting/createElection.tsx
@@ -61,7 +61,7 @@ const CreateElection = () => {
                     )}
 
                     <button
-                        className='btn flex items-center border gap-4 border-color-blue-1 justify-self-end text-color-blue-1 font-Satoshi-Medium justify-center rounded-2xl w-[15rem] disabled:opacity-50 disabled:cursor-not-allowed'
+                        className='btn flex items-center border gap-4 border-color-blue-1 justify-self-end text-color-blue-1 font-Satoshi-Medium justify-center rounded-2xl w-[15rem] ml-auto disabled:opacity-50 disabled:cursor-not-allowed'
                         disabled={step === 3 || !electionCategory[0]}
                         onClick={() => setStep((prev) => prev + 1)}
                     >
