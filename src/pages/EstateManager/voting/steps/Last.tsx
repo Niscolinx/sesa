9604146bd6283b190fff8efbe2c0db71e6acebe5
Igboanugwo Collins <munisco12@gmail.com>
2023-03-14 14:02:49 +0100

    working on election

--- src/pages/EstateManager/voting/steps/Last.tsx
+++ src/pages/EstateManager/voting/steps/Last.tsx
@@ -15,6 +15,10 @@ const Last: FC = () => {
             <p className='text-[2rem] font-Satoshi-Medium'>Election Title</p>
             <p>Peace Estate 2023 General Election</p>
           </section>
+
+          <section>
+            <p className='text-[2rem] font-Satoshi-Medium'>Election Categories</p>
+          </section>
         </main>
     )
 }
