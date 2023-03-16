b30831331d98b5968afe1e3100e3a8c2e32a132a
Igboanugwo Collins <munisco12@gmail.com>
2023-03-16 12:22:47 +0100

    working on reports

--- src/pages/EstateManager/Reports/paths/ResidentAccess.tsx
+++ src/pages/EstateManager/Reports/paths/ResidentAccess.tsx
@@ -205,41 +205,11 @@ function ResidentAccess() {
                         <div className='border-l border-l-color-grey bg-white rounded-lg p-8 grid gap-10'>
                             <div className='flex justify-between'>
                                 <p className='text-[1.6rem] font-bold p-8'>
-                                    ResidentAccess Trend
+                                     Trend
                                 </p>
 
-                                <div className='relative grid gap-4'>
-                                    <div className='relative flex items-center w-[12rem]'>
-                                        <p
-                                            className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] cursor-pointer'
-                                            onClick={menuToggler}
-                                        >
-                                            {selectedTrend}
-                                        </p>
-                                        {toggleMenu ? (
-                                            <GrUp className='absolute right-4' />
-                                        ) : (
-                                            <GrDown className='absolute right-4' />
-                                        )}
-                                    </div>
-
-                                    {toggleMenu && (
-                                        <div className='absolute top-[8rem]  left-0 border border-color-primary-light  bg-color-white rounded-lg grid gap-2 shadow z-20 capitalize'>
-                                            {trend.map((item, index) => (
-                                                <p
-                                                    className='text-[1.4rem] hover:bg-color-grey border-b p-4 cursor-pointer'
-                                                    key={index}
-                                                    onClick={() =>
-                                                        handleSelectedTrend(
-                                                            item
-                                                        )
-                                                    }
-                                                >
-                                                    {item}
-                                                </p>
-                                            ))}
-                                        </div>
-                                    )}
+                                <div className='flex items-center gap-4'>
+                                    
                                 </div>
                             </div>
                             {/* <ResidentAccessBarChart /> */}
