1257de208c29403b6e86238ee4468cf26263f7bf
Igboanugwo Collins <munisco12@gmail.com>
2023-03-16 15:06:21 +0100

    working on reports

--- src/pages/EstateManager/Reports/Reports.tsx
+++ src/pages/EstateManager/Reports/Reports.tsx
@@ -6,7 +6,6 @@ import ResidentAccess from './paths/ResidentReport'
 import SiteWorkerReport from './paths/SiteWorkerReport'
 import VisitorReport from './paths/VisitorReport'
 
-
 function Reports() {
     type Path =
         | 'resident_access'
