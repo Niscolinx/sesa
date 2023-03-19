1d162aac81e53bf927c68e0814858240df00932f
Igboanugwo Collins <munisco12@gmail.com>
2023-03-19 21:05:24 +0100

    working on approval details

--- src/pages/EstateManager/approvals/ApprovalDetail.tsx
+++ src/pages/EstateManager/approvals/ApprovalDetail.tsx
@@ -183,7 +183,7 @@ const ApprovalDetail = () => {
                         {Object.entries(approval.requester).map(
                             ([key, value], i) => (
                                 <div key={i} className='grid gap-2 justify-items-start capitalize'>
-                                    <p className='text-gray-500'>{key.replace('_', '  ')}</p>
+                                    <p className='text-gray-500'>{key.replace('_', ' ')}</p>
                                     <p>{value}</p>
                                 </div>
                             )
