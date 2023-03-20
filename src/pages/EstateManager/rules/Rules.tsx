7f8caea71f7013a459bb24fd3e2d644d114a495c
Igboanugwo Collins <munisco12@gmail.com>
2023-03-20 07:26:53 +0100

    working on approval

---
+++ src/pages/EstateManager/rules/Rules.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { IoMdAdd } from 'react-icons/io'
+
+function Rules() {
+    const [isRules, setIsRules] = useState(false)
+
+    const addRulesHandler = () => {
+        setIsRules(true)
+    }
+
+    return (
+        <div className='grid'>
+            <div className='rounded-lg mt-[3rem] h-[80vh]'>
+                {isRules ? (
+                    <section>
+                    </section>
+                ) : (
+                    <section className='grid  place-content-center w-full h-full justify-items-center gap-4 bg-white rounded-lg'>
+                        <img src='/icons/admins/errorSvg.svg' alt='' />
+                        <p className='text'>
+                            Ooops you have not added any Rules yet
+                        </p>
+                        <button
+                            className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg'
+                            onClick={addRulesHandler}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            Add Rules
+                        </button>
+                    </section>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Rules
