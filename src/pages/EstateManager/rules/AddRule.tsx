1406cc6e694e1bd291eb58271d2f19b8993bdd39
Igboanugwo Collins <munisco12@gmail.com>
2023-03-20 12:33:29 +0100

    working on rules and regulations

--- src/pages/EstateManager/rules/AddRule.tsx
+++ src/pages/EstateManager/rules/AddRule.tsx
@@ -14,43 +14,44 @@ const AddRule = () => {
     return (
         <>
             <ToastContainer />
-
-            <form className='grid p-8 bg-white gap-16 rounded-lg'>
-                <div className='grid gap-4 relative '>
-                    <label
-                        htmlFor='ruleTitle'
-                        className='text-[1.4rem] font-Satoshi-Medium'
-                    >
-                        Rule title
-                    </label>
-                    <input
-                        type='text'
-                        required
-                        id='ruleTitle'
-                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                    />
-                </div>
-                <div className='grid gap-4 relative '>
-                    <label
-                        htmlFor='rules_and_regulations'
-                        className='text-[1.4rem] font-Satoshi-Medium'
-                    >
-                        Estate rules and regulations
-                    </label>
-                    <textarea
-                        rows={5}
-                        required
-                        id='rules_and_regulations'
-                        className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
-                    />
-                </div>
-                <button className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg justify-start'>
-                    <span>
-                        <IoMdAdd />
-                    </span>{' '}
-                    Add Rule
-                </button>
-            </form>
+            <section className='p-8 bg-white gap-16 rounded-lg'>
+                <form className='grid p-8 gap-16 max-w-[70rem]'>
+                    <div className='grid gap-4 relative '>
+                        <label
+                            htmlFor='ruleTitle'
+                            className='text-[1.4rem] font-Satoshi-Medium'
+                        >
+                            Rule title
+                        </label>
+                        <input
+                            type='text'
+                            required
+                            id='ruleTitle'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                        />
+                    </div>
+                    <div className='grid gap-4 relative '>
+                        <label
+                            htmlFor='rules_and_regulations'
+                            className='text-[1.4rem] font-Satoshi-Medium'
+                        >
+                            Estate rules and regulations
+                        </label>
+                        <textarea
+                            rows={7}
+                            required
+                            id='rules_and_regulations'
+                            className='w-full rounded-lg border border-color-grey text-[1.6rem] outline-none py-4 px-4'
+                        />
+                    </div>
+                    <button className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg justify-start justify-self-start'>
+                        <span>
+                            <IoMdAdd />
+                        </span>{' '}
+                        Add Rule
+                    </button>
+                </form>
+            </section>
         </>
     )
 }
