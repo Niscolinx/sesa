644c0a6bf7b134eecef7ff7c573cb7d0c3615a64
Igboanugwo Collins <munisco12@gmail.com>
2023-03-20 09:59:06 +0100

    working on rules and regulations

--- src/pages/EstateManager/rules/ViewRule.tsx
+++ src/pages/EstateManager/rules/ViewRule.tsx
@@ -7,13 +7,11 @@ import { IoMdAdd } from 'react-icons/io'
 import { useLocation, useNavigate } from 'react-router'
 import { RuleContent } from './Rules'
 
-
 function ViewRuleContent() {
-     const location = useLocation()
+    const location = useLocation()
 
-     const ruleContent: RuleContent[] = location.state || {}
+    const ruleContent: RuleContent[] = location.state || {}
 
-     
     const [isRuleContents, setIsRuleContents] = useState(false)
 
     const addRuleContentsHandler = () => {
@@ -72,15 +70,23 @@ function ViewRuleContent() {
 
     useEffect(() => {
         const slicedPages: RuleContent[][] = []
-        for (let i = 0; i < ruleContentsList.length; i += paginate.itemsPerPage) {
-            slicedPages.push(ruleContentsList.slice(i, i + paginate.itemsPerPage))
+        for (
+            let i = 0;
+            i < ruleContentsList.length;
+            i += paginate.itemsPerPage
+        ) {
+            slicedPages.push(
+                ruleContentsList.slice(i, i + paginate.itemsPerPage)
+            )
         }
 
         setPaginate((prev) => {
             return {
                 ...prev,
                 slicedPages,
-                totalPage: Math.ceil(ruleContentsList.length / paginate.itemsPerPage),
+                totalPage: Math.ceil(
+                    ruleContentsList.length / paginate.itemsPerPage
+                ),
             }
         })
     }, [ruleContentsList])
@@ -131,143 +137,131 @@ function ViewRuleContent() {
 
     return (
         <div className='grid'>
-            <div className='rounded-lg mt-[3rem] h-[80vh]'>
-               
-                    <section className='rounded-lg'>
-                        <div className='grid gap-10 rounded-lg  min-w-[112rem]'>
-                            <div className='grid text-[1.6rem]  rounded-lg'>
-                                <div className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
-                                    <p className=' font-bold'>
-                                        RuleContents <span>(200)</span>
-                                    </p>
-                                    <div className='relative flex items-center'>
-                                        <img
-                                            src='/icons/admins/search.svg'
-                                            alt=''
-                                            className='absolute left-4 text-[4rem]'
-                                        />
-                                        <input
-                                            type='text'
-                                            value={search}
-                                            onChange={handleSearch}
-                                            placeholder='Search Parameters'
-                                            className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
-                                        />
-                                    </div>
-                                    <div className='relative flex items-center'>
-                                        <select className=' cursor-pointer w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'>
-                                            <option hidden value=''>
-                                                Sort By
-                                            </option>
-                                            <option value='date'>date</option>
-                                            <option value='alpha'>Alpha</option>
-                                        </select>
-                                        <GrDown className='absolute right-4 text-[1.3rem]' />
-                                    </div>
-                                   
-                                </div>
+            <div className='mt-[3rem] h-[80vh] grid gap-10 rounded-lg  min-w-[112rem]'>
+                <div className='grid text-[1.6rem]  rounded-lg'>
+                    <div className='flex w-full justify-start items-center gap-12 p-10 bg-white rounded-lg'>
+                        <p className=' font-bold'>
+                            RuleContents <span>(200)</span>
+                        </p>
+                        <div className='relative flex items-center'>
+                            <img
+                                src='/icons/admins/search.svg'
+                                alt=''
+                                className='absolute left-4 text-[4rem]'
+                            />
+                            <input
+                                type='text'
+                                value={search}
+                                onChange={handleSearch}
+                                placeholder='Search Parameters'
+                                className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
+                            />
+                        </div>
+                        <div className='relative flex items-center'>
+                            <select className=' cursor-pointer w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'>
+                                <option hidden value=''>
+                                    Sort By
+                                </option>
+                                <option value='date'>date</option>
+                                <option value='alpha'>Alpha</option>
+                            </select>
+                            <GrDown className='absolute right-4 text-[1.3rem]' />
+                        </div>
+                    </div>
 
-                                <div className='grid gap-8 mt-8'>
-                                    {slicedPages && slicedPages.length > 0 ? (
-                                        React.Children.toArray(
-                                            slicedPages[paginate.index].map(
-                                                (ruleContentsBody) => {
-                                                    const {
-                                                        id,
-                                                        description
-                                                    } = ruleContentsBody
-                                                    return (
-                                                        <div
-                                                            className='grid relative p-16 bg-white rounded-lg gap-2 justify-items-start'
-                                                            key={id}
-                                                        >
-                                                            <p className='flex items-center gap-4'>
-                                                                <span className='w-[1rem] h-[1rem] rounded-full bg-[#EF6AF2] object-cover'></span>
-                                                                <span>
-                                                                    {title}
-                                                                </span>
-                                                            </p>
-                                                            <p className='text-gray-500'>
-                                                                Created{' '}
-                                                                {createAt}
-                                                            </p>
-                                                            <button className='text-color-blue'>
-                                                                View Details
-                                                            </button>
-                                                        </div>
-                                                    )
-                                                }
-                                            )
-                                        )
-                                    ) : (
-                                        <div>
-                                            <div className='relative'>
-                                                <div className='absolute w-full grid place-content-center'>
-                                                    <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
-                                                </div>
+                    <div className='grid gap-8 mt-8'>
+                        {slicedPages && slicedPages.length > 0 ? (
+                            React.Children.toArray(
+                                slicedPages[paginate.index].map(
+                                    (ruleContentsBody) => {
+                                        const { id, description } =
+                                            ruleContentsBody
+                                        return (
+                                            <div
+                                                className='grid relative p-16 bg-white rounded-lg gap-2 justify-items-start'
+                                                key={id}
+                                            >
+                                                <p className='flex items-center gap-4'>
+                                                    <span className='w-[1rem] h-[1rem] rounded-full bg-[#EF6AF2] object-cover'></span>
+                                                    <span>{title}</span>
+                                                </p>
+                                                <p className='text-gray-500'>
+                                                    Created {createAt}
+                                                </p>
+                                                <button className='text-color-blue'>
+                                                    View Details
+                                                </button>
                                             </div>
-                                        </div>
-                                    )}
+                                        )
+                                    }
+                                )
+                            )
+                        ) : (
+                            <div>
+                                <div className='relative'>
+                                    <div className='absolute w-full grid place-content-center'>
+                                        <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
+                                    </div>
                                 </div>
                             </div>
-                            <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
-                                <div className='flex gap-8 items-center'>
-                                    <p>View</p>
-                                    <select
-                                        name=''
-                                        id=''
-                                        className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
-                                        onChange={handleItemsPerPage}
-                                    >
-                                        {itemsPerPageArr.map((item, index) => (
-                                            <option
-                                                value={item}
-                                                key={index}
-                                                selected={item === itemsPerPage}
-                                                className='capitalize cursor-pointer bg-white'
-                                            >
-                                                {item}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <p className='text'>List per page</p>
-                                </div>
-                                <ul className='flex items-center gap-5 ml-10'>
-                                    <HiOutlineChevronLeft
-                                        onClick={handlePrev}
-                                        className='cursor-pointer'
-                                    />
+                        )}
+                    </div>
+                </div>
+                <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
+                    <div className='flex gap-8 items-center'>
+                        <p>View</p>
+                        <select
+                            name=''
+                            id=''
+                            className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
+                            onChange={handleItemsPerPage}
+                        >
+                            {itemsPerPageArr.map((item, index) => (
+                                <option
+                                    value={item}
+                                    key={index}
+                                    selected={item === itemsPerPage}
+                                    className='capitalize cursor-pointer bg-white'
+                                >
+                                    {item}
+                                </option>
+                            ))}
+                        </select>
+                        <p className='text'>List per page</p>
+                    </div>
+                    <ul className='flex items-center gap-5 ml-10'>
+                        <HiOutlineChevronLeft
+                            onClick={handlePrev}
+                            className='cursor-pointer'
+                        />
 
-                                    {slicedPages?.map((item, index) => {
-                                        return (
-                                            <li key={index}>
-                                                {index + 1 === currentPage ? (
-                                                    <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
-                                                        {index + 1}
-                                                    </span>
-                                                ) : (
-                                                    <span
-                                                        className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
-                                                        onClick={(e) =>
-                                                            jumpToPage(e, index)
-                                                        }
-                                                    >
-                                                        {index + 1}
-                                                    </span>
-                                                )}
-                                            </li>
-                                        )
-                                    })}
+                        {slicedPages?.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    {index + 1 === currentPage ? (
+                                        <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
+                                            {index + 1}
+                                        </span>
+                                    ) : (
+                                        <span
+                                            className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
+                                            onClick={(e) =>
+                                                jumpToPage(e, index)
+                                            }
+                                        >
+                                            {index + 1}
+                                        </span>
+                                    )}
+                                </li>
+                            )
+                        })}
 
-                                    <HiOutlineChevronRight
-                                        onClick={handleNext}
-                                        className='cursor-pointer'
-                                    />
-                                </ul>
-                            </footer>
-                        </div>
-                    </section>
-                
+                        <HiOutlineChevronRight
+                            onClick={handleNext}
+                            className='cursor-pointer'
+                        />
+                    </ul>
+                </footer>
             </div>
         </div>
     )
