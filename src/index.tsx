b23df5a9d03f17c4408eaef3769da1eed7ca3a80
Igboanugwo Collins <munisco12@gmail.com>
2023-03-22 15:05:34 +0100

    added json server

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import ReactDOM from 'react-dom/client';
-import App from './Entry/App';
+import ReactDOM from 'react-dom/client'
+import App from './Entry/App'
 import './index.css'
-import './sass/main.scss';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistedStore, store } from './store/app/store';
+import './sass/main.scss'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistedStore, store } from './store/app/store'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-      <App />
-  </Provider>
-);
+    <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </Provider>
+)
