7acb92546c27528c065632dc586f01f7c41e68d6
Igboanugwo Collins <munisco12@gmail.com>
2023-03-22 15:29:31 +0100

    added json server

--- src/pages/EstateManager/Reports/Reports.tsx
+++ src/pages/EstateManager/Reports/Reports.tsx
@@ -56,8 +56,8 @@ function Reports() {
         name: string
     }[]
 
-   const {isLoading, data} =  useQuery('user', () => {
-        axios.get('http://localhost:4000/users')
+   const {isLoading, data: response_data} =  useQuery('user', () => {
+       return axios.get('http://localhost:4000/users')
     })
 
     if (isLoading) {
@@ -109,7 +109,7 @@ function Reports() {
                     </section>
                 ) : (
                     <section className='grid place-content-center w-full h-[80vh] justify-items-center gap-4 bg-white rounded-lg'>
-                        {data?.map((data) => (
+                        {response_data?.data.map((data) => (
                             <div
                                 key={data.id}
                                 className='grid p-8 border rounded-2xl gap-4 items-center'
