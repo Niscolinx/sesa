04b08ac71de32fd4e5603bf2c017e82d3cdd228b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-31 11:47:48 +0100

    axios interceptor setup

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,9 +2,10 @@ import axios, { AxiosError, AxiosResponse } from 'axios'
 
 const client = axios.create({ baseURL: 'https://sesadigital.com/api' })
 
-export const AxiosRequest = ({ ...options }) => {
-    client.defaults.headers.common.Authorization = 'Bearer token'
-
+export const AxiosRequest = (token: string, { ...options }) => {
+    if (token) {
+        client.defaults.headers.common.Authorization = `Bearer ${token}`
+    }
     const onSuccess = (response: AxiosResponse) => response
     const onError = (error: AxiosError) => error
 
