bde5cfaa836097915278e6a53acba5f5f8142d6b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-31 18:59:26 +0100

    axios interceptor setup

--- src/components/SuperAdmin/admins/AddAdmin.tsx
+++ src/components/SuperAdmin/admins/AddAdmin.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { GrDown } from 'react-icons/gr'
 import { IoMdAdd } from 'react-icons/io'
@@ -21,7 +21,7 @@ const AddAdmin = () => {
     }
 
     const ModalContextData = useContext(ModalContext)
-    const { handleOpen } = ModalContextData
+    // const { handleOpen } = ModalContextData
 
     const [photoPreview, setPhotoPreview] = useState('')
     const [imageUrl, setImageUrl] = useState<File | null>(null)
@@ -35,7 +35,7 @@ const AddAdmin = () => {
         const target = e.target as HTMLInputElement
         const file: File = (target.files as FileList)[0]
 
-        console.log({file})
+        console.log({ file })
 
         const preview = URL.createObjectURL(file)
         setPhotoPreview(preview)
@@ -47,8 +47,6 @@ const AddAdmin = () => {
 
     //     handleOpen('renderedAdmins')
     // }
-
-
 
     const {
         register,
@@ -86,9 +84,9 @@ const AddAdmin = () => {
     } = useMutation(postLogin) as any
 
     useEffect(() => {
-        console.log({response_data})
+        console.log({ response_data })
         if (response_data?.status === 200) {
-            handleOpen('renderedAdmins')
+            handleOpen()
         } else {
             setResponseMessage({
                 className: 'text-red-600',
@@ -114,15 +112,48 @@ const AddAdmin = () => {
             image: imageUrl?.name,
         }
 
-        console.log({ adminData })
-
         mutate(adminData)
     })
 
+    const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+    const handleClose = () => {
+        if (dialogRef.current) {
+            dialogRef.current.close()
+        }
+    }
+
+    const handleOpen = () => {
+        if (dialogRef.current) {
+            dialogRef.current.showModal()
+        }
+    }
 
     return (
-        <div className='addAdmin'>
-            <p className='addAdmin__heading'>Personal Information</p>
+        <>
+            <dialog className='dialog' ref={dialogRef}>
+                <section className='grid place-content-center w-full h-[100vh]'>
+                    <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8'>
+                        <img src='/icons/admins/modalSuccess.svg' alt='' />
+                        <p>You have successfully added an Estate</p>
+
+                        <div className='flex w-full justify-center gap-8'>
+                            <button className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'>
+                                View details
+                            </button>
+                            <button
+                                className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem]'
+                                onClick={handleRouteChange}
+                            >
+                                Ok
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </dialog>
+
+            <div className='addAdmin'>
+                <p className='addAdmin__heading'>Personal Information</p>
                 {responseMessage?.displayMessage && (
                     <p className='text-center'>
                         <span className={responseMessage?.className}>
@@ -130,113 +161,117 @@ const AddAdmin = () => {
                         </span>
                     </p>
                 )}
-            <form onSubmit={onSubmit} className='addAdmin__formBox'>
-                <section className='addAdmin__form'>
-                    <div className='addAdmin__form--item'>
-                        <Input
-                            label={'first_name'}
-                            register={register}
-                            formErrors={formErrors}
-                        />
-                    </div>
-                    <div className='addAdmin__form--item'>
-                        <Input
-                            label={'last_name'}
-                            register={register}
-                            formErrors={formErrors}
-                        />
-                    </div>
-                    <div className='addAdmin__form--item'>
-                        <Input
-                            type={'date'}
-                            label={'dob'}
-                            name={'Date of Birth'}
-                            register={register}
-                            formErrors={formErrors}
-                        />
-                    </div>
-                    <div className='addAdmin__form--item'>
-                        <label htmlFor='gender'>Gender *</label>
-                        <div className='item__select border border-color-grey rounded-lg w-full'>
-                            <select
-                                id='gender'
-                                {...register('gender', { required: true })}
-                            >
-                                <option value='male'>Male</option>
-                                <option value='female'>Female</option>
-                            </select>
-                            <GrDown />
-                        </div>
-                    </div>
-                    <div className='addAdmin__form--phoneNumber'>
-                        <label htmlFor='phoneNumber'>Phone Number *</label>
-
-                        <div className='phoneNumber__box'>
-                            <select>
-                                <option value='234'>+234</option>
-                            </select>
-                            <input
-                                required
-                                {...register('phoneNumber', {
-                                    required: true,
-                                    //minLength: 8,
-                                    // maxLength: 10,
-                                })}
-                                type='number'
-                                inputMode='numeric'
-                                id='phoneNumber'
+                <form onSubmit={onSubmit} className='addAdmin__formBox'>
+                    <section className='addAdmin__form'>
+                        <div className='addAdmin__form--item'>
+                            <Input
+                                label={'first_name'}
+                                register={register}
+                                formErrors={formErrors}
                             />
                         </div>
-                    </div>
-                    <div className='addAdmin__form--item'>
-                        <Input
-                            type={'email'}
-                            label={'email'}
-                            name={'email address'}
-                            register={register}
-                            formErrors={formErrors}
-                        />
-                    </div>
-                    <div className='addAdmin__form--file'>
-                        <label htmlFor='photoUpload'>
-                            <img src='/icons/admins/photo_library.svg' alt='' />
-                            <p>
-                                Drag estate manager picture here or{' '}
-                                <span>click</span> to upload
-                            </p>
-                        </label>
-                        <input
-                            type='file'
-                            name='photoUpload'
-                            id='photoUpload'
-                            accept='image/*'
-                            className='hidden'
-                            onChange={handlePicture}
-                            //onClick={handlePhotoPreview}
-                        />
+                        <div className='addAdmin__form--item'>
+                            <Input
+                                label={'last_name'}
+                                register={register}
+                                formErrors={formErrors}
+                            />
+                        </div>
+                        <div className='addAdmin__form--item'>
+                            <Input
+                                type={'date'}
+                                label={'dob'}
+                                name={'Date of Birth'}
+                                register={register}
+                                formErrors={formErrors}
+                            />
+                        </div>
+                        <div className='addAdmin__form--item'>
+                            <label htmlFor='gender'>Gender *</label>
+                            <div className='item__select border border-color-grey rounded-lg w-full'>
+                                <select
+                                    id='gender'
+                                    {...register('gender', { required: true })}
+                                >
+                                    <option value='male'>Male</option>
+                                    <option value='female'>Female</option>
+                                </select>
+                                <GrDown />
+                            </div>
+                        </div>
+                        <div className='addAdmin__form--phoneNumber'>
+                            <label htmlFor='phoneNumber'>Phone Number *</label>
 
-                        {photoPreview && (
-                            <div className='file__uploadImgBox'>
-                                <img
-                                    src={photoPreview}
-                                    alt='photoPreview'
-                                    className='object-contain'
+                            <div className='phoneNumber__box'>
+                                <select>
+                                    <option value='234'>+234</option>
+                                </select>
+                                <input
+                                    required
+                                    {...register('phoneNumber', {
+                                        required: true,
+                                        //minLength: 8,
+                                        // maxLength: 10,
+                                    })}
+                                    type='number'
+                                    inputMode='numeric'
+                                    id='phoneNumber'
                                 />
                             </div>
-                        )}
-                    </div>
-                    <button
-                        className='btn admins__btn'
-                        style={{ justifySelf: 'start' }}
-                    >
-                        <span>
-                            <IoMdAdd />
-                        </span>{' '}
-                        {isLoading ? 'Loading...' : 'Add'}
-                    </button>
-                </section>
-            </form>
-        </div>
+                        </div>
+                        <div className='addAdmin__form--item'>
+                            <Input
+                                type={'email'}
+                                label={'email'}
+                                name={'email address'}
+                                register={register}
+                                formErrors={formErrors}
+                            />
+                        </div>
+                        <div className='addAdmin__form--file'>
+                            <label htmlFor='photoUpload'>
+                                <img
+                                    src='/icons/admins/photo_library.svg'
+                                    alt=''
+                                />
+                                <p>
+                                    Drag estate manager picture here or{' '}
+                                    <span>click</span> to upload
+                                </p>
+                            </label>
+                            <input
+                                type='file'
+                                name='photoUpload'
+                                id='photoUpload'
+                                accept='image/*'
+                                className='hidden'
+                                onChange={handlePicture}
+                                //onClick={handlePhotoPreview}
+                            />
+
+                            {photoPreview && (
+                                <div className='file__uploadImgBox'>
+                                    <img
+                                        src={photoPreview}
+                                        alt='photoPreview'
+                                        className='object-contain'
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            className='btn admins__btn'
+                            style={{ justifySelf: 'start' }}
+                        >
+                            <span>
+                                <IoMdAdd />
+                            </span>{' '}
+                            {isLoading ? 'Loading...' : 'Add'}
+                        </button>
+                    </section>
+                </form>
+            </div>
+        </>
     )
 }
 
