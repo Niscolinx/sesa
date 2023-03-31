e666c61f96816144d31fd588e43ab4c40b853497
Igboanugwo Collins <munisco12@gmail.com>
2023-03-31 16:12:44 +0100

    axios interceptor setup

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react"
+
+interface Input {
+    label: string
+    type: string
+}
+
+const Input:FC<Input> = ({}) => {
+    return (
+        <div className='w-full grid gap-4'>
+            <label htmlFor='email' className='font-semibold'>
+                Email
+            </label>
+            <input
+                type='email'
+                {...register('email', {
+                    required: true,
+                    // pattern: /^\S+@\S+$/i,
+                    pattern:
+                        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                })}
+                className={`border border-color-grey p-4 rounded-lg w-full ${
+                    formErrors.email && 'border-red-500 '
+                }`}
+            />
+            {formErrors.email && (
+                <p className='text-[1.2rem] text-red-500'>
+                    {formErrors.email.type === 'required' ? (
+                        <span>Field cannot be empty</span>
+                    ) : (
+                        <span>Invalid email</span>
+                    )}
+                </p>
+            )}
+        </div>
+    )
+}
+
+export default Input
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -93,43 +93,7 @@ const Login = () => {
         }
     })
 
-    interface Input {
-        label: string
-        type: string
-    }
-
-
-    const Input = () => {
-        
-        return (
-            <div className='w-full grid gap-4'>
-                <label htmlFor='email' className='font-semibold'>
-                    Email
-                </label>
-                <input
-                    type='email'
-                    {...register('email', {
-                        required: true,
-                        // pattern: /^\S+@\S+$/i,
-                        pattern:
-                            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    })}
-                    className={`border border-color-grey p-4 rounded-lg w-full ${
-                        formErrors.email && 'border-red-500 '
-                    }`}
-                />
-                {formErrors.email && (
-                    <p className='text-[1.2rem] text-red-500'>
-                        {formErrors.email.type === 'required' ? (
-                            <span>Field cannot be empty</span>
-                        ) : (
-                            <span>Invalid email</span>
-                        )}
-                    </p>
-                )}
-            </div>
-        )
-    }
+   
 
     return (
         <div className='flex h-screen overflow-hidden'>
