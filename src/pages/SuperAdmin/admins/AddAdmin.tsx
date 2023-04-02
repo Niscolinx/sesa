83a49e45f743df9a4c930da5f8160c1e228cb492
Igboanugwo Collins <munisco12@gmail.com>
2023-04-02 17:14:15 +0100

    working on the admin

--- src/pages/SuperAdmin/admins/AddAdmin.tsx
+++ src/pages/SuperAdmin/admins/AddAdmin.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { IoMdAdd } from 'react-icons/io'
 import { useMutation } from 'react-query'
