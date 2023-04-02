46c8c7cf830410c5e87f50095ab69b1fc334a470
Igboanugwo Collins <munisco12@gmail.com>
2023-04-02 20:24:37 +0100

    working on the admin

--- src/pages/SuperAdmin/admins/EditAdmin.tsx
+++ src/pages/SuperAdmin/admins/EditAdmin.tsx
@@ -196,7 +196,6 @@ const EditAdmin = () => {
                     />
 
                     <div className='flex gap-8'>
-                        
                         <button
                             className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
                             //onClick={() => handleSelectedAction('Delete')}
@@ -248,51 +247,18 @@ const EditAdmin = () => {
                             )
                         })}
 
-                        <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
-                            <label
-                                htmlFor='photoUpload'
-                                className='flex justify-center gap-4 items-center cursor-pointer'
-                            >
-                                <img
-                                    src='/icons/admins/photo_library.svg'
-                                    alt=''
-                                />
-                                <p>
-                                    Drag picture here or{' '}
-                                    <span className='text-color-blue font-bold'>
-                                        click
-                                    </span>{' '}
-                                    to upload
-                                </p>
-                            </label>
-                            <input
-                                type='file'
-                                name='photoUpload'
-                                id='photoUpload'
-                                accept='image/*'
-                                className='hidden'
-                                onChange={handlePicture}
-                                //onClick={handlePhotoPreview}
-                            />
-
-                            {photoPreview && (
-                                <div className='flex justify-center justify-self-center'>
-                                    <img
-                                        src={photoPreview}
-                                        alt='photoPreview'
-                                        className='object-cover w-[11rem] h-[11rem] rounded-full'
-                                    />
-                                </div>
-                            )}
-                        </div>
                         <button
-                            className='btn admins__btn'
+                            className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg col-span-full mt-[5rem]'
                             style={{ justifySelf: 'start' }}
                         >
                             <span>
-                                <IoMdAdd />
+                                <img
+                                    src='/icons/admins/saveDisk.svg'
+                                    alt=''
+                                    className='w-[1.7rem] h-[1.7rem]'
+                                />
                             </span>{' '}
-                            {isLoading ? 'Loading...' : 'Add'}
+                            Save Changes
                         </button>
                     </>
                 </form>
