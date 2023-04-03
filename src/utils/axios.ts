4e8ab5adc0fefdfa1683d9a0759f656823f299f5
Igboanugwo Collins <munisco12@gmail.com>
2023-04-03 11:28:10 +0100

    working on admin

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios'
+import { getToken } from './token'
 
-const client = axios.create({ baseURL: 'https://sesadigital.com/api' })
+const instance = axios.create({ baseURL: 'https://sesadigital.com/api' })
 
 interface RequestOptions {
     method: string
@@ -10,10 +11,25 @@ interface RequestOptions {
     headers: any
 }
 
+instance.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
+        const token = getToken()
+        if (token) {
+            instance.defaults.headers.common.Authorization = `Bearer ${token}`
+        }
+        
+        return config
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error)
+    }
+)
 export const AxiosRequest = ({ ...options }: Partial<RequestOptions>) => {
 
     const onSuccess = (response: AxiosResponse) => response
     const onError = (error: AxiosError) => error
 
-    return client(options).then(onSuccess).catch(onError)
+    return instance(options).then(onSuccess).catch(onError)
 }
