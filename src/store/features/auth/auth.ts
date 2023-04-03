7b0184fcbb86acb1ee9bf487df8ce942782bf371
Igboanugwo Collins <munisco12@gmail.com>
2023-04-03 08:31:19 +0100

    working on admin

--- src/store/features/auth/auth.ts
+++ src/store/features/auth/auth.ts
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     isAuth: false,
+    token: ''
 }
 
 type StateKey = keyof typeof initialState
@@ -13,7 +14,6 @@ export const authSlice = createSlice({
     reducers: {
         storeToken: (state, action) => {
             const token: string = action.payload
-            // const hashedToken = hashToken(token)
             const expirationDate = new Date().getTime() + 3600 * 1000 // set expiration date to 1 hour from now
             localStorage.setItem(
                 'token',
@@ -23,7 +23,7 @@ export const authSlice = createSlice({
             state.isAuth = true
         },
 
-        isAuthenticated: (state, action) => {
+        isAuthenticated: (state, _) => {
             const tokenData = localStorage.getItem('token')
             if (tokenData) {
                 const { token, expirationDate } = JSON.parse(tokenData)
@@ -33,7 +33,8 @@ export const authSlice = createSlice({
                     
                     return state.isAuth = false
                 }
-                return token
+                
+                return state.token = token
             }
             return state.isAuth = false
         },
