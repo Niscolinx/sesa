f2a06e3b78d860e5891a73b4886e60dadc37937d
Igboanugwo Collins <munisco12@gmail.com>
2023-04-04 13:26:17 +0100

    working on admins

--- src/components/hooks/useAxios.tsx
+++ src/components/hooks/useAxios.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useAppDispatch } from '../../store/app/hooks'
+import { AxiosRequest } from '../../utils/axios'
 
 function useAxios() {
-  return (
-    <div>useAxios</div>
-  )
+    const dispatch = useAppDispatch()
+    const fetchAdmins = async () => {
+        return AxiosRequest({
+            dispatch,
+            url: '/admin/get/all',
+        })
+    }
+
+    return <div>useAxios</div>
 }
 
 export default useAxios
