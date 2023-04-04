4f10672a87a0eec10321bce6ed779b45b66ff8fb
Igboanugwo Collins <munisco12@gmail.com>
2023-04-04 20:03:32 +0100

    working on estates

--- src/components/UI/input/imageInput.tsx
+++ src/components/UI/input/imageInput.tsx
@@ -1,43 +1,44 @@
-
-function imageInput() {
-  return (
-      <div>
-          <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
-              <label
-                  htmlFor='photoUpload'
-                  className='flex justify-center gap-4 items-center cursor-pointer'
-              >
-                  <img src='/icons/admins/photo_library.svg' alt='' />
-                  <p>
-                      Drag picture here or{' '}
-                      <span className='text-color-blue font-Satoshi-Medium'>
-                          click
-                      </span>{' '}
-                      to upload
-                  </p>
-              </label>
-              <input
-                  type='file'
-                  name='photoUpload'
-                  id='photoUpload'
-                  accept='image/*'
-                  className='hidden'
-                  onChange={handlePicture}
-                  //onClick={handlePhotoPreview}
-              />
-
-              {photoPreview && (
-                  <div className='flex justify-center justify-self-center'>
-                      <img
-                          src={photoPreview}
-                          alt='photoPreview'
-                          className='object-cover w-[11rem] h-[11rem] rounded-full'
-                      />
-                  </div>
-              )}
-          </div>
-    
-  )
+interface ImageInput {
+    handlePicture: (e: React.ChangeEvent) => void
+    photoPreview: string
 }
 
-export default imageInput
+function ImageInput({ handlePicture, photoPreview }: ImageInput) {
+    return (
+        <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
+            <label
+                htmlFor='photoUpload'
+                className='flex justify-center gap-4 items-center cursor-pointer'
+            >
+                <img src='/icons/admins/photo_library.svg' alt='' />
+                <p>
+                    Drag picture here or{' '}
+                    <span className='text-color-blue font-Satoshi-Medium'>
+                        click
+                    </span>{' '}
+                    to upload
+                </p>
+            </label>
+            <input
+                type='file'
+                name='photoUpload'
+                id='photoUpload'
+                accept='image/*'
+                className='hidden'
+                onChange={handlePicture}
+            />
+
+            {photoPreview && (
+                <div className='flex justify-center justify-self-center'>
+                    <img
+                        src={photoPreview}
+                        alt='photoPreview'
+                        className='object-cover w-[11rem] h-[11rem] rounded-full'
+                    />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default ImageInput
--- src/pages/SuperAdmin/admins/AddAdmin.tsx
+++ src/pages/SuperAdmin/admins/AddAdmin.tsx
@@ -6,6 +6,7 @@ import { AxiosRequest } from '../../../utils/axios'
 import Input from '../../../components/UI/input/Input'
 import { Select } from '../../../components/SuperAdmin/UI/Select'
 import { useAppDispatch } from '../../../store/app/hooks'
+import ImageInput from '../../../components/UI/input/ImageInput'
 
 const AddAdmin = () => {
     interface Inputs {
@@ -211,43 +212,10 @@ const AddAdmin = () => {
                             )
                         })}
 
-                        <div className='col-span-full rounded-lg border border-width-[.2rem] border-dashed border-color-grey-1 p-8 text-[1.6rem] relative w-full'>
-                            <label
-                                htmlFor='photoUpload'
-                                className='flex justify-center gap-4 items-center cursor-pointer'
-                            >
-                                <img
-                                    src='/icons/admins/photo_library.svg'
-                                    alt=''
-                                />
-                                <p>
-                                    Drag picture here or{' '}
-                                    <span className='text-color-blue font-Satoshi-Medium'>
-                                        click
-                                    </span>{' '}
-                                    to upload
-                                </p>
-                            </label>
-                            <input
-                                type='file'
-                                name='photoUpload'
-                                id='photoUpload'
-                                accept='image/*'
-                                className='hidden'
-                                onChange={handlePicture}
-                                //onClick={handlePhotoPreview}
-                            />
-
-                            {photoPreview && (
-                                <div className='flex justify-center justify-self-center'>
-                                    <img
-                                        src={photoPreview}
-                                        alt='photoPreview'
-                                        className='object-cover w-[11rem] h-[11rem] rounded-full'
-                                    />
-                                </div>
-                            )}
-                        </div>
+                        <ImageInput
+                            handlePicture={handlePicture}
+                            photoPreview={photoPreview}
+                        />
                         <button
                             className='btn admins__btn'
                             style={{ justifySelf: 'start' }}
