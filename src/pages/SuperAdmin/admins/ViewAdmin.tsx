771870e409bd3b9c6c03f5e6400883af053b1d80
Igboanugwo Collins <munisco12@gmail.com>
2023-04-05 14:55:52 +0100

    working on estate manager

--- src/pages/SuperAdmin/admins/ViewAdmin.tsx
+++ src/pages/SuperAdmin/admins/ViewAdmin.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
-import AxiosRequest from '../../../utils/axios'
 import Input, { SelectProps } from '../../../components/UI/input/Input'
 import { Select } from '../../../components/SuperAdmin/UI/Select'
 import { useParams } from 'react-router'
-import { getToken } from '../../../utils/token'
 import { toast, ToastContainer } from 'react-toastify'
-import { useAppDispatch } from '../../../store/app/hooks'
 import useAxios from '../../../components/hooks/useAxios'
 
 const ViewAdmin = () => {
