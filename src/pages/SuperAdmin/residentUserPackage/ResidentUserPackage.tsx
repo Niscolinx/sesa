86d41d883781876de5a50a012770e7e71734b116
Igboanugwo Collins <munisco12@gmail.com>
2023-04-07 09:12:08 +0100

    working on resident package

--- src/pages/SuperAdmin/residentUserPackage/ResidentUserPackage.tsx
+++ src/pages/SuperAdmin/residentUserPackage/ResidentUserPackage.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
 import { useLocation } from 'react-router'
 import ResidentUsers from './ResidentUserPackages'
+import ResidentPackageHistory from './ResidentPackageHistory'
+import ResidentPackageList from './ResidentPackageList'
 
 function AdditionalResident() {
     const navigate = useLocation()
@@ -10,14 +12,51 @@ function AdditionalResident() {
     const handleAddEstate = () => {
         setAdditionalResident(true)
     }
+    type SwitchRoute = 'list' | 'history'
+
+    const resident_paths = [
+        { path: 'list', label: 'Additional Resident Package' },
+        { path: 'history', label: 'Package purchase history' },
+    ] satisfies { path: SwitchRoute; label: string }[]
+
+    const [currentPath, setCurrentPath] = useState<SwitchRoute>('list')
+
+    const switchRoute = new Map([
+        ['list', <ResidentPackageList />],
+        ['history', <ResidentPackageHistory />],
+    ]) satisfies Map<SwitchRoute, JSX.Element>
 
     return (
         <div>
             <div className='rounded-lg mt-[3rem] h-[80vh]'>
                 {additionalResident ? (
-                    <section>
-                        <ResidentUsers />
-                    </section>
+                    <>
+                        <div className='estateDetail__radioBox capitalize'>
+                            {resident_paths.map((eachPath, idx) => {
+                                const { label, path } = eachPath
+                                return (
+                                    <Fragment key={label + idx}>
+                                        <input
+                                            type='radio'
+                                            name='report'
+                                            id={label + idx}
+                                            className='hidden'
+                                            onChange={() =>
+                                                setCurrentPath(path)
+                                            }
+                                            checked={currentPath === path}
+                                        />
+                                        <label htmlFor={label + idx}>
+                                            {label}
+                                        </label>
+                                    </Fragment>
+                                )
+                            })}
+                        </div>
+                        <section className='bg-color-white rounded-lg border min-w-[112rem] overflow-scroll mt-8 min-h-[60vh]'>
+                            {switchRoute.get(currentPath)}
+                        </section>
+                    </>
                 ) : (
                     <section className='grid place-content-center w-full h-full justify-items-center gap-4 bg-white'>
                         <img src='/icons/admins/errorSvg.svg' alt='' />
--- src/pages/SuperAdmin/residentUserPackage/ResidentUserPackages.tsx
+++ src/pages/SuperAdmin/residentUserPackage/ResidentUserPackages.tsx
@@ -3,43 +3,11 @@ import ResidentPackageList from './ResidentPackageList'
 import ResidentPackageHistory from './ResidentPackageHistory'
 
 function ResidentUsers() {
-    type SwitchRoute = 'list' | 'history'
-
-    const resident_paths = [
-        { path: 'list', label: 'Additional Resident Package' },
-        { path: 'history', label: 'Package purchase history' },
-    ] satisfies { path: SwitchRoute; label: string }[]
-
-    const [currentPath, setCurrentPath] = useState<SwitchRoute>('list')
-
-    const switchRoute = new Map([
-        ['list', <ResidentPackageList />],
-        ['history', <ResidentPackageHistory />],
-    ]) satisfies Map<SwitchRoute, JSX.Element>
+    
 
     return (
         <>
-            <div className='estateDetail__radioBox capitalize'>
-                {resident_paths.map((eachPath, idx) => {
-                    const { label, path } = eachPath
-                    return (
-                        <Fragment key={label + idx}>
-                            <input
-                                type='radio'
-                                name='report'
-                                id={label + idx}
-                                className='hidden'
-                                onChange={() => setCurrentPath(path)}
-                                checked={currentPath === path}
-                            />
-                            <label htmlFor={label + idx}>{label}</label>
-                        </Fragment>
-                    )
-                })}
-            </div>
-            <section className='bg-color-white rounded-lg border min-w-[112rem] overflow-scroll mt-8 min-h-[60vh]'>
-                {switchRoute.get(currentPath)}
-            </section>
+            
         </>
     )
 }
