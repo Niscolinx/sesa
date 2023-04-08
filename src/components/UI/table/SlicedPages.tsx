7b68b3961868414816eb9c4ade72a2624f889d69
Igboanugwo Collins <munisco12@gmail.com>
2023-04-08 21:51:21 +0100

    working on table

--- src/components/UI/table/SlicedPages.tsx
+++ src/components/UI/table/SlicedPages.tsx
@@ -37,47 +37,46 @@ const SlicedPages: FC<SlicedPages> = ({
         Object.entries(user).map(
             ([key, value]: any, idx: number) => {
                 console.log({key, value})
-                return dataToDisplay.includes(key) && details.push(value)
+                return dataToDisplay.includes(key) && details.push({
+                    key,
+                    value
+                })
             }
                 
         )
 
         console.log({details})
         return (
-            details?.map((detail: any, idx: any) => (
-                <p>
-                {JSON.stringify(detail)}
-                </p>
-            ))
-        )
+            details?.map((detail: any, idx: number) => {
+              
+                    return (
+                        <div className='flex items-center gap-4  '>
+                            <input type='checkbox' className='cursor-pointer' />
+                            <div className='flex items-center gap-2'>
+                                {/* {key === 'image' && (
+                                    <>
+                                        {value && (
+                                            <figure className='w-[3.5rem] h-[3.5rem]'>
+                                                <img
+                                                    src={value}
+                                                    alt=''
+                                                    className='w-full h-full rounded-full object-cover'
+                                                />
+                                            </figure>
+                                        )}
+                                    </>
+                                )} */}
+                                <p className='min-w-[30rem] overflow-hidden text-ellipsis whitespace-nowrap'>
+                                    {JSON.stringify(detail)}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                
+            }
+        ))
 
-        // if (idx === 0) {
-        //     return (
-        //         <div className='flex items-center gap-4  '>
-        //             <input type='checkbox' className='cursor-pointer' />
-
-        //             <div className='flex items-center gap-2'>
-        //                 {key === 'image' && (
-        //                     <>
-        //                         {value && (
-        //                             <figure className='w-[3.5rem] h-[3.5rem]'>
-        //                                 <img
-        //                                     src={value}
-        //                                     alt=''
-        //                                     className='w-full h-full rounded-full object-cover'
-        //                                 />
-        //                             </figure>
-        //                         )}
-        //                     </>
-        //                 )}
-
-        //                 <p className='min-w-[30rem] overflow-hidden text-ellipsis whitespace-nowrap'>
-        //                     {value}
-        //                 </p>
-        //             </div>
-        //         </div>
-        //     )
-        // }
+        
         // if (key === 'created_at') {
         //     return (
         //         <p>
