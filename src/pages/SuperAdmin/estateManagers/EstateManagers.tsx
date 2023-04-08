e95435abbd41806866d6fd2cdca86fdc7ca8723d
Igboanugwo Collins <munisco12@gmail.com>
2023-04-08 14:03:49 +0100

    working on the table

--- src/pages/SuperAdmin/estateManagers/EstateManagers.tsx
+++ src/pages/SuperAdmin/estateManagers/EstateManagers.tsx
@@ -1,7 +1,18 @@
+import Table from "../../../components/UI/table/Table"
+
 function EstateManagers() {
     return (
         <div>
-            <div className='rounded-lg mt-[3rem] h-[80vh]'></div>
+            <div className='rounded-lg mt-[3rem] h-[80vh]'>
+                <Table
+                    fetch_url={'/manager/get/all'}
+                    title={'estateManager'}
+                    view_page_url={'/manager/get'}
+                    add_page_url={'/superAdmin/estateManagers/add'}
+                    is_add_btn={true}
+                    deactivate_url={'change/user/status'}
+                />
+            </div>
         </div>
     )
 }
