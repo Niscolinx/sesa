eb82df4736679e24e4d2bfa39baa1fcf5d4761e5
Igboanugwo Collins <munisco12@gmail.com>
2023-04-08 21:53:36 +0100

    working on table

--- src/components/UI/table/SlicedPages.tsx
+++ src/components/UI/table/SlicedPages.tsx
@@ -33,50 +33,49 @@ const SlicedPages: FC<SlicedPages> = ({
     ]
 
     const TableItem = ({ user, id }: any) => {
-        const details:any = []
-        Object.entries(user).map(
-            ([key, value]: any, idx: number) => {
-                console.log({key, value})
-                return dataToDisplay.includes(key) && details.push({
+        const details: any = []
+        Object.entries(user).map(([key, value]: any, idx: number) => {
+            console.log({ key, value })
+            return (
+                dataToDisplay.includes(key) &&
+                details.push({
                     key,
-                    value
+                    value,
                 })
-            }
-                
-        )
+            )
+        })
 
-        console.log({details})
-        return (
-            details?.map(({key, value}: any, idx: number) => {
-              
-                    return (
-                        <div className='flex items-center gap-4  '>
-                            <input type='checkbox' className='cursor-pointer' />
-                            <div className='flex items-center gap-2'>
-                                {key === 'image' && (
-                                    <>
-                                        {value && (
-                                            <figure className='w-[3.5rem] h-[3.5rem]'>
-                                                <img
-                                                    src={value}
-                                                    alt=''
-                                                    className='w-full h-full rounded-full object-cover'
-                                                />
-                                            </figure>
-                                        )}
-                                    </>
-                                )}
-                                <p className='min-w-[30rem] overflow-hidden text-ellipsis whitespace-nowrap'>
-                                    {value}
-                                </p>
-                            </div>
+        console.log({ details })
+        return details?.map(({ key, value }: any, idx: number) => {
+            if (idx === 0) {
+                return (
+                    <div className='flex items-center gap-4  '>
+                        <input type='checkbox' className='cursor-pointer' />
+                        <div className='flex items-center gap-2'>
+                            {key === 'image' && (
+                                <>
+                                    {value && (
+                                        <figure className='w-[3.5rem] h-[3.5rem]'>
+                                            <img
+                                                src={value}
+                                                alt=''
+                                                className='w-full h-full rounded-full object-cover'
+                                            />
+                                        </figure>
+                                    )}
+                                </>
+                            )}
+                            <p className='min-w-[30rem] overflow-hidden text-ellipsis whitespace-nowrap'>
+                                {value}
+                            </p>
                         </div>
-                    )
-                
+                    </div>
+                )
+            } else {
+                return <p>{value}</p>
             }
-        ))
+        })
 
-        
         // if (key === 'created_at') {
         //     return (
         //         <p>
