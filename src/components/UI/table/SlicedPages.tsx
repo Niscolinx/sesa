f65b7b444c532fcc9be763de0a08e1c330008c4f
Igboanugwo Collins <munisco12@gmail.com>
2023-04-08 20:22:16 +0100

    working on table

--- src/components/UI/table/SlicedPages.tsx
+++ src/components/UI/table/SlicedPages.tsx
@@ -1,23 +1,28 @@
-import { FC } from "react"
-import { Paginate } from "./TableData"
+import { FC } from 'react'
+import { Paginate } from './TableData'
 
 interface SlicedPages {
     pages: any[][] | null
     index: number
 }
 
-const SlicedPages:FC<SlicedPages> = ({ pages, index }) => {
-    
+const SlicedPages: FC<SlicedPages> = ({ pages, index }) => {
     // Return early if page is undefined or empty
     if (!pages || !pages.length) {
         return null
     }
     const page = pages[index]
-    
+
+    const TableItem = ({ name }: any) => {
+        return <p>{name}</p>
+    }
+
     return (
         <>
             {page.map(({ data: { id, user } }) =>
-                user.map((name, i) => <User key={`${id}-${i}`} name={name} />)
+                user.map((name, i) => (
+                    <TableItem key={`${id}-${i}`} name={name} />
+                ))
             )}
         </>
     )
