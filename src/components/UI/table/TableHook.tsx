3528f1e7b7d66f81a706e2787bc5eb4487e02c3d
Igboanugwo Collins <munisco12@gmail.com>
2023-04-08 09:01:49 +0100

    working on the table

--- src/components/UI/table/TableHook.tsx
+++ src/components/UI/table/TableHook.tsx
@@ -1,12 +1,24 @@
-import  { createContext } from 'react'
+import  { createContext, useState } from 'react'
+import { useNavigate } from 'react-router'
+import useAxios from '../../hooks/useAxios'
 
 
 
 
 const CreateTableHook = createContext(null)
 
-function TableHook() {
+function TableHook<T>() {
+    const navigate = useNavigate()
+    const axiosInstance = useAxios()
+
+    const [sortBy, setSortBy] = useState<string | null>(null)
+    const [estateManagerId, setEstateManagerId] = useState('')
+    const [fetchedEstateManagers, setFetchedEstateManagers] = useState<
+        T[]
+    >([])
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
     
+
   return (
     <div>TableHook</div>
   )
