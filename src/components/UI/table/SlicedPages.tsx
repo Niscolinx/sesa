23eb273b4b09c8cb5ef21c170a56b337c86da64e
Igboanugwo Collins <munisco12@gmail.com>
2023-04-10 14:24:45 +0100

    working on table implementation

--- src/components/UI/table/SlicedPages.tsx
+++ src/components/UI/table/SlicedPages.tsx
@@ -80,6 +80,7 @@ const SlicedPages: FC<SlicedPages> = ({
         return (
             <>
                 {sorted.map(({ key, value }: any, idx: number) => {
+                    console.log({value})
                     if (key === 'actions') {
                         return (
                             <TableDropDown
