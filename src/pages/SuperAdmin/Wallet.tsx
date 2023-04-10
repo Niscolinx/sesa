3ac9c4f2841a9533c356c4d9712f064a601c844c
Igboanugwo Collins <munisco12@gmail.com>
2023-04-10 22:10:46 +0100

    working on api implemention

--- src/pages/SuperAdmin/Wallet.tsx
+++ src/pages/SuperAdmin/Wallet.tsx
@@ -4,6 +4,8 @@ import { TbCurrencyNaira } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
 import OverviewChart from '../../components/SuperAdmin/charts/OverviewChart'
 import { OverviewWallet } from '../../components/SuperAdmin/overview/OverviewWallets'
+import useAxios from '../../components/hooks/useAxios'
+import { useQuery } from 'react-query'
 
 export const overviewChart_data = [
     { name: 'Resident Wallet', value: 10, color: '#098DFF' },
@@ -13,98 +15,108 @@ export const overviewChart_data = [
 ]
 
 const Wallet = () => {
+    const axiosInstance = useAxios()
+    const fetchWallets = () => {
+        return axiosInstance({
+            url: '/admin/get/wallet',
+        })
+    }
+
+    const { data, isLoading, isError } = useQuery('wallets', fetchWallets)
+
+    if (isLoading) {
+        return <p className='p-8'> Loading...</p>
+    }
+
     return (
-        <div>
-            <div className='bg-white grid mt-12 py-10 gap-8 rounded-lg'>
-                <div className='flex items-center max-w-[90rem] mx-auto gap-10'>
-                    <div>
-                        <p className='text-[1.6rem] font-semibold'>
-                            Total Balance (Pool Wallet)
+        <div className='bg-white grid mt-12 py-10 gap-8 rounded-lg'>
+            <div className='flex items-center max-w-[90rem] mx-auto gap-10'>
+                <div>
+                    <p className='text-[1.6rem] font-semibold'>
+                        Total Balance (Pool Wallet)
+                    </p>
+                    <h1 className='text-[3rem] font-Satoshi-Medium flex items-center gap-0'>
+                        <TbCurrencyNaira className='text-[3rem]' />
+                        800,836,709.74
+                    </h1>
+                </div>
+                <div className='overviewChart__box'>
+                    <OverviewChart />
+
+                    <div className='overviewChart__label'>
+                        <p className='text-[3.4rem] font-Satoshi-Medium relative'>
+                            45
+                            <span>%</span>
                         </p>
-                        <h1 className='text-[3rem] font-Satoshi-Medium flex items-center gap-0'>
-                            <TbCurrencyNaira className='text-[3rem]' />
-                            800,836,709.74
-                        </h1>
+                        <p className='text-[1rem] max-w-[9.8rem]'>
+                            Resident Wallet
+                        </p>
                     </div>
-                    <div className='overviewChart__box'>
-                        <OverviewChart />
-
-                        <div className='overviewChart__label'>
-                            <p className='text-[3.4rem] font-Satoshi-Medium relative'>
-                                45
-                                <span>%</span>
-                            </p>
-                            <p className='text-[1rem] max-w-[9.8rem]'>
-                                Resident Wallet
-                            </p>
-                        </div>
-                    </div>
-                    <ul className='overviewChart__list'>
-                        {React.Children.toArray(
-                            overviewChart_data.map((item, index) => {
-                                return (
-                                    <li className='overviewChart__list--item'>
-                                        <span
-                                            style={{
-                                                background: item.color,
-                                            }}
-                                        >
-                                            &nbsp;
-                                        </span>
-                                        <p>{item.name}</p>
-                                    </li>
-                                )
-                            })
-                        )}
-                    </ul>
                 </div>
-                <div
-                    className='grid max-w-[90rem] gap-10 mx-auto'
-                    style={{
-                        gridTemplateColumns:
-                            'repeat(auto-fit, minmax(35rem, 1fr))',
-                    }}
-                >
-                    <Link to='/superAdmin/wallet/resident'>
-                        <OverviewWallet
-                            amount={200_333_500.89}
-                            title='Resident Wallet'
-                            isWalletScreen
-                        />
-                    </Link>
+                <ul className='overviewChart__list'>
+                    {React.Children.toArray(
+                        overviewChart_data.map((item, index) => {
+                            return (
+                                <li className='overviewChart__list--item'>
+                                    <span
+                                        style={{
+                                            background: item.color,
+                                        }}
+                                    >
+                                        &nbsp;
+                                    </span>
+                                    <p>{item.name}</p>
+                                </li>
+                            )
+                        })
+                    )}
+                </ul>
+            </div>
+            <div
+                className='grid max-w-[90rem] gap-10 mx-auto'
+                style={{
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(35rem, 1fr))',
+                }}
+            >
+                <Link to='/superAdmin/wallet/resident'>
+                    <OverviewWallet
+                        amount={200_333_500.89}
+                        title='Resident Wallet'
+                        isWalletScreen
+                    />
+                </Link>
 
-                    <Link to='/superAdmin/wallet/security-company'>
-                        <OverviewWallet
-                            amount={160_847}
-                            title={'Security Company Wallet'}
-                            isWalletScreen
-                            bgImgUri='/icons/overview/card/bgS.svg'
-                            lefIconUri='/icons/overview/card/leftS.svg'
-                            bgColor='bg-[#6AB95F]'
-                        />
-                    </Link>
+                <Link to='/superAdmin/wallet/security-company'>
+                    <OverviewWallet
+                        amount={160_847}
+                        title={'Security Company Wallet'}
+                        isWalletScreen
+                        bgImgUri='/icons/overview/card/bgS.svg'
+                        lefIconUri='/icons/overview/card/leftS.svg'
+                        bgColor='bg-[#6AB95F]'
+                    />
+                </Link>
 
-                    <Link to='/superAdmin/wallet/estate'>
-                        <OverviewWallet
-                            amount={1_032_422}
-                            title={'Estate Wallet'}
-                            isWalletScreen
-                            bgImgUri='/icons/overview/card/bgE.svg'
-                            lefIconUri='/icons/overview/card/leftE.svg'
-                            bgColor='bg-[#97346F]'
-                        />
-                    </Link>
-                    <Link to='/superAdmin/wallet/commission'>
-                        <OverviewWallet
-                            amount={4_000_832}
-                            title={'Commission Wallet'}
-                            isWalletScreen
-                            bgImgUri='/icons/overview/card/bgC.svg'
-                            lefIconUri='/icons/overview/card/leftC.svg'
-                            bgColor='bg-[#333333]'
-                        />
-                    </Link>
-                </div>
+                <Link to='/superAdmin/wallet/estate'>
+                    <OverviewWallet
+                        amount={1_032_422}
+                        title={'Estate Wallet'}
+                        isWalletScreen
+                        bgImgUri='/icons/overview/card/bgE.svg'
+                        lefIconUri='/icons/overview/card/leftE.svg'
+                        bgColor='bg-[#97346F]'
+                    />
+                </Link>
+                <Link to='/superAdmin/wallet/commission'>
+                    <OverviewWallet
+                        amount={4_000_832}
+                        title={'Commission Wallet'}
+                        isWalletScreen
+                        bgImgUri='/icons/overview/card/bgC.svg'
+                        lefIconUri='/icons/overview/card/leftC.svg'
+                        bgColor='bg-[#333333]'
+                    />
+                </Link>
             </div>
         </div>
     )
