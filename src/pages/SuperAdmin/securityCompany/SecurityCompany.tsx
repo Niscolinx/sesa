d67ee7912526cbdf6ae30eec12e2b39768e03aab
Igboanugwo Collins <munisco12@gmail.com>
2023-04-10 17:52:21 +0100

    implementing table

--- src/pages/SuperAdmin/securityCompany/SecurityCompany.tsx
+++ src/pages/SuperAdmin/securityCompany/SecurityCompany.tsx
@@ -2,20 +2,37 @@ import { useState } from 'react'
 import { IoMdAdd } from 'react-icons/io'
 import RenderSecurityCompanies from './RenderedSecurityCompanies'
 import { useNavigate } from 'react-router'
+import Table from '../../../components/UI/table/Table'
 
 function SecurityCompany() {
-    const navigate = useNavigate()
-
-    const [securityCompanies, setSecurityCompanies] = useState(false)
-
-    const addSecurityCompanyHandler = () => {
-        setSecurityCompanies(true)
-
-        navigate('/superAdmin/security-company/add')
-    }
+    
 
     return (
-       
+        <div className='rounded-lg mt-[3rem] h-[80vh]'>
+            <Table
+                fetch_url={'/manager/get/all'}
+                title={'estateManager'}
+                view_page_url={'/superAdmin/estateManagers/view/'}
+                add_page_url={'/superAdmin/security-company/add'}
+                is_add_btn={true}
+                THeader={[
+                    'name',
+                    'gender',
+                    'phone number',
+                    'joined date',
+                    'status',
+                    'actions',
+                ]}
+                data_to_display={[
+                    'name',
+                    'image',
+                    'gender',
+                    'phone',
+                    'created_at',
+                    'status',
+                ]}
+                deactivateProp={{ url: 'change/user/status' }}
+            />
         </div>
     )
 }
