8f5a401800d6d9776461892a1e5593e45c8dbad3
Igboanugwo Collins <munisco12@gmail.com>
2023-04-10 17:50:55 +0100

    working on table

--- src/pages/SuperAdmin/securityCompany/SecurityCompany.tsx
+++ src/pages/SuperAdmin/securityCompany/SecurityCompany.tsx
@@ -15,30 +15,7 @@ function SecurityCompany() {
     }
 
     return (
-        <div>
-            <div className='rounded-lg mt-[3rem] h-[80vh]'>
-                {!securityCompanies ? (
-                    <section>
-                        <RenderSecurityCompanies />
-                    </section>
-                ) : (
-                    <section className='estates__wrapper bg-white'>
-                        <img src='/icons/admins/errorSvg.svg' alt='' />
-                        <p className='text'>
-                            Ooops you have not added any security Company yet
-                        </p>
-                        <button
-                            className='btn text-white bg-color-blue-1 flex items-center gap-4 py-4 px-16 rounded-lg'
-                            onClick={addSecurityCompanyHandler}
-                        >
-                            <span>
-                                <IoMdAdd />
-                            </span>{' '}
-                            Add Company
-                        </button>
-                    </section>
-                )}
-            </div>
+       
         </div>
     )
 }
