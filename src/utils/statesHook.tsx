f40d190ce6356eda50680a9bbcc2bb99f7328313
Igboanugwo Collins <munisco12@gmail.com>
2023-04-11 17:58:27 +0100

    working on the API implementation

--- src/utils/statesHook.tsx
+++ src/utils/statesHook.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState } from "react"
+import { useQuery } from "react-query"
 
-function statesHook() {
-  return (
-    <div>statesHook</div>
-  )
+const useFetchData = (url) => {
+    const [queryParams, setQueryParams] = useState({})
+
+    const fetchData = (
+
+        return 
+    )
+
+    const { isLoading, data, error, refetch } = useQuery(
+        ['data', queryParams],
+        async () => {
+            const res = await fetch(url, queryParams)
+            if (!res.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return res.json()
+        }
+    )
+
+    const updateQueryParams = (newParams) => {
+        setQueryParams(newParams)
+    }
+
+    return { isLoading, data, error, refetch, updateQueryParams }
 }
-
-export default statesHook
