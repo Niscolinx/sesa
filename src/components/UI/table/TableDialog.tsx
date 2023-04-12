d87bb26215c9a1d92814369828766248d1824e8a
Igboanugwo Collins <munisco12@gmail.com>
2023-04-12 12:35:10 +0100

    working on API implementation

--- src/components/UI/table/TableDialog.tsx
+++ src/components/UI/table/TableDialog.tsx
@@ -30,7 +30,7 @@ function TableDialog() {
             return axiosInstance({
                 url: '/admin/category',
                 method: 'post',
-                data: artisanCategory,
+                data: { name: artisanCategory },
             })
         }
         return axiosInstance({
