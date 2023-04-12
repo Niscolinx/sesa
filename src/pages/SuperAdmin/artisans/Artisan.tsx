7ec4727d6c5fe77a0109ec069dea036021101f2d
Igboanugwo Collins <munisco12@gmail.com>
2023-04-12 10:04:23 +0100

    working on API implementation

--- src/pages/SuperAdmin/artisans/Artisan.tsx
+++ src/pages/SuperAdmin/artisans/Artisan.tsx
@@ -6,90 +6,87 @@ import Table from '../../../components/UI/table/Table'
 
 function ArtisanCategory() {
     return (
-        <div className='rounded-lg mt-[3rem] h-[80vh]'>
-            <Table
-                fetch_url={'/manager/get/all'}
-                title={'estateManager'}
-                view_page_url={'/superAdmin/estateManagers/view/'}
-                add_page_url={'/superAdmin/estateManagers/add'}
-                is_add_btn={true}
-                THeader={[
-                    'name',
-                    'gender',
-                    'phone number',
-                    'joined date',
-                    'status',
-                    'actions',
-                ]}
-                data_to_display={[
-                    'name',
-                    'image',
-                    'gender',
-                    'phone',
-                    'created_at',
-                    'status',
-                ]}
-                deactivateProp={{ url: 'change/user/status' }}
-            />
-        </div>
+        <Table
+            fetch_url={'/manager/get/all'}
+            title={'estateManager'}
+            view_page_url={'/superAdmin/estateManagers/view/'}
+            add_page_url={'/superAdmin/estateManagers/add'}
+            is_add_btn={true}
+            THeader={[
+                'name',
+                'gender',
+                'phone number',
+                'joined date',
+                'status',
+                'actions',
+            ]}
+            data_to_display={[
+                'name',
+                'image',
+                'gender',
+                'phone',
+                'created_at',
+                'status',
+            ]}
+            deactivateProp={{ url: 'change/user/status' }}
+        />
     )
 }
 
 function ArtisanList() {
     return (
-            <Table
-                fetch_url={'/manager/get/all'}
-                title={'estateManager'}
-                view_page_url={'/superAdmin/estateManagers/view/'}
-                add_page_url={'/superAdmin/estateManagers/add'}
-                is_add_btn={true}
-                THeader={[
-                    'name',
-                    'gender',
-                    'phone number',
-                    'joined date',
-                    'status',
-                    'actions',
-                ]}
-                data_to_display={[
-                    'name',
-                    'image',
-                    'gender',
-                    'phone',
-                    'created_at',
-                    'status',
-                ]}
-                deactivateProp={{ url: 'change/user/status' }}
-            />
+        <Table
+            fetch_url={'/manager/get/all'}
+            title={'estateManager'}
+            view_page_url={'/superAdmin/estateManagers/view/'}
+            add_page_url={'/superAdmin/estateManagers/add'}
+            is_add_btn={true}
+            THeader={[
+                'name',
+                'gender',
+                'phone number',
+                'joined date',
+                'status',
+                'actions',
+            ]}
+            data_to_display={[
+                'name',
+                'image',
+                'gender',
+                'phone',
+                'created_at',
+                'status',
+            ]}
+            deactivateProp={{ url: 'change/user/status' }}
+        />
     )
 }
 function ArtisanGroup() {
     return (
-            <Table
-                fetch_url={'/manager/get/all'}
-                title={'estateManager'}
-                view_page_url={'/superAdmin/estateManagers/view/'}
-                add_page_url={'/superAdmin/estateManagers/add'}
-                is_add_btn={true}
-                THeader={[
-                    'name',
-                    'gender',
-                    'phone number',
-                    'joined date',
-                    'status',
-                    'actions',
-                ]}
-                data_to_display={[
-                    'name',
-                    'image',
-                    'gender',
-                    'phone',
-                    'created_at',
-                    'status',
-                ]}
-                deactivateProp={{ url: 'change/user/status' }}
-            />
-     
+        <Table
+            fetch_url={'/manager/get/all'}
+            title={'estateManager'}
+            view_page_url={'/superAdmin/estateManagers/view/'}
+            add_page_url={'/superAdmin/estateManagers/add'}
+            is_add_btn={true}
+            THeader={[
+                'name',
+                'gender',
+                'phone number',
+                'joined date',
+                'status',
+                'actions',
+            ]}
+            data_to_display={[
+                'name',
+                'image',
+                'gender',
+                'phone',
+                'created_at',
+                'status',
+            ]}
+            deactivateProp={{ url: 'change/user/status' }}
+        />
     )
 }
 
