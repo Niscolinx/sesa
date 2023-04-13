dc406ffd3d0a0e1526741a95fc14d08632c1d3a5
Igboanugwo Collins <munisco12@gmail.com>
2023-04-13 12:32:09 +0100

    working on API implementation

--- src/pages/SuperAdmin/platformSettings/settings/PropertyType.tsx
+++ src/pages/SuperAdmin/platformSettings/settings/PropertyType.tsx
@@ -3,17 +3,15 @@ import Table from '../../../../components/UI/table/Table'
 
 function PropertyType() {
     return (
-        <div className='rounded-lg mt-[3rem] h-[80vh]'>
-            <Table
-                fetch_url={'/platformsettings/propertytype/getall'}
-                title={'propertyType'}
-                is_dropdown={false}
-                add_page_url={'/superAdmin/platformSettings/addProperty'}
-                is_add_btn={true}
-                THeader={['property type', 'description']}
-                data_to_display={['property_type', 'description']}
-            />
-        </div>
+        <Table
+            fetch_url={'/platformsettings/propertytype/getall'}
+            title={'propertyType'}
+            is_dropdown={false}
+            add_page_url={'/superAdmin/platformSettings/addProperty'}
+            is_add_btn={true}
+            THeader={['property type', 'description']}
+            data_to_display={['property_type', 'description']}
+        />
     )
 }
 
