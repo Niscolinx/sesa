38fdac51dfb739596c49499f1a299d3570a57be7
Igboanugwo Collins <munisco12@gmail.com>
2023-04-30 20:44:05 +0100

    working on UI updates

--- src/pages/SuperAdmin/admin/AddAdmin.tsx
+++ src/pages/SuperAdmin/admin/AddAdmin.tsx
@@ -94,14 +94,16 @@ const AddAdmin = () => {
 
             if (KBSize.length > 3) {
                 const MBSize = Number(KBSize) / 1000
-                return MBSize > 2
-                    ? setResponseMessage({
+                return MBSize > 2 &&
+                    setResponseMessage({
                           className: 'text-red-600',
                           displayMessage: 'File size must not exceed 2MB',
                       })
-                    : setResponseMessage(null)
+                  
             }
         }
+
+        setResponseMessage(null)
 
         const adminData = {
             name: `${first_name} ${last_name}`,
