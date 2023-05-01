d73610d18e784c6b329f5dcb5cd1a3c50e8d1e5f
Igboanugwo Collins <munisco12@gmail.com>
2023-05-01 13:10:08 +0100

    working on UI updates

--- src/components/SuperAdmin/UI/StarRating.tsx
+++ src/components/SuperAdmin/UI/StarRating.tsx
@@ -15,19 +15,10 @@ const StarRating:FC<IStarRating> = ({starsNum}) => {
     const [hover, setHover] = useState(0)
     return (
         <div>
-            {[...Array(starsNum)].map((star, index) => {
+            {[...Array(5)].map((star, index) => {
                 index += 1
                 return (
-                    <button
-                        type='button'
-                        key={index}
-                        // className={
-                        //     index <= ((rating && hover) || hover) ? 'on' : 'off'
-                        // }
-                        // onClick={() => setRating(index)}
-                        // onMouseEnter={() => setHover(index)}
-                        // onMouseLeave={() => setHover(rating)}
-                    >
+                    
                         <span className='text-[#E69B55]'>&#9733;</span>
                     </button>
                 )
