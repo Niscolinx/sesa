084a7376443aa5ac6095e7e3ad3ea508b472685f
Igboanugwo Collins <munisco12@gmail.com>
2023-05-02 18:19:30 +0100

    working on UI

--- src/pages/SuperAdmin/admin/ViewAdmin.tsx
+++ src/pages/SuperAdmin/admin/ViewAdmin.tsx
@@ -131,7 +131,7 @@ const ViewAdmin = () => {
     })
 
     const { data: get_admin_response, isLoading: get_admin_loading } = useQuery(
-        ['get_admin'],
+        [`get_admin_${admin_id}`],
         getAdmin
     )
 
@@ -281,21 +281,21 @@ const ViewAdmin = () => {
                         {<>{console.log(get_admin_response?.data)}</>}
                         {get_admin_response?.data.status ? (
                             <button
-                                className='border border-green-600 px-16 py-4 flex items-center  rounded-lg gap-4'
-                                onClick={openDialog}
-                            >
-                                <span className='text-green-600 text-[1.4rem] font-semibold capitalize'>
-                                    Activate
-                                </span>
-                            </button>
-                        ) : (
-                            <button
                                 className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
                                 onClick={openDialog}
                             >
                                 <img src='/icons/admins/delete.svg' alt='' />
                                 <span className='text-red-600 text-[1.4rem] font-semibold capitalize'>
                                     deactivate
+                                </span>
+                            </button>
+                        ) : (
+                            <button
+                                className='border border-green-600 px-16 py-4 flex items-center  rounded-lg gap-4'
+                                onClick={openDialog}
+                            >
+                                <span className='text-green-600 text-[1.4rem] font-semibold capitalize'>
+                                    Activate
                                 </span>
                             </button>
                         )}
