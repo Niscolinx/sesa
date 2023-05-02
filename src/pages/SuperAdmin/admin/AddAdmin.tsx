837e4defd9cc85bfa2822a6a8fed89614e252c18
Igboanugwo Collins <munisco12@gmail.com>
2023-05-02 13:48:51 +0100

    working on UI

--- src/pages/SuperAdmin/admin/AddAdmin.tsx
+++ src/pages/SuperAdmin/admin/AddAdmin.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { IoMdAdd } from 'react-icons/io'
-import { useMutation } from 'react-query'
+import { QueryClient, useMutation, useQueryClient } from 'react-query'
 import Input, { SelectProps } from '../../../components/UI/input/Input'
 import useAxios from '../../../components/hooks/useAxios'
 import Spinner from '../../../components/UI/Spinner'
@@ -64,7 +64,7 @@ const AddAdmin = () => {
         })
     }
 
-    const query
+    const queryClient = useQueryClient()
     const { mutate, isLoading } = useMutation(postAdmin, {
         onSuccess: () => {
             handleOpen()
