c1fb6f157f3203d8ac7cd98d6c1b8f32fab4bfa1
Igboanugwo Collins <munisco12@gmail.com>
2023-05-03 18:22:50 +0100

    working on UI changes

--- src/pages/SuperAdmin/advert/Advert.tsx
+++ src/pages/SuperAdmin/advert/Advert.tsx
@@ -1,12 +1,47 @@
-
-import RenderAdverts from './RenderAdverts'
+import { useState } from 'react'
+import AdvertList from './AdvertList'
+import AvailableEstateAdvert from './AvailableEstateAdvert'
 
 function Advert() {
+    type PathSwitch = 'advertList' | 'availableEstates'
+
+    const [pathToSwitch, setPathToSwitch] = useState<PathSwitch>('advertList')
+
+    const handlePathSwitch: Record<PathSwitch, JSX.Element> = {
+        advertList: <AdvertList />,
+        availableEstates: <AvailableEstateAdvert />,
+    }
 
     return (
         <div>
             <div className='rounded-lg mt-[3rem] h-[80vh]'>
-                <RenderAdverts />
+                <div className='estateDetail__radioBox'>
+                    <input
+                        type='radio'
+                        name='advert'
+                        id='advertList'
+                        className='hidden'
+                        defaultChecked
+                        onChange={() => setPathToSwitch('advertList')}
+                    />
+                    <label htmlFor='advertList' className='capitalize'>
+                        Advert List
+                    </label>
+
+                    <input
+                        type='radio'
+                        name='advert'
+                        id='availableEstates'
+                        className='hidden'
+                        onChange={() => setPathToSwitch('availableEstates')}
+                    />
+                    <label htmlFor='availableEstates'>Available Estates</label>
+                </div>
+                <div className='mt-8 grid gap-8'>
+                    <section className='bg-color-white rounded-lg border min-w-[112rem] overflow-scroll'>
+                        {handlePathSwitch[pathToSwitch]}
+                    </section>
+                </div>
             </div>
         </div>
     )
