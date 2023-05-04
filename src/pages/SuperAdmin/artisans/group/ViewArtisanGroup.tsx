05b99a37cbc779ba3c24cf636583a85243e0f916
Igboanugwo Collins <munisco12@gmail.com>
2023-05-04 10:46:50 +0100

    working on UI updates

--- src/pages/SuperAdmin/artisans/group/ViewArtisanGroup.tsx
+++ src/pages/SuperAdmin/artisans/group/ViewArtisanGroup.tsx
@@ -1,6 +1,10 @@
 import { useNavigate, useParams } from 'react-router'
 import Table from '../../../../components/UI/table/Table'
 import useFetchData from '../../../../utils/useFetchData'
+import { useState, FormEvent } from 'react'
+import { useMutation } from 'react-query'
+import { toast } from 'react-toastify'
+import useAxios from '../../../../components/hooks/useAxios'
 
 export interface IViewArtisanGroup {
     id: string
@@ -13,12 +17,73 @@ export interface IViewArtisanGroup {
 
 const ViewArtisanGroup = () => {
     const params = useParams()
+    const axiosInstance = useAxios()
 
     const groupId = params.id?.replace(':', '')
 
     const { data: artisan_data, isLoading: artisan_loading } = useFetchData({
         url: `/admin/group/getSingleGroup/${groupId}`,
     })
+
+     const [name, setName] = useState('')
+
+     const updateRequest = () => {
+         return axiosInstance({
+             url: `/admin/category/update/2`,
+             // url: `/admin/category/update/${category_id}`,
+             method: 'post',
+             data: { name },
+         })
+     }
+     const { isLoading, mutate } = useMutation(
+         'update_category_name',
+         updateRequest,
+         {
+             onSuccess: () => {
+                 toast('Category updated successfully', {
+                     type: 'success',
+                     className: 'bg-green-100 text-green-600 text-[1.4rem]',
+                 })
+             },
+
+             onError: () => {
+                 toast('Failed to update category', {
+                     type: 'error',
+                     className: 'bg-red-100 text-red-600 text-[1.4rem]',
+                 })
+             },
+         }
+     )
+
+     const { data: category_detail, isLoading: category_detail_loading } =
+         useFetchData({
+             url: '/admin/category/get/single/2',
+             name: 'category_single',
+         })
+     const { data: category_users, isLoading: category_users_loading } =
+         useFetchData({
+             url: '/admin/category/get/single/users/2',
+             name: 'category_users',
+         })
+
+     const onSubmit = (e: FormEvent) => {
+         e.preventDefault()
+
+         if (name === '') {
+             toast('Update the name of the category', {
+                 type: 'error',
+                 className: 'bg-red-100 text-red-600 text-[1.4rem]',
+             })
+             return
+         }
+
+         mutate()
+     }
+
+     if (category_detail_loading || category_users_loading) {
+         return <p className='p-8'>Loading...</p>
+     }
+
 
   
 
@@ -50,7 +115,7 @@ const ViewArtisanGroup = () => {
                         Update
                     </button>
                 </div>
-
+{/* 
                 <Table
                     title={'artisan'}
                     view_page_url={'/superAdmin/admin/view/'}
@@ -67,7 +132,7 @@ const ViewArtisanGroup = () => {
                         'phone_number',
                     ]}
                     isDataProvided={true}
-                    providedData={data.data}
+                    providedData={artisan_data.data}
                     deactivateProp={{ url: '/admin/deactivate_activate' }}
                 />
                 <Table
@@ -77,9 +142,10 @@ const ViewArtisanGroup = () => {
                     actions={['remove']}
                     data_to_display={['estate_name', 'address']}
                     isDataProvided={true}
-                    providedData={data.data}
+                    providedData={artisan_data.data}
                 />
-            </div>
+                */}
+                </div> 
         </>
     )
 }
