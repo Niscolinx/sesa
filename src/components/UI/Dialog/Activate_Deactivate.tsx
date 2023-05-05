f50621c6360afa4892f5785a377754b2892ea48c
Igboanugwo Collins <munisco12@gmail.com>
2023-05-05 12:59:27 +0100

    working on API and UI updates

---
+++ src/components/UI/Dialog/Activate_Deactivate.tsx
@@ -0,0 +1,88 @@
+import React, { useRef } from 'react'
+import { useMutation } from 'react-query'
+import { toast } from 'react-toastify'
+
+function Activate_Deactivate() {
+     const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+
+    const postDeactivate = () => {
+        return axiosInstance({
+            url: '/security-company/deactivate_activate',
+            method: 'post',
+            data: { id: company_id },
+        })
+    }
+
+        const { mutate: deactivate_mutation, isLoading: deactivate_loading } =
+        useMutation(postDeactivate, {
+            onSuccess: (res) => {
+                toast('Company Deactivated successfully', {
+                    type: 'success',
+                    className: 'bg-green-100 text-green-600 text-[1.4rem]',
+                })
+            },
+            onError: (err: any) => {
+                toast('Company Failed to ', {
+                    type: 'success',
+                    className: 'bg-green-100 text-green-600 text-[1.4rem]',
+                })
+                return setResponseMessage({
+                    className: 'text-red-600',
+                    displayMessage: err?.response?.data.message,
+                })
+            },
+
+            onSettled: () => {
+                return handleClose()
+            },
+        })
+
+    const handleClose = () => {
+        if (dialogRef.current) {
+            dialogRef.current.close()
+        }
+    }
+
+    const handleOpen = () => {
+        if (dialogRef.current) {
+            dialogRef.current.showModal()
+        }
+    }
+
+    const confirmDeactivation = () => {
+        return deactivate_mutation()
+    }
+
+  return (
+      <dialog className='dialog' ref={dialogRef}>
+          <section className='grid place-content-center w-full h-[100vh]'>
+              <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8 text-[1.6rem]'>
+                  <img src='/icons/admins/modalWarning.svg' alt='' />
+
+                  <p>
+                      Are you sure you want to deactivate this security company?
+                  </p>
+
+                  <div className='flex w-full justify-center gap-8'>
+                      <button
+                          className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
+                          onClick={() => handleClose()}
+                      >
+                          Cancel
+                      </button>
+
+                      <button
+                          className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem] capitalize'
+                          onClick={confirmDeactivation}
+                      >
+                          {deactivate_loading ? 'Loading...' : 'deactivate'}
+                      </button>
+                  </div>
+              </div>
+          </section>
+      </dialog>
+  )
+}
+
+export default Activate_Deactivate
--- src/pages/SuperAdmin/securityCompany/SecurityCompanyDetail.tsx
+++ src/pages/SuperAdmin/securityCompany/SecurityCompanyDetail.tsx
@@ -109,13 +109,6 @@ const SecurityCompanyDetail = () => {
         return <p className='p-4'> Not found!</p>
     }
 
-    const postDeactivate = () => {
-        return axiosInstance({
-            url: '/security-company/deactivate_activate',
-            method: 'post',
-            data: { id: company_id },
-        })
-    }
     const postRequest = (data: any) => {
         return axiosInstance({
             url: `/security-company/update/${company_id}`,
@@ -130,29 +123,7 @@ const SecurityCompanyDetail = () => {
         })
     }
 
-    const { mutate: deactivate_mutation, isLoading: deactivate_loading } =
-        useMutation(postDeactivate, {
-            onSuccess: (res) => {
-                toast('Company Deactivated successfully', {
-                    type: 'success',
-                    className: 'bg-green-100 text-green-600 text-[1.4rem]',
-                })
-            },
-            onError: (err: any) => {
-                toast('Company Failed to ', {
-                    type: 'success',
-                    className: 'bg-green-100 text-green-600 text-[1.4rem]',
-                })
-                return setResponseMessage({
-                    className: 'text-red-600',
-                    displayMessage: err?.response?.data.message,
-                })
-            },
-
-            onSettled: () => {
-                return handleClose()
-            },
-        })
+ 
 
     const { data: get_response, isLoading: get_loading } = useQuery(
         [`get_company_${company_id}`],
@@ -217,10 +188,6 @@ const SecurityCompanyDetail = () => {
         }
     }
 
-    const confirmDeactivation = () => {
-        return deactivate_mutation()
-    }
-
     if (get_loading) {
         return <p className='p-8'> Loading...</p>
     }
@@ -228,36 +195,7 @@ const SecurityCompanyDetail = () => {
     return (
         <>
             <ToastContainer />
-            <dialog className='dialog' ref={dialogRef}>
-                <section className='grid place-content-center w-full h-[100vh]'>
-                    <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8 text-[1.6rem]'>
-                        <img src='/icons/admins/modalWarning.svg' alt='' />
-
-                        <p>
-                            Are you sure you want to deactivate this security
-                            company?
-                        </p>
-
-                        <div className='flex w-full justify-center gap-8'>
-                            <button
-                                className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
-                                onClick={() => handleClose()}
-                            >
-                                Cancel
-                            </button>
-
-                            <button
-                                className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem] capitalize'
-                                onClick={confirmDeactivation}
-                            >
-                                {deactivate_loading
-                                    ? 'Loading...'
-                                    : 'deactivate'}
-                            </button>
-                        </div>
-                    </div>
-                </section>
-            </dialog>
+            
             <div className='grid p-8 bg-white min-h-[60vh] items-baseline overflow-y-scroll rounded-lg'>
                 <div className='flex justify-between items-center my-10'>
                     <img
