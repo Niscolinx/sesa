d73900945d8eb51644235d3822ebb85868dbe361
Igboanugwo Collins <munisco12@gmail.com>
2023-05-05 17:10:55 +0100

    working on API and UI updates

--- src/pages/SuperAdmin/admin/ViewAdmin.tsx
+++ src/pages/SuperAdmin/admin/ViewAdmin.tsx
@@ -5,6 +5,7 @@ import Input, { SelectProps } from '../../../components/UI/input/Input'
 import { useNavigate, useParams } from 'react-router'
 import { toast, ToastContainer } from 'react-toastify'
 import useAxios from '../../../components/hooks/useAxios'
+import Activate_Deactivate from '../../../components/UI/Dialog/Activate_Deactivate'
 
 const ViewAdmin = () => {
     interface Inputs {
@@ -218,40 +219,6 @@ const ViewAdmin = () => {
     return (
         <>
             <ToastContainer />
-            <dialog className='dialog' ref={dialogRef}>
-                <section className='grid place-content-center w-full h-[100vh]'>
-                    <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8'>
-                        <img
-                            src='/icons/admins/modalWarning.svg'
-                            alt=''
-                            className='animate__animated animate__pulse '
-                            style={{
-                                animationIterationCount: 'infinite',
-                            }}
-                        />
-                        <p>Are you sure you want to deactivate this admin?</p>
-
-                        <div className='flex w-full justify-center gap-8'>
-                            <button
-                                className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
-                                onClick={closeDialog}
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                className='bg-red-500 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem] capitalize'
-                                onClick={() =>
-                                    deactivate_admin_mutation(admin_id!)
-                                }
-                            >
-                                {deactivate_admin_loading
-                                    ? 'Loading...'
-                                    : 'deactivate'}
-                            </button>
-                        </div>
-                    </div>
-                </section>
-            </dialog>
 
             <div className='bg-white rounded-2xl grid p-8'>
                 <div className='flex justify-between items-center mb-20'>
@@ -278,26 +245,13 @@ const ViewAdmin = () => {
                     />
 
                     <div className='flex gap-8'>
-                        {get_admin_response?.data.status ? (
-                            <button
-                                className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
-                                onClick={openDialog}
-                            >
-                                <img src='/icons/admins/delete.svg' alt='' />
-                                <span className='text-red-600 text-[1.4rem] font-semibold capitalize'>
-                                    deactivate
-                                </span>
-                            </button>
-                        ) : (
-                            <button
-                                className='border border-green-600 px-16 py-4 flex items-center  rounded-lg gap-4'
-                                onClick={openDialog}
-                            >
-                                <span className='text-green-600 text-[1.4rem] font-semibold capitalize'>
-                                    Activate
-                                </span>
-                            </button>
-                        )}
+                     <Activate_Deactivate
+                            id={company_id}
+                            url={'/security-company/deactivate_activate'}
+                            status={get_response.data.status}
+                            title={'security company'}
+                            queryCache={`get_company_${company_id}`}
+                        />
                     </div>
                 </div>
                 <p className='text-[2rem] font-Satoshi-Medium'>
