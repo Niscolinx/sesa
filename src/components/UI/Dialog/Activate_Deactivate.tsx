3218743cf365e282d2aaf69caa699d368ce8e1f0
Igboanugwo Collins <munisco12@gmail.com>
2023-05-05 13:01:38 +0100

    working on API and UI updates

--- src/components/UI/Dialog/Activate_Deactivate.tsx
+++ src/components/UI/Dialog/Activate_Deactivate.tsx
@@ -55,33 +55,57 @@ function Activate_Deactivate() {
     }
 
   return (
-      <dialog className='dialog' ref={dialogRef}>
-          <section className='grid place-content-center w-full h-[100vh]'>
-              <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8 text-[1.6rem]'>
-                  <img src='/icons/admins/modalWarning.svg' alt='' />
+      <>
+          <dialog className='dialog' ref={dialogRef}>
+              <section className='grid place-content-center w-full h-[100vh]'>
+                  <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] h-[30rem] gap-8 text-[1.6rem]'>
+                      <img src='/icons/admins/modalWarning.svg' alt='' />
 
-                  <p>
-                      Are you sure you want to deactivate this security company?
-                  </p>
+                      <p>
+                          Are you sure you want to deactivate this security
+                          company?
+                      </p>
 
-                  <div className='flex w-full justify-center gap-8'>
-                      <button
-                          className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
-                          onClick={() => handleClose()}
-                      >
-                          Cancel
-                      </button>
+                      <div className='flex w-full justify-center gap-8'>
+                          <button
+                              className='btn border-[#0556E5] text-[#0556E5] border rounded-lg w-[15rem]'
+                              onClick={() => handleClose()}
+                          >
+                              Cancel
+                          </button>
 
-                      <button
-                          className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem] capitalize'
-                          onClick={confirmDeactivation}
-                      >
-                          {deactivate_loading ? 'Loading...' : 'deactivate'}
-                      </button>
+                          <button
+                              className='bg-red-600 py-2 px-12 text-white text-[1.6rem] rounded-lg w-[15rem] capitalize'
+                              onClick={confirmDeactivation}
+                          >
+                              {deactivate_loading ? 'Loading...' : 'deactivate'}
+                          </button>
+                      </div>
                   </div>
-              </div>
-          </section>
-      </dialog>
+              </section>
+          </dialog>
+
+          {get_response?.data.status ? (
+              <button
+                  className='border border-red-600 px-16 py-4 flex items-center  rounded-lg gap-4'
+                  onClick={() => handleOpen()}
+              >
+                  <img src='/icons/admins/delete.svg' alt='' />
+                  <span className='text-red-600 text-[1.4rem] font-semibold'>
+                      Deactivate
+                  </span>
+              </button>
+          ) : (
+              <button
+                  className='border border-green-600 px-16 py-4 flex items-center  rounded-lg gap-4'
+                  onClick={() => handleOpen()}
+              >
+                  <span className='text-green-600 text-[1.4rem] font-semibold capitalize'>
+                      Activate
+                  </span>
+              </button>
+          )}
+      </>
   )
 }
 
