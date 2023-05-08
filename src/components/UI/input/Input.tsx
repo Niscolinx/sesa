a88beaa1025312af5983882e8e45224db582824c
Igboanugwo Collins <munisco12@gmail.com>
2023-05-08 18:10:42 +0100

    working on estate details

--- src/components/UI/input/Input.tsx
+++ src/components/UI/input/Input.tsx
@@ -122,9 +122,9 @@ const Input: FC<Partial<Input> & { label: string }> = ({
     }
 
     useEffect(() => {
-        console.log({tag, label})
-        if (tag === 'money') {
-            setPrice(getValues!(label))
+        console.log({tag, label, getValues}, getValues(label))
+        if (tag === 'money' && getValues) {
+            setPrice(getValues(label))
         }
     }, [tag, label])
 
