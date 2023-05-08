7b81d28f040322f1c086ac30e8b462fef90d0aa1
Igboanugwo Collins <munisco12@gmail.com>
2023-05-08 17:39:33 +0100

    working on estate details

--- src/pages/SuperAdmin/Wallet/resident/ResidentWalletDetails.tsx
+++ src/pages/SuperAdmin/Wallet/resident/ResidentWalletDetails.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import { useParams } from 'react-router'
 import { toast } from 'react-toastify'
 import useFetchData from '../../../../utils/useFetchData'
+import Input from '../../../../components/UI/input/Input'
 
 const ResidentWalletDetails = () => {
     interface Inputs {
@@ -20,6 +21,7 @@ const ResidentWalletDetails = () => {
         label: keyof Inputs
         type?: string
         name?: string
+        tag?: string
         disabled?: string
     }
 
@@ -58,6 +60,7 @@ const ResidentWalletDetails = () => {
         {
             label: 'amount',
             name: 'transaction_amount',
+            tag: 'money',
         },
         {
             label: 'tran_id',
@@ -83,24 +86,17 @@ const ResidentWalletDetails = () => {
 
     useEffect(() => {
         if (data) {
-            const {
-                type,
-                created_at,
-                category,
-                name,
-                amount,
-                tran_id,
-                description,
-            } = data
+            const { created_at, name, ...other } = data
 
             const transaction_date = created_at.slice(0, 10)
             const transaction_time = created_at.slice(11, -1)
             const transaction_source = name
 
-
             reset({
                 transaction_date,
                 transaction_time,
+                transaction_source,
+                ...other,
             })
         }
     }, [data])
@@ -111,178 +107,33 @@ const ResidentWalletDetails = () => {
 
     return (
         <div className=' p-8 bg-white min-h-[60vh] rounded-lg overflow-y-scroll'>
-            <section
-                className='grid max-w-[65vw] gap-16'
-                style={{
-                    gridTemplateColumns: 'repeat(auto-fit, minmax(40rem, 1fr))',
-                }}
-            >
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='residentName'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Type
-                    </label>
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='residentName'
-                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={'02-May-2022'}
-                    />
-                </div>
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='withdrawalTime'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Time
-                    </label>
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='withdrawalTime'
-                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={'3:00pm'}
-                    />
-                </div>
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='status'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Type
-                    </label>
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='status'
-                        className='border border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-green-500 disabled:cursor-not-allowed'
-                        value={'Credit'}
-                    />
-                </div>
+            <div
+                    className='grid max-w-[84rem] gap-16 mt-12 '
+                    style={{
+                        gridTemplateColumns:
+                            ' repeat(auto-fit, minmax(35rem, 1fr))',
+                    }}
+                >
+                    <>
+                        {formInputs.map((input, idx) => {
+                            const { label, name, tag } = input
 
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Category
-                    </label>
+                            return (
+                                <Input
+                                    key={idx + label}
+                                    label={label}
+                                    tag={tag}
+                                    register={register}
+                                    formErrors={formErrors}
+                                    name={name}
+                                 
+                                />
+                            )
+                        })}
 
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='description'
-                        className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={'Product Purchase'}
-                    />
-                </div>
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Source
-                    </label>
-
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='description'
-                        className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={'Product Purchase'}
-                    />
-                </div>
-
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Amount
-                    </label>
-                    <div className='relative flex items-center'>
-                        <img
-                            src='/icons/Naira.svg'
-                            alt=''
-                            className='absolute left-3'
-                        />
-                        <input
-                            disabled
-                            type='text'
-                            required
-                            id='description'
-                            className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                            value={6000}
-                        />
-                    </div>
-                </div>
-
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction ID
-                    </label>
-
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='description'
-                        className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={1004238232}
-                    />
-                </div>
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Transaction Amount
-                    </label>
-                    <div className='relative flex items-center'>
-                        <img
-                            src='/icons/Naira.svg'
-                            alt=''
-                            className='absolute left-3'
-                        />
-                        <input
-                            disabled
-                            type='text'
-                            required
-                            id='description'
-                            className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                            value={6000}
-                        />
-                    </div>
-                </div>
-                <div className='w-full grid gap-4'>
-                    <label
-                        htmlFor='description'
-                        className='text-[1.4rem] font-semibold'
-                    >
-                        Narration
-                    </label>
-
-                    <input
-                        disabled
-                        type='text'
-                        required
-                        id='description'
-                        className='border pl-8 border-color-grey p-4 outline-none rounded-lg w-full text-[1.6rem] disabled:text-gray-500 disabled:cursor-not-allowed'
-                        value={'Product Purchase for 6000'}
-                    />
-                </div>
-            </section>
+                       
+                    </>
+                </form>
         </div>
     )
 }
