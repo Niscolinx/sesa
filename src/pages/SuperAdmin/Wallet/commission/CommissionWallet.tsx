6fd2cb1fefed963be2f3f3ac4df1eb65574003d4
Igboanugwo Collins <munisco12@gmail.com>
2023-05-09 19:27:27 +0100

    working on resident wallet

--- src/pages/SuperAdmin/Wallet/commission/CommissionWallet.tsx
+++ src/pages/SuperAdmin/Wallet/commission/CommissionWallet.tsx
@@ -3,27 +3,11 @@ import Table from '../../../../components/UI/table/Table'
 import CommissionDialog from './CommissionDialog'
 
 const CommissionWallet = () => {
-    type Path = 'balance' | 'history'
+   
 
-    const paths: Path[] = ['balance', 'history']
-
-    const [currentPath, setcurrentPath] = useState<Path>('balance')
-
-    const ResidentBalance = () => {
-        return (
-            <div className='rounded-lg mt-[3rem] min-h-[60vh]'>
-                <Table
-                    is_dropdown={false}
-                    fetch_url={'/admin/get/wallet/balance/resident'}
-                    title={'balance'}
-                    THeader={['resident name', 'amount']}
-                    data_to_display={['name', 'balance']}
-                />
-            </div>
-        )
-    }
-    const TransactionHistory = () => {
-        return (
+    return (
+        <div className='grid mt-12 pb-10 rounded-lg  items-baseline gap-10'>
+            <CommissionDialog />
             <div className='rounded-lg mt-[3rem] min-h-[60vh]'>
                 <Table
                     fetch_url={'/admin/get/wallet/transaction/resident'}
@@ -49,44 +33,6 @@ const CommissionWallet = () => {
                         'created_at',
                     ]}
                 />
-            </div>
-        )
-    }
-
-    const handlePathSwitch = new Map([
-        ['history', <TransactionHistory />],
-        ['balance', <ResidentBalance />],
-    ]) satisfies Map<Path, JSX.Element>
-
-    return (
-
-            <div className='grid mt-12 pb-10 rounded-lg  items-baseline gap-10'>
-            
-                <CommissionDialog />
-                <div className='grid gap-10'>
-                    <div className='estateDetail__radioBox'>
-                        {paths.map((path) => (
-                            <>
-                                <input
-                                    type='radio'
-                                    name='resident'
-                                    id={path}
-                                    className='hidden'
-                                    onChange={() => setcurrentPath(path)}
-                                    checked={path === currentPath}
-                                />
-                                <label htmlFor={path} className='capitalize'>
-                                    {path.replace('-', ' ')}
-                                </label>
-                            </>
-                        ))}
-                    </div>
-                    <div className='mt-8 grid gap-8'>
-                        <section className='bg-color-white rounded-lg border min-w-[112rem] overflow-scroll'>
-                            {handlePathSwitch.get(currentPath)}
-                        </section>
-                    </div>
-                </div>
             </div>
         </div>
     )
