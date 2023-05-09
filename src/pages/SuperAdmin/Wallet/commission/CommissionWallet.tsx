971de287cb17c9d455d0dee49113ccf4111570fa
Igboanugwo Collins <munisco12@gmail.com>
2023-05-09 18:49:43 +0100

    working on resident wallet

--- src/pages/SuperAdmin/Wallet/commission/CommissionWallet.tsx
+++ src/pages/SuperAdmin/Wallet/commission/CommissionWallet.tsx
@@ -13,11 +13,11 @@ import useFetchData from '../../../../utils/useFetchData'
 import CommissionDialog from './CommissionDialog'
 
 const CommissionWallet = () => {
-    type Path = 'resident-balance' | 'transaction-history'
+    type Path = 'balance' | 'history'
 
-    const paths: Path[] = ['resident-balance', 'transaction-history']
+    const paths: Path[] = ['balance', 'history']
 
-    const [currentPath, setcurrentPath] = useState<Path>('resident-balance')
+    const [currentPath, setcurrentPath] = useState<Path>('balance')
 
     const ResidentBalance = () => {
         return (
@@ -64,8 +64,8 @@ const CommissionWallet = () => {
     }
 
     const handlePathSwitch = new Map([
-        ['transaction-history', <TransactionHistory />],
-        ['resident-balance', <ResidentBalance />],
+        ['history', <TransactionHistory />],
+        ['balance', <ResidentBalance />],
     ]) satisfies Map<Path, JSX.Element>
 
     const { data: graph_data, isLoading: graph_loading } = useFetchData({
