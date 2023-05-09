16a4e17adb4011e9176f78607d0e901249cc4a99
Igboanugwo Collins <munisco12@gmail.com>
2023-05-09 12:40:14 +0100

    working on resident wallet

--- src/components/UI/table/TableData.tsx
+++ src/components/UI/table/TableData.tsx
@@ -90,11 +90,15 @@ const TableData = () => {
             const store_data: Record<string, string>[] = []
 
             for (let i = 0; i < data_to_display.length; i++) {
-                if (data_to_display[i] !== 'image') {
-                    store_data.push({
-                        [data_to_display[i]]: THeader[i],
-                    })
+                if (data_to_display[i] === 'image') {
+                    continue
                 }
+
+                console.log(data_to_display[i])
+
+                store_data.push({
+                    [data_to_display[i]]: THeader[i],
+                })
             }
 
             // data.forEach((item) => {
