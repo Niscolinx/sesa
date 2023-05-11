ff2882691a13ee97f3dde9a182fa6e80bddedfb4
Igboanugwo Collins <munisco12@gmail.com>
2023-05-11 14:31:33 +0100

    working on UI updates

--- src/pages/SuperAdmin/advert/AdvertViews.tsx
+++ src/pages/SuperAdmin/advert/AdvertViews.tsx
@@ -1,34 +1,27 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
-import { CgSpinnerTwo } from 'react-icons/cg'
-import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi'
-import { FiDownload } from 'react-icons/fi'
-import { useNavigate, useParams } from 'react-router'
+import { useParams } from 'react-router'
 import Table from '../../../components/UI/table/Table'
 
-const AdvertClickrateIncrease = () => {
+const AdvertViews = () => {
     const params = useParams()
     const advert_id = params.id
-
 
     return (
         <div className='rounded-lg mt-[3rem] min-h-[60vh]'>
             <Table
-                fetch_url={
-                    `/advertstatistics/clickorview/details?advert_id=${advert_id}&category=click&perPage=10`
-                }
+                fetch_url={`/advertstatistics/clickorview/details?advert_id=${advert_id}&category=click&perPage=10`}
                 title={'clicks'}
                 is_dropdown={false}
                 THeader={[
                     'estate_name',
                     'address',
                     'resident_count',
-                    'clicks_count'
+                    'clicks_count',
                 ]}
                 data_to_display={[
                     'estate_name',
-                    'address', 
+                    'address',
                     'no_of_resident',
-                    'clicks'
+                    'clicks',
                 ]}
             />
         </div>
