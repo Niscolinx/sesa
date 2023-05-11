a569e7e6fc53eaa6cb25662e5b3aed00fe0af3ef
Igboanugwo Collins <munisco12@gmail.com>
2023-05-11 13:35:10 +0100

    working on UI updates

--- src/pages/SuperAdmin/advert/AdvertClickrateIncrease.tsx
+++ src/pages/SuperAdmin/advert/AdvertClickrateIncrease.tsx
@@ -3,6 +3,7 @@ import { CgSpinnerTwo } from 'react-icons/cg'
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi'
 import { FiDownload } from 'react-icons/fi'
 import { useNavigate } from 'react-router'
+import Table from '../../../components/UI/table/Table'
 
 export interface IAdvertClickrateIncrease {
     id: string
@@ -58,271 +59,36 @@ export const ADVERT_CLICK_RATE_INCREASE: IAdvertClickrateIncrease[] = [
 ]
 
 const AdvertClickrateIncrease = () => {
-    const navigate = useNavigate()
-
-    const [fetchedAdvertClickrateIncrease, setFetchedAdvertClickrateIncrease] =
-        useState<IAdvertClickrateIncrease[]>([])
-
-    useEffect(() => {
-        setTimeout(() => {
-            setFetchedAdvertClickrateIncrease(ADVERT_CLICK_RATE_INCREASE)
-        }, 200)
-    }, [])
-
-    interface Paginate {
-        index: number
-        currentPage: number
-        itemsPerPage: number
-        totalPage: number
-        slicedPages: IAdvertClickrateIncrease[][] | null
-    }
-
-    const itemsPerPageArr = [2, 4, 6, 8]
-    const perPage = 4
-
-    const [paginate, setPaginate] = useState<Paginate>({
-        index: 0,
-        currentPage: 1,
-        itemsPerPage: perPage,
-        totalPage: Math.ceil(fetchedAdvertClickrateIncrease.length / perPage),
-        slicedPages: null,
-    })
-
-    const handleItemsPerPage = (e: ChangeEvent<HTMLSelectElement>) => {
-        const item = parseInt(e.target.value)
-
-        const slicedPages: IAdvertClickrateIncrease[][] = []
-        for (let i = 0; i < fetchedAdvertClickrateIncrease.length; i += item) {
-            slicedPages.push(fetchedAdvertClickrateIncrease.slice(i, i + item))
-        }
-
-        setPaginate((prev) => {
-            return {
-                ...prev,
-                itemsPerPage: item,
-                index: 0,
-                currentPage: 1,
-                slicedPages,
-                totalPage: Math.ceil(
-                    fetchedAdvertClickrateIncrease.length / item
-                ),
-            }
-        })
-    }
-
-    useEffect(() => {
-        const slicedPages: IAdvertClickrateIncrease[][] = []
-        for (
-            let i = 0;
-            i < fetchedAdvertClickrateIncrease.length;
-            i += paginate.itemsPerPage
-        ) {
-            slicedPages.push(
-                fetchedAdvertClickrateIncrease.slice(
-                    i,
-                    i + paginate.itemsPerPage
-                )
-            )
-        }
-
-        setPaginate((prev) => {
-            return {
-                ...prev,
-                slicedPages,
-                totalPage: Math.ceil(
-                    fetchedAdvertClickrateIncrease.length /
-                        paginate.itemsPerPage
-                ),
-            }
-        })
-    }, [fetchedAdvertClickrateIncrease])
-
-    const handleNext = () => {
-        console.log(paginate.currentPage, paginate.totalPage)
-        if (paginate.currentPage === paginate.totalPage) return
-        setPaginate((prev) => {
-            return {
-                ...prev,
-                index: prev.index + 1,
-                currentPage: prev.currentPage + 1,
-            }
-        })
-    }
-
-    const handlePrev = () => {
-        if (paginate.currentPage === 1) return
-        setPaginate((prev) => {
-            return {
-                ...prev,
-                index: prev.index - 1,
-                currentPage: prev.currentPage - 1,
-            }
-        })
-    }
-
-    const { currentPage, slicedPages, itemsPerPage } = paginate
-
-    const jumpToPage = (e: React.MouseEvent, index: number) => {
-        setPaginate((prev) => {
-            return {
-                ...prev,
-                index,
-                currentPage: index + 1,
-            }
-        })
-    }
-
-    const handleCSVDownload = () => {
-        console.log('download csv')
-    }
-
+    
     return (
-        <>
-            <div className='grid text-[1.6rem] border'>
-                <div className='flex w-full items-center gap-12 p-10 bg-white rounded-lg'>
-                    <p className=' font-Satoshi-Medium'>
-                        Total Views <span>(10)</span>
-                    </p>
-                    <div className='relative flex items-center'>
-                        <img
-                            src='/icons/admins/search.svg'
-                            alt=''
-                            className='absolute left-4 text-[4rem]'
-                        />
-                        <input
-                            type='text'
-                            placeholder='Search Parameters'
-                            className='pl-16 w-[25rem] rounded-lg border border-color-blue-light appearance-none outline-none p-4'
-                        />
-                    </div>
-                    <button
-                        className='btn border-[#0556E5] text-[#0556E5] border rounded-lg gap-3 ml-auto flex items-center'
-                        onClick={handleCSVDownload}
-                    >
-                        <FiDownload />
-                        Download CSV
-                    </button>
-                </div>
-
-                <div className='grid bg-white'>
-                    <div
-                        className='grid justify-between text-color-dark-1 bg-color-grey p-8 grid-cols-4 gap-8'
-                        style={{
-                            fontSize: '1.4rem',
-                        }}
-                    >
-                        <p className='flex items-center gap-2'>
-                            <input type='checkbox' className='cursor-pointer' />
-                            <p>Estate Name</p>
-                        </p>
-                        <p>Location</p>
-                        <p>No of Views</p>
-                        <p>No of Residents</p>
-                    </div>
-
-                    <div className='grid gap-8 mt-8 p-8'>
-                        {slicedPages && slicedPages.length > 0 ? (
-                            React.Children.toArray(
-                                slicedPages[paginate.index].map(
-                                    (
-                                        {
-                                            estateName,
-                                            location,
-                                            noOfResidents,
-                                            noOfViews,
-                                        },
-                                        i
-                                    ) => {
-                                        return (
-                                            <div className='grid justify-between border-b grid-cols-4 items-center gap-8 '>
-                                                <p className='flex items-center gap-4'>
-                                                    <input
-                                                        type='checkbox'
-                                                        className='cursor-pointer'
-                                                    />
-
-                                                    <span>{estateName}</span>
-                                                </p>
-                                                <p>{location}</p>
-                                                <p>{noOfResidents}</p>
-                                                <p>{noOfViews}</p>
-                                            </div>
-                                        )
-                                    }
-                                )
-                            )
-                        ) : (
-                            <div>
-                                <div className='relative'>
-                                    <div className='absolute w-full grid place-content-center'>
-                                        <CgSpinnerTwo className='animate-spin text-[#0660FE] text-4xl' />
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <footer className='flex items-center p-4 mt-4 bg-color-white rounded-lg'>
-                    <div className='flex gap-8 items-center'>
-                        <p>View</p>
-                        <select
-                            name=''
-                            id=''
-                            className='flex items-center border px-4 rounded-lg outline-none cursor-pointer'
-                            onChange={handleItemsPerPage}
-                        >
-                            {itemsPerPageArr.map((item, index) => (
-                                <option
-                                    value={item}
-                                    key={index}
-                                    selected={item === itemsPerPage}
-                                    className='capitalize cursor-pointer bg-white'
-                                >
-                                    {item}
-                                </option>
-                            ))}
-                        </select>
-                        <p className='text'>List per page</p>
-                    </div>
-                    <ul className='flex items-center gap-5 ml-10'>
-                        <HiOutlineChevronLeft
-                            onClick={handlePrev}
-                            className='cursor-pointer'
-                        />
-
-                        {slicedPages?.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    {index + 1 === currentPage ? (
-                                        <span className='bg-color-primary text-white grid place-content-center w-[3rem] h-[3rem] cursor-pointer'>
-                                            {index + 1}
-                                        </span>
-                                    ) : (
-                                        <span
-                                            className='text-color-primary bg-white grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'
-                                            onClick={(e) =>
-                                                jumpToPage(e, index)
-                                            }
-                                        >
-                                            {index + 1}
-                                        </span>
-                                    )}
-                                </li>
-                            )
-                        })}
-
-                        {/* <li className='grid place-content-center border w-[3rem] h-[3rem] cursor-pointer'>
-                        {totalPage}
-                    </li> */}
-                        <HiOutlineChevronRight
-                            onClick={handleNext}
-                            className='cursor-pointer'
-                        />
-                    </ul>
-                </footer>
-            </div>
-        </>
+         <div className='rounded-lg mt-[3rem] min-h-[60vh]'>
+            <Table
+                fetch_url={'/admin/get/all'}
+                title={'admin'}
+                view_page_url={'/superAdmin/admin/view/'}
+                add_page_url={'/superAdmin/admin/add'}
+                is_add_btn={true}
+                THeader={[
+                    'name',
+                    'gender',
+                    'phone number',
+                    'joined date',
+                    'status',
+                    'actions',
+                ]}
+                data_to_display={[
+                    'name',
+                    'image',
+                    'gender',
+                    'phone',
+                    'created_at',
+                    'status',
+                ]}
+                deactivateProp={{ url: '/admin/deactivate_activate' }}
+            />
+        </div>
     )
 }
+
 
 export default AdvertClickrateIncrease
