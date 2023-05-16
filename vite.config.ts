f594157e460a66463e80d361e059bbe1cc0fb7c6
Igboanugwo Collins <munisco12@gmail.com>
2023-05-16 09:34:18 +0100

    working on naming convention

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    }
+  },
 
   resolve: {
     // alias: [
