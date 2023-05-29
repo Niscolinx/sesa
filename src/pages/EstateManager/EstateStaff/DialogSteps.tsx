bdcd301ef43dda92ce0574f18c7d5158e2b880e3
Igboanugwo Collins <munisco12@gmail.com>
2023-05-29 13:27:01 +0100

    working on property

--- src/pages/EstateManager/EstateStaff/DialogSteps.tsx
+++ src/pages/EstateManager/EstateStaff/DialogSteps.tsx
@@ -1,55 +1,14 @@
 import { useContext } from 'react'
 import { TbCopy } from 'react-icons/tb'
 import { Select } from '../../../components/ui/Select'
-import { AddedEstateStaffContext } from './AddEstateStaff'
 
-export const AddedEstateStaffSuccessfully = ({
-    context,
-}: {
-    context: React.Context<AddedEstateStaffContext>
-}) => {
-    const { handleClose, setAddedEstateStaffStep } = useContext(context)
 
-    return (
-        <div className='w-full grid justify-items-center gap-4'>
-            <img src='/icons/admins/modalSuccess.svg' alt='' />
-
-            <p>You have successfully added an Estate Staff </p>
-
-            <p
-                className='font-Satoshi-Medium text-[1.8rem] max-w-[40rem] text-center'
-                style={{
-                    fontFamily: 'Satoshi-Medium',
-                }}
-            >
-                Do you want to open a bank account for this Estate Staff ?
-            </p>
-
-            <div className='flex w-full justify-center gap-8 mt-10'>
-                <button
-                    className='btn bg-white text-[#0556E5] border-[#0556E5] border rounded-lg w-[20rem] font-Satoshi-Medium'
-                    onClick={() => handleClose()}
-                >
-                    Skip, Later
-                </button>
-                <button
-                    className='bg-[#0556E5] py-2 px-12 text-white text-[1.6rem] rounded-lg w-[20rem]'
-                    onClick={() => setAddedEstateStaffStep('addBankAccount')}
-                >
-                    Open an Account
-                </button>
-            </div>
-        </div>
-    )
-}
-
-export const AddBankAccount = ({
-    context,
-}: {
-    context: React.Context<AddedEstateStaffContext>
-}) => {
+export const AddBankAccount = () => {
     const { selectedBank, setSelectedBank, setAddedEstateStaffStep } =
         useContext(context)
+
+
+    
 
     return (
         <div className='w-[40rem] grid justify-items-center gap-10'>
