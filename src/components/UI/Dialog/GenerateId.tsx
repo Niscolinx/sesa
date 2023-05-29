637adf261c16e57a287adf739f587d2265335711
Igboanugwo Collins <munisco12@gmail.com>
2023-05-29 13:55:24 +0100

    working on property

---
+++ src/components/UI/Dialog/GenerateId.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from 'react'
+
+
+interface Props {
+    title: string
+    open: boolean
+    type?: string
+    isBank?: boolean
+    close: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+function GenerateId({
+    title,
+    open = false,
+    close,
+    type = 'added',
+    isBank,
+}: Props) {
+    const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+    useEffect(() => {
+        if (open) {
+            handleOpen()
+        }
+    }, [open])
+
+    const handleOpen = () => {
+        if (dialogRef.current) {
+            dialogRef.current.showModal()
+        }
+    }
+    const handleClose = () => {
+        close(false)
+        if (dialogRef.current) {
+            dialogRef.current.close()
+        }
+    }
+
+    return (
+        <dialog className='dialog' ref={bankRef}>
+            <section className='grid place-content-center w-full h-[100vh]'>
+                <div className='bg-white rounded-2xl grid place-content-center justify-items-center w-[64rem] min-h-[30rem] gap-8 p-10'>
+                    <div className='bg-white rounded-2xl grid place-content-center justify-items-center  gap-8 text-[1.6rem]'>
+                        <img src='/img/new_Id.svg' alt='' />
+                        <button
+                            className='btn text-white bg-color-blue-1 py-4 px-16 rounded-lg w-[15rem]'
+                            // onClick={() => handleClose()}
+                        >
+                            Print
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </dialog>
+    )
+}
+
+export default GenerateId
