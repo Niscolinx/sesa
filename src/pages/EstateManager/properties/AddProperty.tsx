df4e112cc3288a2892c2fada4b23740428688c49
Igboanugwo Collins <munisco12@gmail.com>
2023-05-29 05:56:35 +0100

    working on property

--- src/pages/EstateManager/properties/AddProperty.tsx
+++ src/pages/EstateManager/properties/AddProperty.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Input, { SelectProps } from '../../../components/ui/input/Input'
 import ImageInput from '../../../components/ui/input/ImageInput'
 import AddBtn from '../../../components/ui/button/AddBtn'
 import AddedSuccess from '../../../components/ui/dialog/AddedSuccess'
 import Spinner from '../../../components/ui/Spinner'
 import useAddPageMutation from '../../../components/hooks/useAddPageMutation'
-import ValidateKY from '../../../components/ui/dialog/ValidateKY'
 import useFetchData from '../../../components/hooks/UseFetchData'
 
 function AddProperty() {
