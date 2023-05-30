004a3b1b46447e6afd8c919b3ae7b80a4558f9d1
Igboanugwo Collins <munisco12@gmail.com>
2023-05-30 12:54:43 +0100

    changed and moved the select input

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
 import { useNavigate } from 'react-router'
