32b7e53fac325d517e18063622c1ba95d4c55c15
Igboanugwo Collins <munisco12@gmail.com>
2023-06-06 16:25:50 +0100

    working on new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client"/>
+// /// <reference types="vitest" />
+// /// <reference types="vite/client"/>
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react', 'react-dom'],
+//     }
+//   },
+
+//   resolve: {
+   
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    }
-  },
-
-  resolve: {
-    // alias: [
-    //   {
-    //     find: /^~(.*)$/,
-    //     replacement: '$1',
-
-    //   },
-    // ]
-  },
-});
+    plugins: [react()],
+})
