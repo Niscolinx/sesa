0e5012fc0fbb911299a6e52f43e369ed08ae9ceb
Igboanugwo Collins <munisco12@gmail.com>
2023-06-14 17:07:39 +0100

    added deactivate api

--- src/pages/estateManager/estateStaff/ViewEstateStaff.tsx
+++ src/pages/estateManager/estateStaff/ViewEstateStaff.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Input, { SelectProps } from '../../../components/ui/input/Input'
-import ImageInput from '../../../components/ui/input/ImageInput'
 import AddBtn from '../../../components/ui/button/AddBtn'
 import AddedSuccess from '../../../components/ui/dialog/AddedSuccess'
 import Spinner from '../../../components/ui/Spinner'
