ca63d02e381faf2d10a12d6b908d6d2ec2bc652b
Igboanugwo Collins <munisco12@gmail.com>
2023-07-07 17:11:11 +0100

    working on iconBox for estatemanager

--- src/components/ui/input/ImageInput.tsx
+++ src/components/ui/input/ImageInput.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface ImageInput {
 	handlePicture: (e: React.ChangeEvent) => void;
 	photoPreview: string;
@@ -31,6 +33,7 @@ export function ShowImage({ handlePicture, photoPreview }: ImageInput) {
 }
 
 function ImageInput({ handlePicture, photoPreview, dimension }: ImageInput) {
+    const [imgSrc, setImgSrc] = useState<string | null>(null)
 	const imageHandler = (e: React.ChangeEvent) => {
 		const target = e.target as HTMLInputElement;
 		const file: File = (target.files as FileList)[0];
@@ -80,7 +83,7 @@ function ImageInput({ handlePicture, photoPreview, dimension }: ImageInput) {
 			{photoPreview && (
 				<div className="flex justify-center justify-self-center">
 					<img
-						src={photoPreview || "/default-avatar.jpg"}
+						src={imgSrc || photoPreview || "/default-avatar.jpg"}
 						alt="photoPreview"
 						className="object-cover w-[11rem] h-[11rem] rounded-full"
 					/>
